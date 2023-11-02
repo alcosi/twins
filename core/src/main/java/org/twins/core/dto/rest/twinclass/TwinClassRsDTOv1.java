475f3595226b51ce949a676406891a0bd317baf1
nikita.mekh <n.mekh@hutkigrosh.by>
2023-11-02 21:32:56 +0300

    TwinClassRsDTOv1 schema name fix

--- core/src/main/java/org/twins/core/dto/rest/twinclass/TwinClassRsDTOv1.java
+++ core/src/main/java/org/twins/core/dto/rest/twinclass/TwinClassRsDTOv1.java
@@ -9,7 +9,7 @@ import org.twins.core.dto.rest.ResponseRelatedObjectsDTOv1;
 @Data
 @Accessors(chain = true)
 @EqualsAndHashCode(callSuper = true)
-@Schema(name = "TwinClassListRsV1")
+@Schema(name = "TwinClassRsV1")
 public class TwinClassRsDTOv1 extends ResponseRelatedObjectsDTOv1 {
     @Schema(description = "results - twin class")
     public TwinClassDTOv1 twinClass;
