75bb9d4e88dc2f73e8b4b03a8c06c3b531112e62
nikita.mekh <n.mekh@hutkigrosh.by>
2023-12-16 11:39:31 +0300

    * swagger examples to reduce swagger-ui size * dependencies update

--- core/src/main/java/org/twins/core/dto/rest/related/RelatedObjectsDTOv1.java
+++ core/src/main/java/org/twins/core/dto/rest/related/RelatedObjectsDTOv1.java
@@ -2,9 +2,7 @@ package org.twins.core.dto.rest.related;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
-import org.twins.core.dto.rest.Response;
 import org.twins.core.dto.rest.twin.TwinDTOv2;
 import org.twins.core.dto.rest.twin.TwinStatusDTOv1;
 import org.twins.core.dto.rest.twinclass.TwinClassDTOv1;
@@ -18,18 +16,18 @@ import java.util.UUID;
 @Accessors(chain = true)
 @Schema(name =  "RelatedObjectsV1")
 public class RelatedObjectsDTOv1 {
-    @Schema(description = "related statuses map", example = "")
+    @Schema(description = "related statuses map", example = "{twin map}")
     public Map<UUID, TwinDTOv2> twinMap;
 
-    @Schema(description = "related statuses map")
+    @Schema(description = "related statuses map", example = "{twin status map}")
     public Map<UUID, TwinStatusDTOv1> statusMap;
 
-    @Schema(description = "related users map")
+    @Schema(description = "related users map", example = "{user map}")
     public Map<UUID, UserDTOv1> userMap;
 
-    @Schema(description = "related twinClass map")
+    @Schema(description = "related twinClass map", example = "{twin class map}")
     public Map<UUID, TwinClassDTOv1> twinClassMap;
 
-    @Schema(description = "related transitionsMap map")
+    @Schema(description = "related transitionsMap map", example = "{twin transition map}")
     public Map<UUID, TwinTransitionViewDTOv1> transitionsMap;
 }
