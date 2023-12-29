4a8a7fe22173fbc8b893d88a4d346f0ba0817861
nikita.mekh <n.mekh@hutkigrosh.by>
2023-12-29 18:41:57 +0300

    ConcurrentModificationException: fix

--- core/src/main/java/org/twins/core/mappers/rest/MapperContext.java
+++ core/src/main/java/org/twins/core/mappers/rest/MapperContext.java
@@ -171,15 +171,16 @@ public class MapperContext {
 
     public MapperContext cloneIgnoreRelatedObjects() {
         MapperContext mapperContext = new MapperContext();
+        mapperContext.lazyRelations = false;
         mapperContext.modes = new Hashtable<>(this.modes); // new map with presets
         mapperContext.cachedObjects = this.cachedObjects; // same map
-        mapperContext.lazyRelations = this.lazyRelations; // same map
         mapperContext.properties = new Hashtable<>(this.properties); // new map with presets
         return mapperContext;
     }
 
     public MapperContext isolateModes() {
         MapperContext mapperContext = cloneIgnoreRelatedObjects();
+        mapperContext.lazyRelations = this.lazyRelations;
         mapperContext.relatedUserMap = this.relatedUserMap;
         mapperContext.relatedTwinClassMap = this.relatedTwinClassMap;
         mapperContext.relatedTwinStatusMap = this.relatedTwinStatusMap;
