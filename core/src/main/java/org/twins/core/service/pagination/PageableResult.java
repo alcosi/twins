6bb3b869f3288e48c9bde87c501b3221a508348f
Aleksey Turkov <alextur94@gmail.com>
2024-02-01 11:01:27 +0300

    TWINS-11: add someone files for pagination

--- core/src/main/java/org/twins/core/service/pagination/PageableResult.java
+++ core/src/main/java/org/twins/core/service/pagination/PageableResult.java
@@ -6,7 +6,7 @@ import lombok.experimental.Accessors;
 @Data
 @Accessors(chain = true)
 public abstract class PageableResult {
-    private int page;
-    private int count;
+    private int offset;
+    private int limit;
     private long total;
 }
