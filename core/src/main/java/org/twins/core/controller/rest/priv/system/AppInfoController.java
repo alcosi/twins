08e67596135d7b29d9c5ee1ae6cd345e5e57551c
Alexander Kuzniatsou <a.kuzniatsou@alcosi.eu>
2024-04-08 13:12:18 +0300

    TWINS-70: [system] get system info API

---
+++ core/src/main/java/org/twins/core/controller/rest/priv/system/AppInfoController.java
@@ -0,0 +1,48 @@
+package org.twins.core.controller.rest.priv.system;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.twins.core.controller.rest.ApiController;
+import org.twins.core.controller.rest.ApiTag;
+import org.twins.core.controller.rest.annotation.Loggable;
+import org.twins.core.controller.rest.annotation.ParametersApiUserHeaders;
+import org.twins.core.dto.rest.system.AppInfoRsDTOv1;
+import org.twins.core.service.system.system.AppInfoService;
+
+@Tag(description = "Application info form MANIFEST.MF", name = ApiTag.SYSTEM)
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RequiredArgsConstructor
+public class AppInfoController extends ApiController {
+    final AppInfoService appInfoService;
+
+    @ParametersApiUserHeaders
+    @Operation(operationId = "Application info", summary = "Returns application info: versions, etc...")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Application info", content = {
+                    @Content(mediaType = "application/json", schema =
+                    @Schema(implementation = AppInfoRsDTOv1.class))}),
+            @ApiResponse(responseCode = "401", description = "Access is denied")})
+    @GetMapping(value = "/private/system/info")
+    @Loggable(rsBodyThreshold = 2000)
+    public ResponseEntity<?> getAppInfo() {
+        AppInfoRsDTOv1 rs = new AppInfoRsDTOv1();
+        try {
+            rs.setAttributes(appInfoService.getManifestAttributes());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return createErrorRs(e, rs);
+        }
+        return new ResponseEntity<>(rs, HttpStatus.OK);
+    }
+}
---
+++ core/src/main/java/org/twins/core/service/system/system/AppInfoService.java
@@ -0,0 +1,51 @@
+package org.twins.core.service.system.system;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.jar.Attributes;
+import java.util.jar.Manifest;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class AppInfoService {
+
+    public static final String BUILD_REVISION = "Build-Revision";
+    public static final String BUILD_JDK = "Build-Jdk";
+    public static final String BUILD_TIMESTAMP = "Build-Timestamp";
+    public static final String IMPLEMENTATION_VERSION = "Implementation-Version";
+    public static final String BUILD_DISPLAY = "Build-Display";
+    public static final String BUILD_TAG = "Build-Tag";
+
+    private static final List<String> infoKeys = new ArrayList<>(){{
+        add(BUILD_REVISION);
+        add(BUILD_JDK);
+        add(BUILD_TIMESTAMP);
+        add(BUILD_DISPLAY);
+        add(BUILD_TAG);
+        add(IMPLEMENTATION_VERSION);
+    }};
+
+    public static Map<String, String> ATTRIBUTES = new HashMap<>();
+
+    public Map<String, String> getManifestAttributes() throws Exception {
+        if(!ATTRIBUTES.isEmpty() && !ATTRIBUTES.containsKey("error")) return ATTRIBUTES;
+        else {
+            InputStream manifestStream = getClass().getClassLoader().getResourceAsStream("META-INF/MANIFEST.MF");
+            if (manifestStream != null) {
+                Manifest manifest = new Manifest(manifestStream);
+                Attributes attrs = manifest.getMainAttributes();
+                for(String key : infoKeys) ATTRIBUTES.put(key, attrs.getValue(key));
+            } else {
+                ATTRIBUTES.put("error", "MANIFEST.MF is not available");
+            }
+            return ATTRIBUTES;
+        }
+    }
+}
