f63719db5cc88b88c051390223ea5e7e3c86194c
Alexander Kuzniatsou <a.kuzniatsou@alcosi.eu>
2024-04-18 13:44:30 +0300

    TWINS-86: [factory] new conditioner ConditionerContextValueExists

---
+++ core/src/main/java/org/twins/core/featurer/factory/conditioner/ConditionerContextValueExists.java
@@ -0,0 +1,31 @@
+package org.twins.core.featurer.factory.conditioner;
+
+import lombok.extern.slf4j.Slf4j;
+import org.cambium.common.exception.ServiceException;
+import org.cambium.featurer.annotations.Featurer;
+import org.cambium.featurer.annotations.FeaturerParam;
+import org.cambium.featurer.params.FeaturerParamUUID;
+import org.springframework.stereotype.Component;
+import org.twins.core.domain.factory.FactoryItem;
+import org.twins.core.featurer.factory.filler.FieldLookupMode;
+
+import java.util.Properties;
+
+@Component
+@Featurer(id = 2418,
+        name = "ConditionerContextValueExists",
+        description = "")
+@Slf4j
+public class ConditionerContextValueExists extends Conditioner {
+    @FeaturerParam(name = "twinClassFieldId", description = "")
+    public static final FeaturerParamUUID twinClassFieldId = new FeaturerParamUUID("twinClassFieldId");
+
+    @Override
+    public boolean check(Properties properties, FactoryItem factoryItem) throws ServiceException {
+        return check(properties, factoryItem, FieldLookupMode.fromContextFields);
+    }
+
+    public boolean check(Properties properties, FactoryItem factoryItem, FieldLookupMode fieldLookupMode) throws ServiceException {
+        return null != factoryService.lookupFieldValue(factoryItem, twinClassFieldId.extract(properties), fieldLookupMode);
+    }
+}
