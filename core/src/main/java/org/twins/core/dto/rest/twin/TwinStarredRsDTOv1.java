c792543432b801aef8da8557f2526e85059b73a6
Aleksey Turkov <alextur94@gmail.com>
2024-05-10 11:24:32 +0300

    TWINS-104: change extends for TwinStarredRsDTOv1

--- core/src/main/java/org/twins/core/dto/rest/twin/TwinStarredRsDTOv1.java
+++ core/src/main/java/org/twins/core/dto/rest/twin/TwinStarredRsDTOv1.java
@@ -4,13 +4,13 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
-import org.twins.core.dto.rest.ResponseRelatedObjectsDTOv1;
+import org.twins.core.dto.rest.Response;
 
 @Data
 @Accessors(fluent = true)
 @EqualsAndHashCode(callSuper = true)
 @Schema(name =  "TwinStarredRsV1")
-public class TwinStarredRsDTOv1 extends ResponseRelatedObjectsDTOv1 {
+public class TwinStarredRsDTOv1 extends Response {
     @Schema(description = "twin starred")
     public TwinStarredDTOv1 twinStarred;
 }
