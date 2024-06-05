ed94bc09dabf8fd52dd1df1cf8fe96afbf81eefc
Alexander Kuzniatsou <a.kuzniatsou@alcosi.eu>
2024-06-05 10:44:16 +0300

    TWINS-105: [search] stored in db fix paramuuidset

--- core/src/main/java/org/cambium/featurer/params/FeaturerParamUUIDSet.java
+++ core/src/main/java/org/cambium/featurer/params/FeaturerParamUUIDSet.java
@@ -23,7 +23,7 @@ public class FeaturerParamUUIDSet extends FeaturerParam<Set<UUID>> {
     @Override
     public Set<UUID> extract(Properties properties) {
         Set<UUID> ret = new HashSet<>();
-        if (!ObjectUtils.isEmpty(properties.get(key)) && !StringUtils.isNotBlank(properties.get(key).toString()))
+        if (!ObjectUtils.isEmpty(properties.get(key)) && StringUtils.isNotBlank(properties.get(key).toString()))
             for (String uuidString : properties.get(key).toString().split(","))
                 ret.add(UUID.fromString(uuidString.trim()));
         return ret;
