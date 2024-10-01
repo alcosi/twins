eb0c47a49c64ce7910ffb02577bce285ba3703c4
Aleksey Turkov <alextur94@gmail.com>
2024-10-01 13:59:58 +0300

    TWINS-170: refactoring code in migration 3

--- core/src/main/resources/db/migration_backlog/V1.3.x.x__TWINS-170_at2_twin_status.sql
+++ core/src/main/resources/db/migration_backlog/V1.3.x.x__TWINS-170_at2_twin_status.sql
@@ -1,5 +1,14 @@
-alter table twin_status
-    rename column color to background_color;
+DO $$
+BEGIN
+    IF NOT EXISTS (
+        SELECT 1
+        FROM information_schema.columns
+        WHERE table_name = 'twin_status' AND column_name = 'background_color'
+    ) THEN
+ALTER TABLE twin_status
+    RENAME COLUMN color TO background_color;
+END IF;
+END $$;
 
 alter table twin_status
     add if not exists font_color varchar;
