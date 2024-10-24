ae254b6471f8c74a2ffee3ad8ab70dfa3a62ab5d
Alexander Kuzniatsou <a.kuzniatsou@alcosi.eu>
2024-10-24 18:05:07 +0300

    TWINS-182: [validators] validators set: top level mappers

---
+++ core/src/main/java/org/twins/core/mappers/rest/validator/TwinActionValidatorRuleBaseV1RestDTOMapper.java
@@ -0,0 +1,35 @@
+package org.twins.core.mappers.rest.validator;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.twins.core.controller.rest.annotation.MapperModeBinding;
+import org.twins.core.dao.validator.TwinActionValidatorRuleEntity;
+import org.twins.core.dto.rest.validator.TwinActionValidatorRuleBaseDTOv1;
+import org.twins.core.mappers.rest.RestSimpleDTOMapper;
+import org.twins.core.mappers.rest.mappercontext.MapperContext;
+import org.twins.core.mappers.rest.mappercontext.modes.ValidatorRuleMode;
+
+@Component
+@RequiredArgsConstructor
+@MapperModeBinding(modes = {ValidatorRuleMode.TwinActionValidatorRule2ValidatorRuleMode.class})
+public class TwinActionValidatorRuleBaseV1RestDTOMapper extends RestSimpleDTOMapper<TwinActionValidatorRuleEntity, TwinActionValidatorRuleBaseDTOv1> {
+
+    private final ValidatorRuleBaseV1RestDTOMapper validatorRuleBaseV1RestDTOMapper;
+
+    @Override
+    public void map(TwinActionValidatorRuleEntity src, TwinActionValidatorRuleBaseDTOv1 dst, MapperContext mapperContext) throws Exception {
+        validatorRuleBaseV1RestDTOMapper.map(src, dst, mapperContext.forkOnPoint(mapperContext.getModeOrUse(ValidatorRuleMode.TwinflowTransitionValidatorRule2ValidatorRuleMode.SHORT)));
+        switch (mapperContext.getModeOrUse(ValidatorRuleMode.TwinActionValidatorRule2ValidatorRuleMode.SHORT)) {
+            case DETAILED, SHORT:
+                dst.setTwinClassId(src.getTwinClassId())
+                        .setTwinAction(src.getTwinAction());
+                break;
+        }
+    }
+
+    @Override
+    public boolean hideMode(MapperContext mapperContext) {
+        return mapperContext.hasModeOrEmpty(ValidatorRuleMode.TwinActionValidatorRule2ValidatorRuleMode.HIDE);
+    }
+
+}
---
+++ core/src/main/java/org/twins/core/mappers/rest/validator/TwinCommentAlienValidatorRuleBaseV1RestDTOMapper.java
@@ -0,0 +1,35 @@
+package org.twins.core.mappers.rest.validator;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.twins.core.controller.rest.annotation.MapperModeBinding;
+import org.twins.core.dao.validator.TwinCommentActionAlienValidatorRuleEntity;
+import org.twins.core.dto.rest.validator.TwinCommentAlienValidatorRuleBaseDTOv1;
+import org.twins.core.mappers.rest.RestSimpleDTOMapper;
+import org.twins.core.mappers.rest.mappercontext.MapperContext;
+import org.twins.core.mappers.rest.mappercontext.modes.ValidatorRuleMode;
+
+@Component
+@RequiredArgsConstructor
+@MapperModeBinding(modes = {ValidatorRuleMode.TwinCommentActionAlienValidatorRule2ValidatorRuleMode.class})
+public class TwinCommentAlienValidatorRuleBaseV1RestDTOMapper extends RestSimpleDTOMapper<TwinCommentActionAlienValidatorRuleEntity, TwinCommentAlienValidatorRuleBaseDTOv1> {
+
+    private final ValidatorRuleBaseV1RestDTOMapper validatorRuleBaseV1RestDTOMapper;
+
+    @Override
+    public void map(TwinCommentActionAlienValidatorRuleEntity src, TwinCommentAlienValidatorRuleBaseDTOv1 dst, MapperContext mapperContext) throws Exception {
+            validatorRuleBaseV1RestDTOMapper.map(src, dst, mapperContext.forkOnPoint(mapperContext.getModeOrUse(ValidatorRuleMode.TwinCommentActionAlienValidatorRule2ValidatorRuleMode.SHORT)));
+            switch (mapperContext.getModeOrUse(ValidatorRuleMode.TwinflowTransitionValidatorRule2ValidatorRuleMode.SHORT)) {
+                case DETAILED, SHORT:
+                    dst.setTwinClassId(src.getTwinClassId())
+                            .setCommentAction(src.getTwinCommentAction());
+                    break;
+            }
+    }
+
+    @Override
+    public boolean hideMode(MapperContext mapperContext) {
+        return mapperContext.hasModeOrEmpty(ValidatorRuleMode.TwinCommentActionAlienValidatorRule2ValidatorRuleMode.HIDE);
+    }
+
+}
