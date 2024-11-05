1dc35164ec8dcced9d8f084e70939a972e4953cb
nikita.mekh <n.mekh@hutkigrosh.by>
2024-11-05 19:50:09 +0300

    TWINS-60: ApiUserResolverService for more easy init of thread local api user

---
+++ core/src/main/java/org/twins/core/service/auth/ApiUserResolverService.java
@@ -0,0 +1,127 @@
+package org.twins.core.service.auth;
+
+import lombok.*;
+import lombok.experimental.Accessors;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.ArrayUtils;
+import org.cambium.common.exception.ServiceException;
+import org.cambium.common.util.CollectionUtils;
+import org.cambium.service.EntitySmartService;
+import org.springframework.stereotype.Service;
+import org.twins.core.dao.businessaccount.BusinessAccountEntity;
+import org.twins.core.dao.businessaccount.BusinessAccountRepository;
+import org.twins.core.dao.businessaccount.BusinessAccountUserEntity;
+import org.twins.core.dao.businessaccount.BusinessAccountUserRepository;
+import org.twins.core.dao.domain.*;
+import org.twins.core.dao.user.UserEntity;
+import org.twins.core.dao.user.UserRepository;
+import org.twins.core.domain.apiuser.DomainResolverHeaders;
+import org.twins.core.domain.apiuser.LocaleResolverDomainUser;
+import org.twins.core.domain.apiuser.LocaleResolverHeader;
+import org.twins.core.domain.apiuser.UserBusinessAccountResolverAuthToken;
+import org.twins.core.exception.ErrorCodeTwins;
+
+import java.util.List;
+import java.util.UUID;
+
+import static org.twins.core.domain.ApiUser.NOT_SPECIFIED;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ApiUserResolverService {
+    final EntitySmartService entitySmartService;
+    final DomainRepository domainRepository;
+    final DomainUserRepository domainUserRepository;
+    final DomainBusinessAccountRepository domainBusinessAccountRepository;
+    final BusinessAccountRepository businessAccountRepository;
+    final BusinessAccountUserRepository businessAccountUserRepository;
+    final UserRepository userRepository;
+    @Getter
+    final DomainResolverHeaders domainResolverHeaders;
+    @Getter
+    final LocaleResolverDomainUser localeResolverDomainUser;
+    @Getter
+    final LocaleResolverHeader localeResolverHeader;
+    @Getter
+    final UserBusinessAccountResolverAuthToken userBusinessAccountResolverAuthToken;
+
+    public DomainEntity findDomain(UUID domainId) throws ServiceException {
+        return entitySmartService.findById(domainId, domainRepository, EntitySmartService.FindMode.ifEmptyThrows);
+    }
+
+    public BusinessAccountEntity findBusinessAccount(UUID businessAccountId) throws ServiceException {
+        return entitySmartService.findById(businessAccountId, businessAccountRepository, EntitySmartService.FindMode.ifEmptyThrows);
+    }
+
+    public UserEntity findUser(UUID userId) throws ServiceException {
+        return entitySmartService.findById(userId, userRepository, EntitySmartService.FindMode.ifEmptyThrows);
+    }
+
+    public void loadDBU(UUID domainId, UUID businessAccountId, UUID userId, DBU dbu, boolean checkMembershipMode) throws ServiceException {
+        if (checkMembershipMode) {
+            if (isUserSpecified(userId) && isDomainSpecified(domainId) && isBusinessAccountSpecified(businessAccountId) && (dbu.getDomain() == null || dbu.getBusinessAccount() == null || dbu.getUser() == null)) {
+                List<Object[]> dbuList = userRepository.findDBU_ByUserIdAndBusinessAccountIdAndDomainId(userId, businessAccountId, domainId);
+                if (CollectionUtils.isEmpty(dbuList) || dbuList.size() != 1 || ArrayUtils.isEmpty(dbuList.get(0)) || dbuList.get(0).length != 3)
+                    throw new ServiceException(ErrorCodeTwins.USER_UNKNOWN, "User[" + userId + "] is not registered in domain[" + domainId + "] or business account[" + businessAccountId + "]");
+                dbu
+                        .setDomain((DomainEntity) dbuList.get(0)[0])
+                        .setBusinessAccount((BusinessAccountEntity) dbuList.get(0)[1])
+                        .setUser((UserEntity) dbuList.get(0)[2]);
+                return;
+            } else if (isDomainSpecified(domainId) && isBusinessAccountSpecified(businessAccountId) && (dbu.getDomain() == null || dbu.getBusinessAccount() == null)) {
+                DomainBusinessAccountEntity domainBusinessAccountEntity = domainBusinessAccountRepository.findByDomainIdAndBusinessAccountId(domainId, businessAccountId);
+                if (domainBusinessAccountEntity == null)
+                    throw new ServiceException(ErrorCodeTwins.USER_UNKNOWN, "Business account[" + businessAccountId + "] is not registered in domain[" + domainId + "]");
+                dbu
+                        .setDomain(domainBusinessAccountEntity.getDomain())
+                        .setBusinessAccount(domainBusinessAccountEntity.getBusinessAccount());
+                return;
+            } else if (isDomainSpecified(domainId) && isUserSpecified(userId) && (dbu.getDomain() == null || dbu.getUser() == null)) {
+                DomainUserEntity domainUserEntity = domainUserRepository.findByDomainIdAndUserId(domainId, userId, DomainUserEntity.class);
+                if (domainUserEntity == null)
+                    throw new ServiceException(ErrorCodeTwins.USER_UNKNOWN, "User[" + userId + "] is not registered in domain[" + domainId + "]");
+                dbu
+                        .setDomain(domainUserEntity.getDomain())
+                        .setUser(domainUserEntity.getUser());
+                return;
+            } else if (isBusinessAccountSpecified(businessAccountId) && isUserSpecified(userId) && (dbu.getBusinessAccount() == null || dbu.getUser() == null)) {
+                BusinessAccountUserEntity businessAccountUserEntity = businessAccountUserRepository.findByBusinessAccountIdAndUserId(businessAccountId, userId, BusinessAccountUserEntity.class);
+                if (businessAccountUserEntity == null)
+                    throw new ServiceException(ErrorCodeTwins.USER_UNKNOWN, "User[" + userId + "] is not registered in business account[" + businessAccountId + "]");
+                dbu
+                        .setBusinessAccount(businessAccountUserEntity.getBusinessAccount())
+                        .setUser(businessAccountUserEntity.getUser());
+                return;
+            }
+        }
+        if (isDomainSpecified(domainId) && dbu.getDomain() == null)
+            dbu.setDomain(findDomain(domainId));
+        if (isBusinessAccountSpecified(businessAccountId) && dbu.getBusinessAccount() == null)
+            dbu.setBusinessAccount(findBusinessAccount(businessAccountId));
+        if (isUserSpecified(userId) && dbu.getUser() == null)
+            dbu.setUser(findUser(userId));
+    }
+
+    private boolean isBusinessAccountSpecified(UUID businessAccountId) {
+        return businessAccountId != null && !NOT_SPECIFIED.equals(businessAccountId);
+    }
+
+    private boolean isDomainSpecified(UUID domainId) {
+        return domainId != null && !NOT_SPECIFIED.equals(domainId);
+    }
+
+    private boolean isUserSpecified(UUID userId) {
+        return userId != null && !NOT_SPECIFIED.equals(userId);
+    }
+
+    @Data
+    @Accessors(chain = true)
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class DBU {
+        private DomainEntity domain;
+        private BusinessAccountEntity businessAccount;
+        private UserEntity user;
+    }
+}
--- core/src/main/java/org/twins/core/service/auth/AuthService.java
+++ core/src/main/java/org/twins/core/service/auth/AuthService.java
@@ -3,19 +3,13 @@ package org.twins.core.service.auth;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.cambium.common.exception.ServiceException;
-import org.cambium.service.EntitySmartService;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.request.RequestContextHolder;
-import org.twins.core.dao.businessaccount.BusinessAccountRepository;
-import org.twins.core.dao.businessaccount.BusinessAccountUserRepository;
-import org.twins.core.dao.domain.DomainBusinessAccountRepository;
-import org.twins.core.dao.domain.DomainRepository;
-import org.twins.core.dao.domain.DomainUserRepository;
-import org.twins.core.dao.user.UserRepository;
 import org.twins.core.domain.ApiUser;
-import org.twins.core.domain.apiuser.*;
+import org.twins.core.domain.apiuser.BusinessAccountResolverGivenId;
+import org.twins.core.domain.apiuser.DomainResolverGivenId;
+import org.twins.core.domain.apiuser.UserResolverGivenId;
 
 import java.util.UUID;
 
@@ -25,37 +19,8 @@ import java.util.UUID;
 @Lazy
 @RequiredArgsConstructor
 public class AuthService {
-    private final ApplicationContext applicationContext;
-//    final HttpRequestService httpRequestService;
-//    final UserRepository userRepository;
-//    final BusinessAccountRepository businessAccountRepository;
-//    final DomainRepository domainRepository;
-//    @Lazy
-//    final DomainService domainService;
-//    final EntitySmartService entitySmartService;
-//    @Lazy
-//    final FeaturerService featurerService;
-//    private ApiUser apiUser = null;
-//
-//    public ApiUser getApiUser() throws ServiceException {
-//        if (apiUser != null)
-//            return apiUser;
-//        UUID domainId;
-//        try {
-//            domainId = UUID.fromString(httpRequestService.getDomainIdFromRequest());
-//        } catch (Exception e) {
-//            throw new ServiceException(ErrorCodeTwins.UUID_UNKNOWN, "Incorrect domain uuid");
-//        }
-//        String authToken = httpRequestService.getAuthTokenFromRequest();
-//        if (StringUtils.isEmpty(authToken)) //todo delete on production
-//            authToken = httpRequestService.getBusinessAccountIdFromRequest() + "," + httpRequestService.getUserIdFromRequest();
-//        DomainEntity domainEntity = domainService.findDomain(domainId, EntitySmartService.FindMode.ifEmptyThrows);
-//        String channel = httpRequestService.getChannelIdFromRequest();
-//        TokenHandler tokenHandler = featurerService.getFeaturer(domainEntity.getTokenHandlerFeaturer(), TokenHandler.class);
-//        apiUser = tokenHandler.resolveUserIdAndBusinessAccountId(domainEntity.getTokenHandlerParams(), authToken, domainEntity, Channel.resolve(channel));
-//        return apiUser;
-//        //todo store ApiUser in session context
-//    }
+    @Lazy
+    private final ApiUserResolverService apiUserResolverService;
 
     final ApiUser apiUser;
 
@@ -64,33 +29,17 @@ public class AuthService {
     public ApiUser getApiUser() throws ServiceException {
         if (RequestContextHolder.getRequestAttributes() != null)
             return apiUser;
-        else
+        else // in so,e cases we need to emulate user work, for example it some job is running from scheduler
             return threadLocalApiUser.get();
     }
 
     public void setThreadLocalApiUser(UUID domainId, UUID businessAccountId, UUID userId) {
         //todo think over ApiUser interface
-        ApiUser apiUser = new ApiUser(
-                applicationContext.getBean(EntitySmartService.class),
-                applicationContext.getBean(DomainRepository.class),
-                applicationContext.getBean(DomainUserRepository.class),
-                applicationContext.getBean(DomainBusinessAccountRepository.class),
-                applicationContext.getBean(BusinessAccountRepository.class),
-                applicationContext.getBean(BusinessAccountUserRepository.class),
-                applicationContext.getBean(UserRepository.class),
-                applicationContext.getBean(DomainResolverHeaders.class),
-                applicationContext.getBean(LocaleResolverDomainUser.class),
-                applicationContext.getBean(LocaleResolverHeader.class),
-                applicationContext.getBean(UserBusinessAccountResolverAuthToken.class));
+        ApiUser apiUser = new ApiUser(apiUserResolverService);
         apiUser
                 .setUserResolver(new UserResolverGivenId(userId))
                 .setDomainResolver(new DomainResolverGivenId(domainId))
                 .setBusinessAccountResolver(new BusinessAccountResolverGivenId(businessAccountId));
-
-        threadLocalApiUser.set(apiUser);
-    }
-
-    public void setThreadLocalApiUser(ApiUser apiUser) {
         threadLocalApiUser.set(apiUser);
     }
 
