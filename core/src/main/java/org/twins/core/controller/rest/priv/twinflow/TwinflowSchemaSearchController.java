ed54dc6315529d1289c43045f3c8ca97405568f3
Aleksey Turkov <alextur94@gmail.com>
2024-11-18 11:48:52 +0300

    TWINS-193: twinflow schema search API

---
+++ core/src/main/java/org/twins/core/controller/rest/priv/twinflow/TwinflowSchemaSearchController.java
@@ -0,0 +1,72 @@
+package org.twins.core.controller.rest.priv.twinflow;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.cambium.common.exception.ServiceException;
+import org.cambium.common.pagination.PaginationResult;
+import org.cambium.common.pagination.SimplePagination;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.twins.core.controller.rest.ApiController;
+import org.twins.core.controller.rest.ApiTag;
+import org.twins.core.controller.rest.annotation.MapperContextBinding;
+import org.twins.core.controller.rest.annotation.ParametersApiUserHeaders;
+import org.twins.core.controller.rest.annotation.SimplePaginationParams;
+import org.twins.core.dao.twinflow.TwinflowSchemaEntity;
+import org.twins.core.dto.rest.twinflow.TwinflowSchemaSearchRqDTOv1;
+import org.twins.core.dto.rest.twinflow.TwinflowSchemaSearchRsDTOv1;
+import org.twins.core.mappers.rest.mappercontext.MapperContext;
+import org.twins.core.mappers.rest.pagination.PaginationMapper;
+import org.twins.core.mappers.rest.related.RelatedObjectsRestDTOConverter;
+import org.twins.core.mappers.rest.twinflow.TwinflowSchemaRestDTOMapperV2;
+import org.twins.core.mappers.rest.twinflow.TwinflowSchemaSearchRestDTOReverseMapper;
+import org.twins.core.service.twinflow.TwinflowSchemaSearchService;
+
+@Tag(name = ApiTag.TWINFLOW)
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RequiredArgsConstructor
+public class TwinflowSchemaSearchController extends ApiController {
+    private final TwinflowSchemaSearchRestDTOReverseMapper twinflowSchemaSearchRestDTOReverseMapper;
+    private final RelatedObjectsRestDTOConverter relatedObjectsRestDTOMapper;
+    private final PaginationMapper paginationMapper;
+    private final TwinflowSchemaSearchService twinflowSchemaSearchService;
+    private final TwinflowSchemaRestDTOMapperV2 twinflowSchemaRestDTOMapperV2;
+
+    @ParametersApiUserHeaders
+    @Operation(operationId = "twinflowSchemaSearchV1", summary = "Returns twinflow schema search result")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Twinflow schema list prepared", content = {
+                    @Content(mediaType = "application/json", schema =
+                    @Schema(implementation = TwinflowSchemaSearchRsDTOv1.class))}),
+            @ApiResponse(responseCode = "401", description = "Access is denied")})
+    @PostMapping(value = "/private/twinflow_schema/search/v1")
+    public ResponseEntity<?> twinflowSchemaSearchV1(
+            @MapperContextBinding(roots = TwinflowSchemaRestDTOMapperV2.class, response = TwinflowSchemaSearchRsDTOv1.class) MapperContext mapperContext,
+            @SimplePaginationParams SimplePagination pagination,
+            @RequestBody TwinflowSchemaSearchRqDTOv1 request) {
+        TwinflowSchemaSearchRsDTOv1 rs = new TwinflowSchemaSearchRsDTOv1();
+        try {
+            PaginationResult<TwinflowSchemaEntity> twinflowSchemaList = twinflowSchemaSearchService
+                    .findTwinflowSchemaForDomain(twinflowSchemaSearchRestDTOReverseMapper.convert(request), pagination);
+            rs
+                    .setTwinflowSchemas(twinflowSchemaRestDTOMapperV2.convertCollection(twinflowSchemaList.getList(), mapperContext))
+                    .setPagination(paginationMapper.convert(twinflowSchemaList))
+                    .setRelatedObjects(relatedObjectsRestDTOMapper.convert(mapperContext));
+        } catch (ServiceException se) {
+            return createErrorRs(se, rs);
+        } catch (Exception e) {
+            return createErrorRs(e, rs);
+        }
+        return new ResponseEntity<>(rs, HttpStatus.OK);
+    }
+}
---
+++ core/src/main/java/org/twins/core/dto/rest/twinflow/TwinflowSchemaSearchRqDTOv1.java
@@ -0,0 +1,47 @@
+package org.twins.core.dto.rest.twinflow;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+import org.twins.core.dto.rest.Request;
+
+import java.util.Set;
+import java.util.UUID;
+
+@Data
+@Accessors(chain = true)
+@EqualsAndHashCode(callSuper = false)
+@Schema(name = "TwinflowSchemaSearchRqV1")
+public class TwinflowSchemaSearchRqDTOv1 extends Request {
+
+    @Schema(description = "id list")
+    public Set<UUID> idList;
+
+    @Schema(description = "id exclude list")
+    public Set<UUID> idExcludeList;
+
+    @Schema(description = "name like list")
+    public Set<String> nameLikeList;
+
+    @Schema(description = "name not like list")
+    public Set<String> nameNotLikeList;
+
+    @Schema(description = "description like list")
+    public Set<String> descriptionLikeList;
+
+    @Schema(description = "description not like list")
+    public Set<String> descriptionNotLikeList;
+
+    @Schema(description = "business account id list")
+    public Set<UUID> businessAccountIdList;
+
+    @Schema(description = "business account id exclude list")
+    public Set<UUID> businessAccountIdExcludeList;
+
+    @Schema(description = "user id list")
+    public Set<UUID> createdByUserIdList;
+
+    @Schema(description = "user id exclude list")
+    public Set<UUID> createdByUserIdExcludeList;
+}
