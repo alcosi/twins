a937e99e1e752453ad8aa767e3e09062bf82e418
Alexander Kuzniatsou <a.kuzniatsou@alcosi.eu>
2024-11-19 17:11:35 +0300

    TWINS-220: [validator] make validation rules linked to twin-validator with many to many relationship

--- core/src/main/java/org/twins/core/service/attachment/AttachmentActionService.java
+++ core/src/main/java/org/twins/core/service/attachment/AttachmentActionService.java
@@ -15,7 +15,7 @@ import org.springframework.stereotype.Service;
 import org.twins.core.dao.attachment.*;
 import org.twins.core.dao.twin.*;
 import org.twins.core.dao.twinclass.TwinClassEntity;
-import org.twins.core.dao.validator.TwinValidatorEntity;
+import org.twins.core.dao.validator.*;
 import org.twins.core.exception.ErrorCodeTwins;
 import org.twins.core.featurer.twin.validator.TwinValidator;
 import org.twins.core.service.auth.AuthService;
