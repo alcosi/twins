6a433c1017035dadcc46189c249885e74efb7a28
Alexander Kuzniatsou <a.kuzniatsou@alcosi.eu>
2024-12-03 12:45:43 +0300

    TWINS-225: ConditionerMathCompareContextTwinFieldValues

---
+++ core/src/main/resources/db/migration_backlog/V1.3.135.03__TWINS-225_ak3_conditioner_field_math_compare .sql
@@ -0,0 +1,1 @@
+INSERT INTO public.featurer (id, featurer_type_id, class, name, description, deprecated) VALUES (2428, 24, 'org.twins.core.featurer.factory.conditioner.ConditionerMathCompareContextTwinFieldValues', 'ConditionerMathCompareContextTwinFieldValues', '', false) on conflict (id) do nothing;
