fb1059ecb0170dda8abf8f2ea935331e06aa67e8
Aleksey Turkov <alextur94@gmail.com>
2025-01-10 11:12:29 +0300

    TWINS-235: factory branch search API

---
+++ core/src/main/java/org/twins/core/domain/search/FactoryBranchSearch.java
@@ -0,0 +1,24 @@
+package org.twins.core.domain.search;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+import org.cambium.common.util.Ternary;
+
+import java.util.Set;
+import java.util.UUID;
+
+@Data
+@Accessors(chain = true)
+public class FactoryBranchSearch {
+    private Set<UUID> idList;
+    private Set<UUID> idExcludeList;
+    private Set<UUID> factoryIdList;
+    private Set<UUID> factoryIdExcludeList;
+    private Set<UUID> factoryConditionSetIdList;
+    private Set<UUID> factoryConditionSetIdExcludeList;
+    private Set<UUID> nextFactoryIdList;
+    private Set<UUID> nextFactoryIdExcludeList;
+    private Set<String> descriptionLikeList;
+    private Set<String> descriptionNotLikeList;
+    private Ternary active;
+}
