1f4ebd3e621bd0192060361e727a5f719a257736
Aleksey Turkov <alextur94@gmail.com>
2025-01-14 23:20:36 +0300

    TWINS-274: new factory create/update api

---
+++ core/src/main/java/org/twins/core/dto/rest/factory/FactoryRsDTOv1.java
@@ -0,0 +1,16 @@
+package org.twins.core.dto.rest.factory;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+import org.twins.core.dto.rest.ResponseRelatedObjectsDTOv1;
+
+@Data
+@Accessors(chain = true)
+@EqualsAndHashCode(callSuper = false)
+@Schema(name = "FactoryCreateRsV1")
+public class FactoryRsDTOv1 extends ResponseRelatedObjectsDTOv1 {
+    @Schema(description = "results - factory")
+    public FactoryDTOv2 factory;
+}
