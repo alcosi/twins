d6ba1f04156776e41f1bc8d38d595140cdbb53c6
Aleksey Turkov <alextur94@gmail.com>
2025-01-29 11:48:39 +0300

    TWINS-283: adding cache for method in repo

--- core/src/main/java/org/twins/core/dao/domain/DomainTypeTwinClassOwnerTypeRepository.java
+++ core/src/main/java/org/twins/core/dao/domain/DomainTypeTwinClassOwnerTypeRepository.java
@@ -1,10 +1,14 @@
 package org.twins.core.dao.domain;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface DomainTypeTwinClassOwnerTypeRepository extends CrudRepository<DomainTypeTwinClassOwnerTypeEntity, UUID> {
+    String CACHE_DOMAIN_OWNER_TYPE_ID = "DomainTypeTwinClassOwnerTypeRepository.findAllByDomainTypeId";
+
+    @Cacheable(value = CACHE_DOMAIN_OWNER_TYPE_ID, key = "#domainType.id")
     List<DomainTypeTwinClassOwnerTypeEntity> findAllByDomainTypeId(DomainType domainType);
 }
