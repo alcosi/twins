e6ce534f7786f0954993919c0544c95bebfd21c2
Aleksey Turkov <alextur94@gmail.com>
2025-02-13 11:02:49 +0300

    TWINS-244: refactoring code

--- core/src/main/java/org/twins/core/domain/search/CommentSearch.java
+++ core/src/main/java/org/twins/core/domain/search/CommentSearch.java
@@ -3,7 +3,7 @@ package org.twins.core.domain.search;
 import lombok.Data;
 import lombok.experimental.Accessors;
 import lombok.experimental.FieldNameConstants;
-import org.twins.core.dto.rest.DataTimeRangeDTOv1;
+import org.twins.core.domain.DataTimeRange;
 
 import java.util.Set;
 import java.util.UUID;
@@ -12,14 +12,14 @@ import java.util.UUID;
 @Accessors(chain = true)
 @FieldNameConstants
 public class CommentSearch {
-    Set<UUID> idList;
-    Set<UUID> idExcludeList;
-    Set<UUID> twinIdList;
-    Set<UUID> twinIdExcludeList;
-    Set<UUID> createdByUserIdList;
-    Set<UUID> createdByUserIdExcludeList;
-    Set<String> textLikeList;
-    Set<String> textNotLikeList;
-    DataTimeRangeDTOv1 createdAt;
-    DataTimeRangeDTOv1 updatedAt;
+    private Set<UUID> idList;
+    private Set<UUID> idExcludeList;
+    private Set<UUID> twinIdList;
+    private Set<UUID> twinIdExcludeList;
+    private Set<UUID> createdByUserIdList;
+    private Set<UUID> createdByUserIdExcludeList;
+    private Set<String> textLikeList;
+    private Set<String> textNotLikeList;
+    private DataTimeRange createdAt;
+    private DataTimeRange updatedAt;
 }
