f21207605398252dd779abfcd20457edeb7f2f63
Kiryl Kurylenka <c.s.2015@mail.ru>
2025-02-14 17:24:04 +0300

    Add TierRsDTOv1

---
+++ core/src/main/java/org/twins/core/dto/rest/domain/TierRsDTOv1.java
@@ -0,0 +1,16 @@
+package org.twins.core.dto.rest.domain;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+import org.twins.core.dto.rest.ResponseRelatedObjectsDTOv1;
+
+@Data
+@Accessors(chain = true)
+@EqualsAndHashCode(callSuper = false)
+@Schema(name = "TagRsV1")
+public class TierRsDTOv1  extends ResponseRelatedObjectsDTOv1 {
+    @Schema(description = "results - tier")
+    public TierDTOv2 tier;
+}
