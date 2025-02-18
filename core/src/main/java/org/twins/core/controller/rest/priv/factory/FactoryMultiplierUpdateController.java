a94e9f3dc1669cef17c10309ecfd58db399a58d3
Kiryl Kurylenka <c.s.2015@mail.ru>
2025-02-18 17:48:22 +0300

    TWINS-307: add FactoryMultiplier update api

---
+++ core/src/main/java/org/twins/core/controller/rest/priv/factory/FactoryMultiplierUpdateController.java
@@ -0,0 +1,69 @@
+package org.twins.core.controller.rest.priv.factory;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.cambium.common.exception.ServiceException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.twins.core.controller.rest.ApiController;
+import org.twins.core.controller.rest.ApiTag;
+import org.twins.core.controller.rest.annotation.MapperContextBinding;
+import org.twins.core.controller.rest.annotation.ParametersApiUserHeaders;
+import org.twins.core.dao.factory.TwinFactoryMultiplierEntity;
+import org.twins.core.dto.rest.DTOExamples;
+import org.twins.core.dto.rest.factory.FactoryBranchRsDTOv1;
+import org.twins.core.dto.rest.factory.FactoryMultiplierRsDTOv1;
+import org.twins.core.dto.rest.factory.FactoryMultiplierUpdateRqDTOv1;
+import org.twins.core.mappers.rest.factory.FactoryMultiplierRestDTOMapperV2;
+import org.twins.core.mappers.rest.factory.FactoryMultiplierUpdateDTOReverseMapper;
+import org.twins.core.mappers.rest.mappercontext.MapperContext;
+import org.twins.core.mappers.rest.related.RelatedObjectsRestDTOConverter;
+import org.twins.core.service.factory.FactoryMultiplierService;
+
+import java.util.UUID;
+
+@Tag(name = ApiTag.FACTORY)
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RequiredArgsConstructor
+public class FactoryMultiplierUpdateController extends ApiController {
+    private final FactoryMultiplierRestDTOMapperV2 factoryMultiplierRestDTOMapperV2;
+    private final FactoryMultiplierUpdateDTOReverseMapper factoryMultiplierUpdateDTOReverseMapper;
+    private final FactoryMultiplierService factoryMultiplierService;
+    private final RelatedObjectsRestDTOConverter relatedObjectsRestDTOConverter;
+
+    @ParametersApiUserHeaders
+    @Operation(operationId = "factoryMultiplierUpdateV1", summary = "Factory multiplier update")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Factory data multiplier update", content = {
+                    @Content(mediaType = "application/json", schema =
+                    @Schema(implementation = FactoryBranchRsDTOv1.class))}),
+            @ApiResponse(responseCode = "401", description = "Access is denied")})
+    @PutMapping(value = "/private/factory_multiplier/{factoryMultiplierId}/v1")
+    public ResponseEntity<?> factoryMultiplierUpdateV1(
+            @MapperContextBinding(roots = FactoryMultiplierRestDTOMapperV2.class, response = FactoryMultiplierRsDTOv1.class) MapperContext mapperContext,
+            @Parameter(example = DTOExamples.FACTORY_MULTIPLIER_ID) @PathVariable UUID factoryMultiplierId,
+            @RequestBody FactoryMultiplierUpdateRqDTOv1 request) {
+        FactoryMultiplierRsDTOv1 rs = new FactoryMultiplierRsDTOv1();
+        try {
+            TwinFactoryMultiplierEntity multiplierEntity = factoryMultiplierUpdateDTOReverseMapper.convert(request);
+              multiplierEntity.setId(factoryMultiplierId);
+            multiplierEntity = factoryMultiplierService.updateFactoryMultiplier(multiplierEntity);
+            rs
+                    .setFactoryMultiplier(factoryMultiplierRestDTOMapperV2.convert(multiplierEntity, mapperContext))
+                    .setRelatedObjects(relatedObjectsRestDTOConverter.convert(mapperContext));
+        } catch (ServiceException se) {
+            return createErrorRs(se, rs);
+        } catch (Exception e) {
+            return createErrorRs(e, rs);
+        }
+        return new ResponseEntity<>(rs, HttpStatus.OK);
+    }
+}
--- core/src/main/java/org/twins/core/service/factory/FactoryMultiplierService.java
+++ core/src/main/java/org/twins/core/service/factory/FactoryMultiplierService.java
@@ -5,16 +5,21 @@ import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.cambium.common.EasyLoggable;
 import org.cambium.common.exception.ServiceException;
+import org.cambium.common.util.ChangesHelper;
+import org.cambium.common.util.UuidUtils;
 import org.cambium.service.EntitySecureFindServiceImpl;
 import org.cambium.service.EntitySmartService;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.twins.core.dao.domain.DomainEntity;
 import org.twins.core.dao.factory.TwinFactoryMultiplierEntity;
 import org.twins.core.dao.factory.TwinFactoryMultiplierRepository;
 import org.twins.core.service.auth.AuthService;
+import org.twins.core.service.twinclass.TwinClassService;
 
+import java.util.HashMap;
 import java.util.UUID;
 import java.util.function.Function;
 
@@ -25,6 +30,7 @@ import java.util.function.Function;
 public class FactoryMultiplierService extends EntitySecureFindServiceImpl<TwinFactoryMultiplierEntity> {
     @Getter
     private final TwinFactoryMultiplierRepository repository;
+    private final TwinClassService twinClassService;
     private final AuthService authService;
 
     @Override
@@ -40,7 +46,7 @@ public class FactoryMultiplierService extends EntitySecureFindServiceImpl<TwinFa
     @Override
     public boolean isEntityReadDenied(TwinFactoryMultiplierEntity entity, EntitySmartService.ReadPermissionCheckMode readPermissionCheckMode) throws ServiceException {
         DomainEntity domain = authService.getApiUser().getDomain();
-        boolean readDenied=!entity.getTwinFactory().getDomainId().equals(domain.getId());
+        boolean readDenied = !entity.getTwinFactory().getDomainId().equals(domain.getId());
         if (readDenied) {
             EntitySmartService.entityReadDenied(readPermissionCheckMode, domain.easyLog(EasyLoggable.Level.NORMAL) + " is not allowed in domain[" + domain.easyLog(EasyLoggable.Level.NORMAL));
         }
@@ -49,10 +55,71 @@ public class FactoryMultiplierService extends EntitySecureFindServiceImpl<TwinFa
 
     @Override
     public boolean validateEntity(TwinFactoryMultiplierEntity entity, EntitySmartService.EntityValidateMode entityValidateMode) throws ServiceException {
-        return !isEntityReadDenied(entity,EntitySmartService.ReadPermissionCheckMode.none);
+        switch (entityValidateMode) {
+            case beforeSave:
+                if (entity.getInputTwinClassId() == null || !entity.getInputTwinClass().getId().equals(entity.getInputTwinClassId()))
+                    entity.setInputTwinClass(twinClassService.findEntitySafe(entity.getInputTwinClassId()));
+        }
+        return true;
     }
 
     public TwinFactoryMultiplierEntity createFactoryMultiplier(TwinFactoryMultiplierEntity entity) throws ServiceException {
+        entity.setId(UUID.randomUUID());
+        validateEntityAndThrow(entity, EntitySmartService.EntityValidateMode.beforeSave);
         return repository.save(entity);
+    }
+
+    @Transactional(rollbackFor = Throwable.class)
+    public TwinFactoryMultiplierEntity updateFactoryMultiplier(TwinFactoryMultiplierEntity multiplierUpdate) throws ServiceException {
+        TwinFactoryMultiplierEntity dbMultiplierEntity = findEntitySafe(multiplierUpdate.getId());
+        multiplierUpdate.setId(dbMultiplierEntity.getId());
+        ChangesHelper changesHelper = new ChangesHelper();
+
+        updateInputTwinClassId(dbMultiplierEntity, multiplierUpdate.getInputTwinClassId(), changesHelper);
+        updateMultiplierFeaturerId(dbMultiplierEntity, multiplierUpdate.getMultiplierFeaturerId(), changesHelper);
+        updateMultiplierParams(dbMultiplierEntity, multiplierUpdate.getMultiplierParams(), changesHelper);
+        updateActive(dbMultiplierEntity, multiplierUpdate.getActive(), changesHelper);
+        updateDescription(dbMultiplierEntity, multiplierUpdate.getDescription(), changesHelper);
+
+        if (changesHelper.hasChanges()) {
+            validateEntity(dbMultiplierEntity, EntitySmartService.EntityValidateMode.beforeSave);
+            dbMultiplierEntity = entitySmartService.saveAndLogChanges(dbMultiplierEntity, repository, changesHelper);
+        }
+        return dbMultiplierEntity;
+    }
+
+    private void updateInputTwinClassId(TwinFactoryMultiplierEntity dbMultiplierEntity, UUID newInputTwinClassId,
+                                        ChangesHelper changesHelper) {
+        if (!changesHelper.isChanged(TwinFactoryMultiplierEntity.Fields.inputTwinClass, dbMultiplierEntity.getInputTwinClassId(), newInputTwinClassId))
+            return;
+        dbMultiplierEntity.setInputTwinClassId(newInputTwinClassId);
+    }
+
+    private void updateMultiplierFeaturerId(TwinFactoryMultiplierEntity dbMultiplierEntity, Integer newMultiplierFeaturerId,
+                                            ChangesHelper changesHelper) {
+        if (!changesHelper.isChanged(TwinFactoryMultiplierEntity.Fields.multiplierFeaturerId, dbMultiplierEntity.getMultiplierFeaturerId(), newMultiplierFeaturerId))
+            return;
+        dbMultiplierEntity.setMultiplierFeaturerId(newMultiplierFeaturerId);
+    }
+
+    private void updateMultiplierParams(TwinFactoryMultiplierEntity dbMultiplierEntity, HashMap<String, String> newMultiplierParams,
+                                        ChangesHelper changesHelper) {
+        if (!changesHelper.isChanged(TwinFactoryMultiplierEntity.Fields.multiplierParams, dbMultiplierEntity.getMultiplierParams(), newMultiplierParams))
+            return;
+        dbMultiplierEntity.setMultiplierParams(newMultiplierParams);
+    }
+
+    private void updateActive(TwinFactoryMultiplierEntity dbMultiplierEntity, boolean newActive,
+                              ChangesHelper changesHelper) {
+        if (!changesHelper.isChanged(TwinFactoryMultiplierEntity.Fields.active, dbMultiplierEntity.getActive(), newActive))
+            return;
+        dbMultiplierEntity.setActive(newActive);
+    }
+
+    private void updateDescription(TwinFactoryMultiplierEntity dbMultiplierEntity, String newDescription,
+                                   ChangesHelper changesHelper) {
+        if (!changesHelper.isChanged(TwinFactoryMultiplierEntity.Fields.description, dbMultiplierEntity.getDescription(), newDescription))
+            return;
+        dbMultiplierEntity.setDescription(newDescription);
     }
 }
