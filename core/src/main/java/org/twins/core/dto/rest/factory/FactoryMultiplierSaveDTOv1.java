01121706d62bfa657458b8d9fff4d76426405d00
Kiryl Kurylenka <158213072+KevinORIS@users.noreply.github.com>
2025-02-21 10:56:01 +0300

    TWINS-307: Add create and update api for FactoryMultiplier  (#387)
    * TWINS-307: FactoryMultiplier create api
    
    * TWINS-307: add FactoryMultiplier update api
    
    * TWINS-307: fixes
    
    * TWINS-307: fixes
    
    * TWINS-307: fixes
    
    * TWINS-307: fixes
    
    * TWINS-307: fixes
    
    * TWINS-307: Fixes
    
    * TWINS-307: fixes
    
    * TWINS-307: fixes
    
    * TWINS-307: update DTO
    
    * TWINS-308: update DTO
    
    * TWINS-307: small naming fixes
    
    ---------
    
    Co-authored-by: nikita.mekh <n.mekh@hutkigrosh.by>

---
+++ core/src/main/java/org/twins/core/dto/rest/factory/FactoryMultiplierSaveDTOv1.java
@@ -0,0 +1,31 @@
+package org.twins.core.dto.rest.factory;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+import org.twins.core.dto.rest.DTOExamples;
+
+import java.util.HashMap;
+import java.util.UUID;
+
+@Data
+@Accessors(chain = true)
+@EqualsAndHashCode(callSuper = false)
+@Schema(name = "FactoryMultiplierSaveV1")
+public class FactoryMultiplierSaveDTOv1 {
+    @Schema(description = "input twin class id", example = DTOExamples.TWIN_CLASS_ID)
+    public UUID inputTwinClassId;
+
+    @Schema(description = "multiplier featurer id", example = DTOExamples.FEATURER_ID)
+    public Integer multiplierFeaturerId;
+
+    @Schema(description = "multiplier params", example = DTOExamples.FACTORY_PARAMS_MAP)
+    public HashMap<String, String> multiplierParams;
+
+    @Schema(description = "is active", example = DTOExamples.BOOLEAN_TRUE)
+    public Boolean active;
+
+    @Schema(description = "description", example = DTOExamples.DESCRIPTION)
+    public String description;
+}
