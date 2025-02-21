7c3d9a826e3387d0b6e6320f46dc91e8169fc705
Kiryl Kurylenka <158213072+KevinORIS@users.noreply.github.com>
2025-02-21 16:22:59 +0300

    TWINS-308: add factory pipeline create/update api (#389)
    * TWINS-308: Add Factory Pipeline create api
    
    * TWINS-308: Add Factory Pipeline update api
    
    * TWINS-308: Update api update for Factory Pipeline
    
    * TWINS-308: fixes
    
    * TWINS-308: update DTO
    
    * TWINS-308: update DTO
    
    * TWINS-308: code style fixes
    
    * TWINS-308: fixes
    
    * TWINS-308: fixes
    
    * TWINS-288: tier validation fix
    
    ---------
    
    Co-authored-by: nikita.mekh <n.mekh@hutkigrosh.by>

---
+++ core/src/main/java/org/twins/core/controller/rest/priv/factory/FactoryPipelineUpdateController.java
@@ -0,0 +1,68 @@
+package org.twins.core.controller.rest.priv.factory;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.cambium.common.exception.ServiceException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.twins.core.controller.rest.ApiController;
+import org.twins.core.controller.rest.ApiTag;
+import org.twins.core.controller.rest.annotation.MapperContextBinding;
+import org.twins.core.controller.rest.annotation.ParametersApiUserHeaders;
+import org.twins.core.dao.factory.TwinFactoryPipelineEntity;
+import org.twins.core.dto.rest.DTOExamples;
+import org.twins.core.dto.rest.factory.FactoryPipelineRsDTOv1;
+import org.twins.core.dto.rest.factory.FactoryPipelineUpdateRqDTOv1;
+import org.twins.core.mappers.rest.factory.FactoryPipelineRestDTOMapperV2;
+import org.twins.core.mappers.rest.factory.FactoryPipelineUpdateDTOReverseMapper;
+import org.twins.core.mappers.rest.mappercontext.MapperContext;
+import org.twins.core.mappers.rest.related.RelatedObjectsRestDTOConverter;
+import org.twins.core.service.factory.FactoryPipelineService;
+
+import java.util.UUID;
+
+@Tag(name = ApiTag.FACTORY)
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RequiredArgsConstructor
+public class FactoryPipelineUpdateController extends ApiController {
+    private final FactoryPipelineRestDTOMapperV2 factoryPipelineRestDTOMapperV2;
+    private final FactoryPipelineUpdateDTOReverseMapper factoryPipelineUpdateDTOReverseMapper;
+    private final RelatedObjectsRestDTOConverter relatedObjectsRestDTOConverter;
+    private final FactoryPipelineService factoryPipelineService;
+
+    @ParametersApiUserHeaders
+    @Operation(operationId = "factoryPipelineUpdateV1", summary = "Factory pipeline update")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Factory data pipeline update", content = {
+                    @Content(mediaType = "application/json", schema =
+                    @Schema(implementation = FactoryPipelineRsDTOv1.class))}),
+            @ApiResponse(responseCode = "401", description = "Access is denied")})
+    @PutMapping(value = "/private/factory_pipeline/{factoryPipelineId}/v1")
+    public ResponseEntity<?> factoryPipelineUpdateV1(
+            @MapperContextBinding(roots = FactoryPipelineRestDTOMapperV2.class, response = FactoryPipelineRsDTOv1.class) MapperContext mapperContext,
+            @Parameter(example = DTOExamples.FACTORY_PIPELINE_ID) @PathVariable UUID factoryPipelineId,
+            @RequestBody FactoryPipelineUpdateRqDTOv1 request) {
+        FactoryPipelineRsDTOv1 rs = new FactoryPipelineRsDTOv1();
+        try {
+            TwinFactoryPipelineEntity factoryPipeline = factoryPipelineUpdateDTOReverseMapper.convert(request)
+                    .setId(factoryPipelineId);
+            factoryPipeline = factoryPipelineService.updateFactoryPipeline(factoryPipeline);
+            rs
+                    .setFactoryPipeline(factoryPipelineRestDTOMapperV2.convert(factoryPipeline, mapperContext))
+                    .setRelatedObjects(relatedObjectsRestDTOConverter.convert(mapperContext));
+        } catch (ServiceException se) {
+            return createErrorRs(se, rs);
+        } catch (Exception e) {
+            return createErrorRs(e, rs);
+        }
+        return new ResponseEntity<>(rs, HttpStatus.OK);
+    }
+}
---
+++ core/src/main/java/org/twins/core/mappers/rest/factory/FactoryPipelineSaveDTOReverseMapper.java
@@ -0,0 +1,25 @@
+package org.twins.core.mappers.rest.factory;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.twins.core.dao.factory.TwinFactoryPipelineEntity;
+import org.twins.core.dto.rest.factory.FactoryPipelineSaveDTOv1;
+import org.twins.core.mappers.rest.RestSimpleDTOMapper;
+import org.twins.core.mappers.rest.mappercontext.MapperContext;
+
+@Component
+@RequiredArgsConstructor
+public class FactoryPipelineSaveDTOReverseMapper extends RestSimpleDTOMapper<FactoryPipelineSaveDTOv1, TwinFactoryPipelineEntity> {
+    @Override
+    public void map(FactoryPipelineSaveDTOv1 src, TwinFactoryPipelineEntity dst, MapperContext mapperContext) throws Exception {
+        dst
+                .setInputTwinClassId(src.getInputTwinClassId())
+                .setTwinFactoryConditionSetId(src.getFactoryConditionSetId())
+                .setTwinFactoryConditionInvert(src.getFactoryConditionSetInvert())
+                .setActive(src.getActive())
+                .setOutputTwinStatusId(src.getOutputStatusId())
+                .setNextTwinFactoryId(src.getNextFactoryId())
+                .setTemplateTwinId(src.getTemplateTwinId())
+                .setDescription(src.getDescription());
+    }
+}
