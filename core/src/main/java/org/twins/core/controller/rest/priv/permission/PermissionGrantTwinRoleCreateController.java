908c31e9648ce855ef5125c6de87dc071e8f6779
Kiryl Kurylenka <158213072+KevinORIS@users.noreply.github.com>
2025-02-27 00:54:45 +0300

    TWINS-300: new Permission Grant twin role create/update/delete api (#402)
    * TWINS-300: add DTO and Mappers
    
    * TWINS-300: create/update/delete api

---
+++ core/src/main/java/org/twins/core/controller/rest/priv/permission/PermissionGrantTwinRoleCreateController.java
@@ -0,0 +1,65 @@
+package org.twins.core.controller.rest.priv.permission;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.cambium.common.exception.ServiceException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.twins.core.controller.rest.ApiController;
+import org.twins.core.controller.rest.ApiTag;
+import org.twins.core.controller.rest.annotation.MapperContextBinding;
+import org.twins.core.controller.rest.annotation.ParametersApiUserHeaders;
+import org.twins.core.dao.permission.PermissionGrantTwinRoleEntity;
+import org.twins.core.dto.rest.permission.PermissionGrantTwinRoleCreateRqDTOv1;
+import org.twins.core.dto.rest.permission.PermissionGrantTwinRoleRsDTOv1;
+import org.twins.core.mappers.rest.mappercontext.MapperContext;
+import org.twins.core.mappers.rest.permission.PermissionGrantTwinRoleCreateDTOReverseMapper;
+import org.twins.core.mappers.rest.permission.PermissionGrantTwinRoleRestDTOMapperV2;
+import org.twins.core.mappers.rest.related.RelatedObjectsRestDTOConverter;
+import org.twins.core.service.permission.PermissionGrantTwinRoleService;
+
+@Tag(description = "", name = ApiTag.PERMISSION)
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RequiredArgsConstructor
+public class PermissionGrantTwinRoleCreateController extends ApiController {
+    private final PermissionGrantTwinRoleService permissionGrantTwinRoleService;
+    private final PermissionGrantTwinRoleRestDTOMapperV2 permissionGrantTwinRoleRestDTOMapper;
+    private final PermissionGrantTwinRoleCreateDTOReverseMapper permissionGrantTwinRoleCreateDTOReverseMapper;
+    private final RelatedObjectsRestDTOConverter relatedObjectsRestDTOConverter;
+
+    @ParametersApiUserHeaders
+    @Operation(operationId = "permissionGrantTwinRoleCreateV1", summary = "permission grant twin role create add")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "permission grant twin role create add", content = {
+                    @Content(mediaType = "application/json", schema =
+                    @Schema(implementation = PermissionGrantTwinRoleRsDTOv1.class))}),
+            @ApiResponse(responseCode = "401", description = "Access is denied")})
+    @PostMapping(value = "/private/permission_grant/twin_role/v1")
+    public ResponseEntity<?> permissionGrantTwinRoleCreateV1(
+            @MapperContextBinding(roots = PermissionGrantTwinRoleRestDTOMapperV2.class, response = PermissionGrantTwinRoleRsDTOv1.class) MapperContext mapperContext,
+            @RequestBody PermissionGrantTwinRoleCreateRqDTOv1 request) {
+        PermissionGrantTwinRoleRsDTOv1 rs = new PermissionGrantTwinRoleRsDTOv1();
+        try {
+            PermissionGrantTwinRoleEntity permissionGrantTwinRole = permissionGrantTwinRoleService.createPermissionGrantTwinRole
+                    (permissionGrantTwinRoleCreateDTOReverseMapper.convert(request.getPermissionGrantTwinRole()));
+            rs
+                    .setPermissionGrantTwinRole(permissionGrantTwinRoleRestDTOMapper.convert(permissionGrantTwinRole, mapperContext))
+                    .setRelatedObjects(relatedObjectsRestDTOConverter.convert(mapperContext));
+        } catch (ServiceException se) {
+            return createErrorRs(se, rs);
+        } catch (Exception e) {
+            return createErrorRs(e, rs);
+        }
+        return new ResponseEntity<>(rs, HttpStatus.OK);
+    }
+}
---
+++ core/src/main/java/org/twins/core/controller/rest/priv/permission/PermissionGrantTwinRoleUpdateController.java
@@ -0,0 +1,70 @@
+package org.twins.core.controller.rest.priv.permission;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.cambium.common.exception.ServiceException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.twins.core.controller.rest.ApiController;
+import org.twins.core.controller.rest.ApiTag;
+import org.twins.core.controller.rest.annotation.MapperContextBinding;
+import org.twins.core.controller.rest.annotation.ParametersApiUserHeaders;
+import org.twins.core.dao.permission.PermissionGrantTwinRoleEntity;
+import org.twins.core.dto.rest.DTOExamples;
+import org.twins.core.dto.rest.permission.PermissionGrantTwinRoleRsDTOv1;
+import org.twins.core.dto.rest.permission.PermissionGrantTwinRoleUpdateRqDTOv1;
+import org.twins.core.mappers.rest.mappercontext.MapperContext;
+import org.twins.core.mappers.rest.permission.PermissionGrantTwinRoleRestDTOMapperV2;
+import org.twins.core.mappers.rest.permission.PermissionGrantTwinRoleUpdateDTOReverseMapper;
+import org.twins.core.mappers.rest.related.RelatedObjectsRestDTOConverter;
+import org.twins.core.service.permission.PermissionGrantTwinRoleService;
+
+import java.util.UUID;
+
+@Tag(name = ApiTag.PERMISSION)
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RequiredArgsConstructor
+public class PermissionGrantTwinRoleUpdateController extends ApiController {
+    private final PermissionGrantTwinRoleService permissionGrantTwinRoleService;
+    private final PermissionGrantTwinRoleRestDTOMapperV2 permissionGrantTwinRoleRestDTOMapper;
+    private final PermissionGrantTwinRoleUpdateDTOReverseMapper permissionGrantTwinRoleUpdateDTOReverseMapper;
+    private final RelatedObjectsRestDTOConverter relatedObjectsRestDTOConverter;
+
+    @ParametersApiUserHeaders
+    @Operation(operationId = "permissionGrantTwinRoleUpdateV1", summary = "Permission grant twin role update")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Permission grant twin role update", content = {
+                    @Content(mediaType = "application/json", schema =
+                    @Schema(implementation = PermissionGrantTwinRoleRsDTOv1.class))}),
+            @ApiResponse(responseCode = "401", description = "Access is denied")})
+    @PutMapping(value = "/private/permission_grant/twin_role/{permissionGrantTwinRoleId}/v1")
+    public ResponseEntity<?> permissionGrantTwinRoleV1(
+            @MapperContextBinding(roots = PermissionGrantTwinRoleRestDTOMapperV2.class, response = PermissionGrantTwinRoleRsDTOv1.class) MapperContext mapperContext,
+            @Parameter(example = DTOExamples.PERMISSION_GRANT_TWIN_ROLE_ID) @PathVariable UUID permissionGrantTwinRoleId,
+            @RequestBody PermissionGrantTwinRoleUpdateRqDTOv1 request) {
+
+        PermissionGrantTwinRoleRsDTOv1 rs = new PermissionGrantTwinRoleRsDTOv1();
+        try {
+            PermissionGrantTwinRoleEntity permissionGrantTwinRole = permissionGrantTwinRoleUpdateDTOReverseMapper.convert(request.getPermissionGrantTwinRole())
+                    .setId(permissionGrantTwinRoleId);
+            permissionGrantTwinRole = permissionGrantTwinRoleService.updatePermissionGrantTwinRole(permissionGrantTwinRole);
+
+            rs
+                    .setPermissionGrantTwinRole(permissionGrantTwinRoleRestDTOMapper.convert(permissionGrantTwinRole, mapperContext))
+                    .setRelatedObjects(relatedObjectsRestDTOConverter.convert(mapperContext));
+        } catch (ServiceException se) {
+            return createErrorRs(se, rs);
+        } catch (Exception e) {
+            return createErrorRs(e, rs);
+        }
+        return new ResponseEntity<>(rs, HttpStatus.OK);
+    }
+}
