961c959089466722937481b056b6a66bcb380190
Aleksey Turkov <74702834+alextur94@users.noreply.github.com>
2025-02-27 15:46:17 +0300

    Twins 301: new permission grant user CUD api (#407)
    * TWINS-302: permission grant user group create/update api
    
    * TWINS-302: refactoring code
    
    * TWINS-302: adding delete controller and refactoring code
    
    * TWINS-302: refactoring code
    
    * TWINS-302: refactoring code 2
    
    * TWINS-302: refactoring code 3
    
    * TWINS-302: refactoring code
    
    * TWINS-302: missing files
    
    * TWINS-302: refactor delete method and change validate logic
    
    * TWINS-317: Creating factory eraser CUD api
    
    * TWINS-302: refactoring code
    
    * TWINS-301: new permission grant user CUD api
    
    * TWINS-317: refactoring code
    
    * TWINS-301: refactoring code

---
+++ core/src/main/java/org/twins/core/dto/rest/permission/PermissionGrantUserSaveDTOv1.java
@@ -0,0 +1,21 @@
+package org.twins.core.dto.rest.permission;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+import lombok.experimental.Accessors;
+import org.twins.core.dao.factory.TwinFactoryEraserEntity;
+import org.twins.core.dto.rest.DTOExamples;
+
+import java.util.UUID;
+
+@Data
+@Accessors(chain = true)
+@Schema(name = "PermissionGrantUserSaveV1")
+public class PermissionGrantUserSaveDTOv1 {
+    @Schema(description = "permission schema id", example = DTOExamples.PERMISSION_SCHEMA_ID)
+    public UUID permissionSchemaId;
+    @Schema(description = "permission id", example = DTOExamples.PERMISSION_ID)
+    public UUID permissionId;
+    @Schema(description = "user id", example = DTOExamples.USER_ID)
+    public UUID userId;
+}
