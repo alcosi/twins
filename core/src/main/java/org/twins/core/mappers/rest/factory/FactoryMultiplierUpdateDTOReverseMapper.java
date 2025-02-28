b4facc71665d8104ec78adc38a58975882936670
Kiryl Kurylenka <c.s.2015@mail.ru>
2025-02-28 09:44:03 +0300

    TWINS-307: rework factory multiplier

--- core/src/main/java/org/twins/core/mappers/rest/factory/FactoryMultiplierUpdateDTOReverseMapper.java
+++ core/src/main/java/org/twins/core/mappers/rest/factory/FactoryMultiplierUpdateDTOReverseMapper.java
@@ -3,17 +3,18 @@ package org.twins.core.mappers.rest.factory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.twins.core.dao.factory.TwinFactoryMultiplierEntity;
+import org.twins.core.dto.rest.factory.FactoryMultiplierUpdateDTOv1;
 import org.twins.core.dto.rest.factory.FactoryMultiplierUpdateRqDTOv1;
 import org.twins.core.mappers.rest.RestSimpleDTOMapper;
 import org.twins.core.mappers.rest.mappercontext.MapperContext;
 
 @Component
 @RequiredArgsConstructor
-public class FactoryMultiplierUpdateDTOReverseMapper extends RestSimpleDTOMapper<FactoryMultiplierUpdateRqDTOv1, TwinFactoryMultiplierEntity> {
+public class FactoryMultiplierUpdateDTOReverseMapper extends RestSimpleDTOMapper<FactoryMultiplierUpdateDTOv1, TwinFactoryMultiplierEntity> {
     private final FactoryMultiplierSaveDTOReverseMapper factoryMultiplierSaveDTOReverseMapper;
 
     @Override
-    public void map(FactoryMultiplierUpdateRqDTOv1 src, TwinFactoryMultiplierEntity dst, MapperContext mapperContext) throws Exception {
-        factoryMultiplierSaveDTOReverseMapper.map(src.getFactoryMultiplier(), dst, mapperContext);
+    public void map(FactoryMultiplierUpdateDTOv1 src, TwinFactoryMultiplierEntity dst, MapperContext mapperContext) throws Exception {
+        factoryMultiplierSaveDTOReverseMapper.map(src, dst, mapperContext);
     }
 }
