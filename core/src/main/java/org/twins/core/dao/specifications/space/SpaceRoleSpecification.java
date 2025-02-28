bdc08914a9dc84f6d9e89e3e7969afb9d9a752d1
Aleksey Turkov <alextur94@gmail.com>
2025-02-28 15:01:54 +0300

    TWINS-320: refactoring code

--- core/src/main/java/org/twins/core/dao/specifications/space/SpaceRoleSpecification.java
+++ core/src/main/java/org/twins/core/dao/specifications/space/SpaceRoleSpecification.java
@@ -1,17 +1,8 @@
 package org.twins.core.dao.specifications.space;
 
-import jakarta.persistence.criteria.JoinType;
-import org.springframework.data.jpa.domain.Specification;
 import org.twins.core.dao.permission.PermissionEntity;
-import org.twins.core.dao.permission.PermissionGroupEntity;
 import org.twins.core.dao.specifications.CommonSpecification;
-
-import java.util.UUID;
 
 
 public class SpaceRoleSpecification extends CommonSpecification<PermissionEntity> {
-
-    public static Specification<PermissionEntity> checkDomainId(UUID domainId) {
-        return (root, query, cb) -> createPredicateWithJoins(root, cb, domainId, (property, criteriaBuilder, filedValue) -> criteriaBuilder.or(criteriaBuilder.isNull(property), criteriaBuilder.equal(property, filedValue)), JoinType.INNER, PermissionEntity.Fields.permissionGroup, PermissionGroupEntity.Fields.domainId);
-    }
 }
