4b415ba1a0a02ae7a87eced2a970a37f27cf7f0f
Aleksey Turkov <74702834+alextur94@users.noreply.github.com>
2025-03-04 16:45:46 +0300

    Twins 320 (#412)
    * TWINS-320: creating search space role api
    
    * TWINS-320: refactoring code
    
    * TWINS-320: refactoring code

---
+++ core/src/main/java/org/twins/core/dao/specifications/space/SpaceRoleSpecification.java
@@ -0,0 +1,8 @@
+package org.twins.core.dao.specifications.space;
+
+import org.twins.core.dao.permission.PermissionEntity;
+import org.twins.core.dao.specifications.CommonSpecification;
+
+
+public class SpaceRoleSpecification extends CommonSpecification<PermissionEntity> {
+}
