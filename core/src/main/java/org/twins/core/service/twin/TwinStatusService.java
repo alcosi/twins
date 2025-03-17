26dfb771896fcff2bc8357420e2ba6221c6af1bc
nikita.mekh <n.mekh@hutkigrosh.by>
2025-03-17 18:19:40 +0300

    TWINS-330: evict fixes

--- core/src/main/java/org/twins/core/service/twin/TwinStatusService.java
+++ core/src/main/java/org/twins/core/service/twin/TwinStatusService.java
@@ -7,6 +7,7 @@ import org.cambium.common.exception.ServiceException;
 import org.cambium.common.kit.Kit;
 import org.cambium.common.util.ChangesHelper;
 import org.cambium.common.util.KeyUtils;
+import org.cambium.common.util.KitUtils;
 import org.cambium.i18n.dao.I18nEntity;
 import org.cambium.i18n.dao.I18nType;
 import org.cambium.i18n.service.I18nService;
@@ -24,7 +25,6 @@ import org.twins.core.dao.twin.TwinStatusRepository;
 import org.twins.core.dao.twinclass.TwinClassEntity;
 import org.twins.core.dao.twinclass.TwinClassRepository;
 import org.twins.core.exception.ErrorCodeTwins;
-import org.twins.core.service.auth.AuthService;
 import org.twins.core.service.twinclass.TwinClassService;
 
 import java.util.*;
@@ -132,20 +132,31 @@ public class TwinStatusService extends EntitySecureFindServiceImpl<TwinStatusEnt
                 .setNameI18nId(i18nService.createI18nAndDefaultTranslation(I18nType.TWIN_STATUS_NAME, nameInDefaultLocale).getId());
         validateEntityAndThrow(twinStatusEntity, EntitySmartService.EntityValidateMode.beforeSave);
         TwinStatusEntity savedStatus = entitySmartService.save(twinStatusEntity, twinStatusRepository, EntitySmartService.SaveMode.saveAndThrowOnException);
-        evictCache(cacheManager, TwinClassRepository.CACHE_TWIN_CLASS_BY_ID, savedStatus.getTwinClassId());
+        evictClassesCache(twinClassEntity);
         return savedStatus;
     }
 
     @Transactional(rollbackFor = Throwable.class)
     public TwinStatusEntity createStatus(TwinStatusEntity twinStatusEntity, I18nEntity nameI18n, I18nEntity descriptionsI18n) throws ServiceException {
+        TwinClassEntity twinClassEntity = twinClassService.findEntitySafe(twinStatusEntity.getTwinClassId());
         twinStatusEntity
                 .setKey(KeyUtils.lowerCaseNullSafe(twinStatusEntity.getKey(), ErrorCodeTwins.TWIN_STATUS_KEY_INCORRECT))
                 .setNameI18nId(i18nService.createI18nAndTranslations(I18nType.TWIN_STATUS_NAME, nameI18n).getId())
                 .setDescriptionI18nId(i18nService.createI18nAndTranslations(I18nType.TWIN_STATUS_DESCRIPTION, descriptionsI18n).getId());
         validateEntityAndThrow(twinStatusEntity, EntitySmartService.EntityValidateMode.beforeSave);
         TwinStatusEntity savedStatus = entitySmartService.save(twinStatusEntity, twinStatusRepository, EntitySmartService.SaveMode.saveAndThrowOnException);
-        evictCache(cacheManager, TwinClassRepository.CACHE_TWIN_CLASS_BY_ID, savedStatus.getTwinClassId());
+        evictClassesCache(twinClassEntity);
         return savedStatus;
+    }
+
+    private void evictClassesCache(TwinClassEntity twinClassEntity) throws ServiceException {
+        evictCache(cacheManager, TwinClassRepository.CACHE_TWIN_CLASS_BY_ID, twinClassEntity.getId());
+        twinClassService.loadExtendsHierarchyChildClasses(twinClassEntity);
+        if (KitUtils.isEmpty(twinClassEntity.getExtendsHierarchyChildClassKit()))
+            return;
+        for (var childClass : twinClassEntity.getExtendsHierarchyChildClassKit()) {
+            evictCache(cacheManager, TwinClassRepository.CACHE_TWIN_CLASS_BY_ID, childClass.getId());
+        }
     }
 
     @Transactional(rollbackFor = Throwable.class)
@@ -176,7 +187,7 @@ public class TwinStatusService extends EntitySecureFindServiceImpl<TwinStatusEnt
         }
         dbEntity = updateSafe(dbEntity, changesHelper);
         if (changesHelper.hasChanges()) {
-            evictCache(cacheManager, TwinClassRepository.CACHE_TWIN_CLASS_BY_ID, dbEntity.getTwinClassId());
+            evictCache(cacheManager, TwinClassRepository.CACHE_TWIN_CLASS_BY_ID, dbEntity.getTwinClassId()); //todo also inherited
         }
         return dbEntity;
     }
