05595656e7b04536caa4f82b117994e6b98693d3
Kiryl Kurylenka <c.s.2015@mail.ru>
2025-03-17 16:36:35 +0300

    TWINS-316: resolve merge conflict 2

--- core/src/main/java/org/twins/face/mappers/rest/widget/FaceWT001RestDTOMapper.java
+++ core/src/main/java/org/twins/face/mappers/rest/widget/FaceWT001RestDTOMapper.java
@@ -1,12 +1,12 @@
 package org.twins.face.mappers.rest.widget;
 
 import lombok.RequiredArgsConstructor;
-import org.cambium.i18n.service.I18nService;
 import org.springframework.stereotype.Component;
 import org.twins.core.mappers.rest.RestSimpleDTOMapper;
 import org.twins.core.mappers.rest.face.FaceRestDTOMapper;
 import org.twins.core.mappers.rest.mappercontext.MapperContext;
 import org.twins.core.mappers.rest.mappercontext.modes.FaceMode;
+import org.twins.core.service.i18n.I18nService;
 import org.twins.face.dao.widget.FaceWT001Entity;
 import org.twins.face.dto.rest.widget.FaceWT001DTOv1;
 
