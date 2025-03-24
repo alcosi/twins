38d16470ef34d6c0d72e461c3217015b93bd0ea0
nikita.mekh <n.mekh@hutkigrosh.by>
2025-03-24 18:34:46 +0300

    TWINS-353: WT003 support: simple twin image gallery

--- core/src/main/resources/db/migration_backlog/V1.3.x.xx__TWINS-353_nm1_twin_class_face.sql
+++ core/src/main/resources/db/migration_backlog/V1.3.x.xx__TWINS-353_nm1_twin_class_face.sql
@@ -6,3 +6,22 @@ alter table twin_class
             on update cascade on delete restrict;
 
 
+create table if not exists face_widget_wt003
+(
+    face_id                     uuid    not null
+        constraint face_widget_wt003_face_id_fk
+            primary key
+        references face
+            on update cascade on delete restrict,
+    key                     varchar not null,
+    label_i18n_id               uuid not null
+        constraint face_widget_wt003_label_i18n_id_fk
+            references i18n
+            on update cascade,
+    images_twin_class_field_id               uuid not null
+        constraint face_widget_wt003_twin_class_id_fk
+            references twin_class_field
+            on update cascade
+);
+
+insert into face_component values ('WT003', 'WIDGET', 'Twins image gallery')  on conflict do nothing ;
