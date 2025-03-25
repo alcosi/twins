ae1f64374d080891e6e79059f3f5d43465f6d3f4
Alexander Kuzniatsou <a.kuzniatsou@alcosi.eu>
2025-03-25 18:05:09 +0300

    TWINS-165: [search] by fields values(fix swagger errors and jackson serialization).

--- core/src/main/java/org/twins/core/dto/rest/twin/TwinFieldSearchDateDTOv1.java
+++ core/src/main/java/org/twins/core/dto/rest/twin/TwinFieldSearchDateDTOv1.java
@@ -1,30 +1,36 @@
 package org.twins.core.dto.rest.twin;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
 
 import java.time.LocalDateTime;
 
+import static org.twins.core.dto.rest.twin.TwinFieldSearchDateDTOv1.KEY;
+
+@EqualsAndHashCode(callSuper = false)
 @Data
 @Accessors(fluent = true)
-@Schema(name = "TwinFieldSearchDateV1", description = "(less & more connected with AND) and after connected to equals with OR and to emty with OR")
-public class TwinFieldSearchDateDTOv1 implements TwinFieldSearchDTOv1 {
+@Schema(name = KEY, description = "(less & more connected with AND) and after connected to equals with OR and to emty with OR")
+public class TwinFieldSearchDateDTOv1 extends TwinFieldSearchDTOv1 {
 
     public static final String KEY = "searchDateValueV1";
 
-    private String type = KEY;
+    @JsonProperty("type")
+    public String type = KEY;
 
     @Schema(description = "Twin field date less then given date")
-    public LocalDateTime dateLessThen;
+    public LocalDateTime lessThen;
 
     @Schema(description = "Twin field date greater then given date")
-    public LocalDateTime dateMoreThen;
+    public LocalDateTime moreThen;
 
     @Schema(description = "Twin field date equals to given date")
-    public LocalDateTime dateEquals;
+    public LocalDateTime equals;
 
     @Schema(description = "include entities with empty or null values to result")
-    public boolean includeEmptyDates;
+    public boolean empty;
 
 }
--- core/src/main/java/org/twins/core/dto/rest/twin/TwinFieldSearchNumericDTOv1.java
+++ core/src/main/java/org/twins/core/dto/rest/twin/TwinFieldSearchNumericDTOv1.java
@@ -1,17 +1,23 @@
 package org.twins.core.dto.rest.twin;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
 
+import static org.twins.core.dto.rest.twin.TwinFieldSearchNumericDTOv1.KEY;
+
 @Data
+@EqualsAndHashCode(callSuper = false)
 @Accessors(fluent = true)
-@Schema(name = "TwinFieldSearchNumericV1", description = "(less & more connected with AND) and after connected to equals with OR")
-public class TwinFieldSearchNumericDTOv1 implements TwinFieldSearchDTOv1 {
+@Schema(name = KEY, description = "(less & more connected with AND) and after connected to equals with OR")
+public class TwinFieldSearchNumericDTOv1 extends TwinFieldSearchDTOv1 {
 
-    public static final String KEY = "searchNumberValueV1";
+    public static final String KEY = "searchNumericValueV1";
 
-    private String type = KEY;
+    @JsonProperty("type")
+    public String type = KEY;
 
     @Schema(description = "Twin field numeric value less then given")
     public String lessThen;
