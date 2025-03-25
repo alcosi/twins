cc346e659403819628f3687eff32dfb46e3bcee1
Alexander Kuzniatsou <a.kuzniatsou@alcosi.eu>
2025-03-25 12:19:12 +0300

    move migration fix

--- core/src/main/resources/db/migration/V1.3.199.03__TWINS-282_nm2_resource_storage_fk_cascade.sql
+++ core/src/main/resources/db/migration/V1.3.199.03__TWINS-282_nm2_resource_storage_fk_cascade.sql
@@ -6,4 +6,4 @@ alter table public.resource
         on update cascade;
 
 
-INSERT INTO public.storage (id, domain_id, storager_featurer_id, storager_params, description, created_at, updated_at) VALUES ('00000000-0000-0000-0007-000000000002', null, 2903, 'selfHostDomainBaseUri => /, supportedMimeTypes => "*/ico,*/icns,*/ico,*/svg,*/svg+xml,*/webp,*/png,*/gif,*/jpeg,*/jpg,*/jpeg-lossless", fileSizeLimit => 1000000', 'External url storage', '2025-02-07 10:02:47.844931', '2025-02-07 10:02:47.844931');
+INSERT INTO public.storage (id, domain_id, storager_featurer_id, storager_params, description, created_at, updated_at) VALUES ('00000000-0000-0000-0007-000000000002', null, 2903, 'selfHostDomainBaseUri => /, supportedMimeTypes => "*/ico,*/icns,*/ico,*/svg,*/svg+xml,*/webp,*/png,*/gif,*/jpeg,*/jpg,*/jpeg-lossless", fileSizeLimit => 1000000', 'External url storage', '2025-02-07 10:02:47.844931', '2025-02-07 10:02:47.844931') on conflict (id) do nothing
