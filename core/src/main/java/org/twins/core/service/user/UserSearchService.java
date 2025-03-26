5adefb7818e49e5b373c8978e9631fa78b5a6773
Kiryl Kurylenka <c.s.2015@mail.ru>
2025-03-26 12:05:17 +0300

    TWINS-164: update

--- core/src/main/java/org/twins/core/service/user/UserSearchService.java
+++ core/src/main/java/org/twins/core/service/user/UserSearchService.java
@@ -1,5 +1,9 @@
 package org.twins.core.service.user;
 
+import jakarta.persistence.criteria.Expression;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
+import jakarta.persistence.criteria.Subquery;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.cambium.common.exception.ServiceException;
@@ -9,10 +13,15 @@ import org.cambium.common.util.PaginationUtils;
 import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
+import org.twins.core.dao.twin.TwinEntity;
 import org.twins.core.dao.user.UserEntity;
 import org.twins.core.dao.user.UserRepository;
+import org.twins.core.domain.search.BasicSearch;
 import org.twins.core.domain.search.UserSearch;
+import org.twins.core.dto.rest.twin.TwinSearchDTOv1;
+import org.twins.core.mappers.rest.twin.TwinSearchDTOReverseMapper;
 import org.twins.core.service.auth.AuthService;
+import org.twins.core.service.twin.TwinSearchService;
 
 import java.util.UUID;
 
@@ -24,11 +33,52 @@ import static org.twins.core.dao.user.UserSpecification.*;
 public class UserSearchService {
     private final AuthService authService;
     private final UserRepository userRepository;
+    private final TwinSearchService twinSearchService;
+    private final TwinSearchDTOReverseMapper twinSearchDTOReverseMapper;
 
     public PaginationResult<UserEntity> findUsers(UserSearch search, SimplePagination pagination) throws ServiceException {
-        Specification<UserEntity> spec = createUserSpecification(search);
-        Page<UserEntity> ret = userRepository.findAll(spec, PaginationUtils.pageableOffset(pagination));
-        return PaginationUtils.convertInPaginationResult(ret, pagination);
+        Specification<UserEntity> userSpec = createUserSpecification(search);
+
+        if (search.getChildTwins() != null) {
+            Specification<UserEntity> combinedSpec = userSpec.and(createTwinSpecification(search.getChildTwins()));
+            Page<UserEntity> page = userRepository.findAll(combinedSpec, PaginationUtils.pageableOffset(pagination));
+            return PaginationUtils.convertInPaginationResult(page, pagination);
+        }
+
+        Page<UserEntity> page = userRepository.findAll(userSpec, PaginationUtils.pageableOffset(pagination));
+        return PaginationUtils.convertInPaginationResult(page, pagination);
+    }
+
+    private Specification<UserEntity> createTwinSpecification(TwinSearchDTOv1 childSearch) {
+        return (userRoot, query, cb) -> {
+            if (query == null) {
+                log.error("Null parameters in twin specification");
+                return cb.disjunction();
+            }
+
+            try {
+                Expression<UUID> userIdPath = userRoot.get(UserEntity.Fields.id);
+
+                BasicSearch childTwinsSearch = twinSearchDTOReverseMapper.convert(childSearch);
+
+                Subquery<Long> countSubquery = query.subquery(Long.class);
+                Root<TwinEntity> twinRoot = countSubquery.from(TwinEntity.class);
+
+                Predicate parentCondition = cb.equal(twinRoot.get("headTwinId"), userIdPath);
+                Predicate searchConditions = twinSearchService
+                        .createTwinEntitySearchSpecification(childTwinsSearch)
+                        .toPredicate(twinRoot, query, cb);
+
+                countSubquery.select(cb.count(twinRoot))
+                        .where(cb.and(parentCondition, searchConditions));
+
+                return cb.greaterThan(countSubquery, 0L);
+
+            } catch (Exception e) {
+                log.error("Child twins filter error", e);
+                return cb.disjunction();
+            }
+        };
     }
 
     public Specification<UserEntity> createUserSpecification(UserSearch search) throws ServiceException {
@@ -41,9 +91,7 @@ public class UserSearchService {
                 checkStatusLikeIn(search.getStatusIdList(), false),
                 checkStatusLikeIn(search.getStatusIdExcludeList(), true),
                 checkSpaceRoleLikeIn(search.getSpaceList(), false),
-                checkSpaceRoleLikeIn(search.getSpaceExcludeList(), true),
-                checkSpaceRoleGroupLikeIn(search.getSpaceList(), false),
-                checkSpaceRoleGroupLikeIn(search.getSpaceExcludeList(), true)
+                checkSpaceRoleLikeIn(search.getSpaceExcludeList(), true)
         );
     }
 }
