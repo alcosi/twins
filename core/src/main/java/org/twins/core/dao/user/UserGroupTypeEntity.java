8fb24fa93da1cb7de2a14d2df22f79d372712ba2
Alexander Kuzniatsou <a.kuzniatsou@alcosi.eu>
2025-03-31 14:29:37 +0300

    TWINS-223: systemScopeDomainManage enum fix

--- core/src/main/java/org/twins/core/dao/user/UserGroupTypeEntity.java
+++ core/src/main/java/org/twins/core/dao/user/UserGroupTypeEntity.java
@@ -40,7 +40,8 @@ public class UserGroupTypeEntity {
         domainScopeDomainManage,
         domainScopeBusinessAccountManage,
         businessAccountScopeBusinessAccountManage,
-        domainAndBusinessAccountScopeBusinessAccountManage
+        domainAndBusinessAccountScopeBusinessAccountManage,
+        systemScopeDomainManage
     }
 
 }
