e5b5f574773adaf9b545d62c2ac79d366da4f38e
Kiryl Kurylenka <158213072+KevinORIS@users.noreply.github.com>
2025-04-07 14:59:41 +0300

    TWINS-164: add user group filther (#452)
    * TWINS-164: create search api for user
    
    * TWINS-164: update
    
    * TWINS-164: fix
    
    * TWINS-164 add space role user group Filter
    
    * TWINS-164: refactor

---
+++ core/src/main/resources/db/migration_backlog/V1.3.207.01__TWINS-164_kk1_add_get_users_by_group_function.sql
@@ -0,0 +1,36 @@
+CREATE
+OR REPLACE FUNCTION get_users_by_groups(
+    p_user_group_ids UUID[],
+    p_domain_id UUID DEFAULT NULL,
+    p_business_account_id UUID DEFAULT NULL
+)
+RETURNS TABLE (user_id UUID)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+RETURN QUERY
+SELECT ugm1.user_id
+FROM user_group_map_type1 ugm1
+WHERE ugm1.user_group_id = ANY (p_user_group_ids)
+
+UNION
+
+SELECT ugm2.user_id
+FROM user_group_map_type2 ugm2
+         JOIN "user" u ON ugm2.user_id = u.id
+WHERE ugm2.user_group_id = ANY (p_user_group_ids)
+  AND (p_business_account_id IS NOT NULL)
+  AND u.business_account_id = p_business_account_id
+  AND (p_domain_id IS NULL OR u.domain_id = p_domain_id)
+
+UNION
+
+SELECT ugm3.user_id
+FROM user_group_map_type3 ugm3
+         JOIN "user" u ON ugm3.user_id = u.id
+WHERE ugm3.user_group_id = ANY (p_user_group_ids)
+  AND (p_domain_id IS NOT NULL)
+  AND u.domain_id = p_domain_id
+  AND (p_business_account_id IS NULL OR u.business_account_id = p_business_account_id);
+END;
+$$;
