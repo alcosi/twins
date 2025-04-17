edf36e5e404887c58e6ae50b21bc5feada157881
Kiryl Kurylenka <c.s.2015@mail.ru>
2025-04-17 14:27:39 +0300

    TWINS-367: update migration

--- core/src/main/resources/db/migration_backlog/V1.3.222.xx__TWINS-367_kk1_add_attachment_restriction.sql
+++ core/src/main/resources/db/migration_backlog/V1.3.222.xx__TWINS-367_kk1_add_attachment_restriction.sql
@@ -1,62 +1,82 @@
-CREATE TABLE IF NOT EXISTS twin_attachment_restriction (
-id UUID PRIMARY KEY,
-min_count INTEGER,
-max_count INTEGER,
-file_size_mb_limit INTEGER,
-file_extension_list TEXT,
-file_name_regexp TEXT
+CREATE TABLE IF NOT EXISTS twin_attachment_restriction
+(
+    id                  UUID PRIMARY KEY,
+    domain_id           UUID NOT NULL,
+    min_count           INTEGER,
+    max_count           INTEGER,
+    file_size_mb_limit  INTEGER,
+    file_extension_list TEXT,
+    file_name_regexp    TEXT
 );
 
-DO $$
+DO
+$$
     BEGIN
-        IF NOT EXISTS (
-            SELECT 1
-            FROM information_schema.columns
-            WHERE table_name = 'twin_class'
-              AND column_name = 'general_attachment_restriction_id'
-        ) THEN
-            ALTER TABLE twin_class ADD COLUMN general_attachment_restriction_id UUID;
+        IF NOT EXISTS (SELECT 1
+                       FROM information_schema.table_constraints
+                       WHERE constraint_name = 'fk_twin_attachment_restriction_domain'
+                         AND table_name = 'twin_attachment_restriction') THEN
+            ALTER TABLE twin_attachment_restriction
+                ADD CONSTRAINT fk_twin_attachment_restriction_domain
+                    FOREIGN KEY (domain_id)
+                        REFERENCES domain (id)
+                        ON DELETE CASCADE;
         END IF;
-    END $$;
+    END
+$$;
 
-DO $$
+DO
+$$
     BEGIN
-        IF NOT EXISTS (
-            SELECT 1
-            FROM information_schema.columns
-            WHERE table_name = 'twin_class'
-              AND column_name = 'comment_attachment_restriction_id'
-        ) THEN
-            ALTER TABLE twin_class ADD COLUMN comment_attachment_restriction_id UUID;
+        IF NOT EXISTS (SELECT 1
+                       FROM information_schema.columns
+                       WHERE table_name = 'twin_class'
+                         AND column_name = 'general_attachment_restriction_id') THEN
+            ALTER TABLE twin_class
+                ADD COLUMN general_attachment_restriction_id UUID;
         END IF;
-    END $$;
+    END
+$$;
 
-DO $$
+DO
+$$
     BEGIN
-        IF NOT EXISTS (
-            SELECT 1
-            FROM information_schema.table_constraints
-            WHERE constraint_name = 'fk_twin_class_general_attachment_restriction'
-              AND table_name = 'twin_class'
-        ) THEN
+        IF NOT EXISTS (SELECT 1
+                       FROM information_schema.columns
+                       WHERE table_name = 'twin_class'
+                         AND column_name = 'comment_attachment_restriction_id') THEN
+            ALTER TABLE twin_class
+                ADD COLUMN comment_attachment_restriction_id UUID;
+        END IF;
+    END
+$$;
+
+DO
+$$
+    BEGIN
+        IF NOT EXISTS (SELECT 1
+                       FROM information_schema.table_constraints
+                       WHERE constraint_name = 'fk_twin_class_general_attachment_restriction'
+                         AND table_name = 'twin_class') THEN
             ALTER TABLE twin_class
                 ADD CONSTRAINT fk_twin_class_general_attachment_restriction
                     FOREIGN KEY (general_attachment_restriction_id)
-                        REFERENCES twin_attachment_restriction(id);
+                        REFERENCES twin_attachment_restriction (id);
         END IF;
-    END $$;
+    END
+$$;
 
-DO $$
+DO
+$$
     BEGIN
-        IF NOT EXISTS (
-            SELECT 1
-            FROM information_schema.table_constraints
-            WHERE constraint_name = 'fk_twin_class_comment_attachment_restriction'
-              AND table_name = 'twin_class'
-        ) THEN
+        IF NOT EXISTS (SELECT 1
+                       FROM information_schema.table_constraints
+                       WHERE constraint_name = 'fk_twin_class_comment_attachment_restriction'
+                         AND table_name = 'twin_class') THEN
             ALTER TABLE twin_class
                 ADD CONSTRAINT fk_twin_class_comment_attachment_restriction
                     FOREIGN KEY (comment_attachment_restriction_id)
-                        REFERENCES twin_attachment_restriction(id);
+                        REFERENCES twin_attachment_restriction (id);
         END IF;
-    END $$;
+    END
+$$;
