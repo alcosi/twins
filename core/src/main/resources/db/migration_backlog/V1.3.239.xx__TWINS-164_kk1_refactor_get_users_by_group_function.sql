df5d74e21cf209a0e79fecfb6a0118f30e41119c
KevinORIS <c.s.2015@mail.ru>
2025-05-05 11:29:40 +0300

    TWINS-164: refactor function

--- core/src/main/resources/db/migration_backlog/V1.3.239.xx__TWINS-164_kk1_refactor_get_users_by_group_function.sql
+++ core/src/main/resources/db/migration_backlog/V1.3.239.xx__TWINS-164_kk1_refactor_get_users_by_group_function.sql
@@ -1,7 +1,6 @@
 DROP FUNCTION IF EXISTS get_users_by_groups(UUID, UUID, UUID);
 
 CREATE OR REPLACE FUNCTION is_user_in_group(
-
     p_user_id UUID,
     p_user_group_id UUID,
     p_domain_id UUID DEFAULT NULL,
@@ -15,20 +14,17 @@ BEGIN
     RETURN EXISTS (SELECT 1
                    FROM user_group_map_type1
                    WHERE user_group_id = p_user_group_id
-                     AND user_id = p_user_id) OR EXISTS (SELECT 1
-                                                         FROM user_group_map_type2
-                                                                  JOIN "user" u ON user_id = u.id
-                                                         WHERE user_group_id = p_user_group_id
-                                                           AND user_id = p_user_id
-                                                           AND (p_business_account_id IS NULL OR
-                                                                u.business_account_id = p_business_account_id)
-                                                           AND (p_domain_id IS NULL OR u.domain_id = p_domain_id)) OR
+                     AND user_id = p_user_id) OR
            EXISTS (SELECT 1
-                   FROM user_group_map_type3
-                            JOIN "user" u ON user_id = u.id
+                   FROM user_group_map_type2
                    WHERE user_group_id = p_user_group_id
                      AND user_id = p_user_id
-                     AND (p_domain_id IS NULL OR u.domain_id = p_domain_id)
-                     AND (p_business_account_id IS NULL OR u.business_account_id = p_business_account_id));
+                     AND (p_business_account_id IS NULL OR
+                          business_account_id = p_business_account_id)) OR
+           EXISTS (SELECT 1
+                   FROM user_group_map_type3
+                   WHERE user_group_id = p_user_group_id
+                     AND user_id = p_user_id
+                     AND (p_domain_id IS NULL OR domain_id = p_domain_id));
 END;
 $$;
