f372a9af87d72b6c648b90c0fae5b7c1f7e929b6
Kiryl Kurylenka <c.s.2015@mail.ru>
2025-05-08 16:45:54 +0300

    TWINS-164: support many child twins search

--- core/src/main/java/org/twins/core/service/user/UserSearchService.java
+++ core/src/main/java/org/twins/core/service/user/UserSearchService.java
@@ -23,6 +23,7 @@ import org.twins.core.mappers.rest.twin.TwinSearchDTOReverseMapper;
 import org.twins.core.service.auth.AuthService;
 import org.twins.core.service.twin.TwinSearchService;
 
+import java.util.Objects;
 import java.util.UUID;
 
 import static org.twins.core.dao.user.UserSpecification.*;
@@ -41,10 +42,18 @@ public class UserSearchService {
         UUID businessAccountId = authService.getApiUser().getBusinessAccountId();
         Specification<UserEntity> userSpec = createUserSpecification(search, domainId, businessAccountId);
 
-        if (search.getChildTwins() != null) {
-            Specification<UserEntity> combinedSpec = userSpec.and(createTwinSpecification(search.getChildTwins()));
-            Page<UserEntity> page = userRepository.findAll(combinedSpec, PaginationUtils.pageableOffset(pagination));
-            return PaginationUtils.convertInPaginationResult(page, pagination);
+        if (search.getChildTwins() != null && !search.getChildTwins().isEmpty()) {
+            Specification<UserEntity> twinSpec = search.getChildTwins().stream()
+                    .filter(Objects::nonNull)
+                    .map(this::createTwinSpecification)
+                    .reduce(Specification::and)
+                    .orElse(null);
+
+            if (twinSpec != null) {
+                Specification<UserEntity> combinedSpec = userSpec.and(twinSpec);
+                Page<UserEntity> page = userRepository.findAll(combinedSpec, PaginationUtils.pageableOffset(pagination));
+                return PaginationUtils.convertInPaginationResult(page, pagination);
+            }
         }
 
         Page<UserEntity> page = userRepository.findAll(userSpec, PaginationUtils.pageableOffset(pagination));
