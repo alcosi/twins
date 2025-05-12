3e069e5d25a4a8d7638de9602e09a364e344819f
Alexander Kuzniatsou <a.kuzniatsou@alcosi.eu>
2025-05-13 02:44:56 +0300

    TWINS-165: empty field search fix

--- core/src/main/java/org/twins/core/domain/search/TwinFieldSearchList.java
+++ core/src/main/java/org/twins/core/domain/search/TwinFieldSearchList.java
@@ -2,6 +2,7 @@ package org.twins.core.domain.search;
 
 import lombok.Data;
 import lombok.experimental.Accessors;
+import org.cambium.common.util.CollectionUtils;
 
 import java.util.Set;
 import java.util.UUID;
@@ -13,4 +14,12 @@ public class TwinFieldSearchList extends TwinFieldSearch {
     public Set<UUID> optionsAnyOfList;
     public Set<UUID> optionsNoAllOfList;
     public Set<UUID> optionsNoAnyOfList;
+
+    public boolean isEmptySearch() {
+        return CollectionUtils.isEmpty(optionsAllOfList) &&
+                CollectionUtils.isEmpty(optionsAnyOfList) &&
+                CollectionUtils.isEmpty(optionsNoAllOfList) &&
+                CollectionUtils.isEmpty(optionsNoAnyOfList);
+    }
+
 }
--- core/src/main/java/org/twins/core/domain/search/TwinFieldSearchText.java
+++ core/src/main/java/org/twins/core/domain/search/TwinFieldSearchText.java
@@ -2,6 +2,7 @@ package org.twins.core.domain.search;
 
 import lombok.Data;
 import lombok.experimental.Accessors;
+import org.cambium.common.util.CollectionUtils;
 
 import java.util.Set;
 
@@ -14,4 +15,10 @@ public class TwinFieldSearchText extends TwinFieldSearch {
     public Set<String> valueLikeNoAllOfList;
     public Set<String> valueLikeNoAnyOfList;
 
+    public boolean isEmptySearch() {
+        return CollectionUtils.isEmpty(valueLikeAllOfList) &&
+                CollectionUtils.isEmpty(valueLikeNoAllOfList) &&
+                CollectionUtils.isEmpty(valueLikeAnyOfList) &&
+                CollectionUtils.isEmpty(valueLikeNoAnyOfList);
+    }
 }
