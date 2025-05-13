333108efdf9f5abaf81901060fba8cd6411b6268
Kiryl Kurylenka <158213072+KevinORIS@users.noreply.github.com>
2025-05-13 10:37:46 +0300

    TWINS-164: add possibility to search users by email (#504)

--- core/src/main/java/org/twins/core/domain/search/UserSearch.java
+++ core/src/main/java/org/twins/core/domain/search/UserSearch.java
@@ -1,5 +1,6 @@
 package org.twins.core.domain.search;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.experimental.Accessors;
 import lombok.experimental.FieldNameConstants;
@@ -17,6 +18,8 @@ public class UserSearch {
     private Set<UUID> userIdExcludeList;
     private Set<String> userNameLikeList;
     private Set<String> userNameLikeExcludeList;
+    private Set<String> userEmailLikeList;
+    private Set<String> userEmailLikeExcludeList;
     private Set<UserStatus> statusIdList;
     private Set<UserStatus> statusIdExcludeList;
     private List<SpaceSearch> spaceList;
