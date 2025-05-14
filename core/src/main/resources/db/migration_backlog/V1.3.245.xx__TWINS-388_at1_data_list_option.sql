398de2b55c67b4b7a33d316a81ca340ea0fe9657
Turkov.Aleksey <alextur94@gmail.com>
2025-05-14 10:35:35 +0300

    TWINS-388: add default value for migration

--- core/src/main/resources/db/migration_backlog/V1.3.245.xx__TWINS-388_at1_data_list_option.sql
+++ core/src/main/resources/db/migration_backlog/V1.3.245.xx__TWINS-388_at1_data_list_option.sql
@@ -1,5 +1,5 @@
 alter table data_list_option
-    add if not exists background_color varchar(10);
+    add if not exists background_color varchar(10) default null;
 
 alter table data_list_option
-    add if not exists font_color varchar(10);
+    add if not exists font_color varchar(10) default null;
