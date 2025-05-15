a416c8ab8ba364b012ebc20b73c28018ee895be8
Turkov.Aleksey <alextur94@gmail.com>
2025-05-15 23:17:13 +0300

    TWINS-374: create/update/search API

--- core/src/main/java/org/twins/core/dto/rest/twinflow/TwinflowTransitionBaseDTOv3.java
+++ core/src/main/java/org/twins/core/dto/rest/twinflow/TwinflowTransitionBaseDTOv3.java
@@ -2,19 +2,17 @@ package org.twins.core.dto.rest.twinflow;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
 import org.twins.core.dto.rest.validator.TransitionValidatorRuleBaseDTOv1;
 
 import java.util.List;
 
 @Data
+@EqualsAndHashCode(callSuper = true)
 @Accessors(chain = true)
 @Schema(name =  "TwinflowTransitionBaseV3")
 public class TwinflowTransitionBaseDTOv3 extends TwinflowTransitionBaseDTOv2 {
-
     @Schema(description = "validators")
     public List<TransitionValidatorRuleBaseDTOv1> validatorRules;
-
-    @Schema(description = "triggers")
-    public List<TriggerDTOv1> triggers;
 }
---
+++ core/src/main/java/org/twins/core/mappers/rest/mappercontext/modes/TransitionTriggerMode.java
@@ -0,0 +1,38 @@
+package org.twins.core.mappers.rest.mappercontext.modes;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.experimental.FieldNameConstants;
+import org.twins.core.mappers.rest.mappercontext.MapperMode;
+import org.twins.core.mappers.rest.mappercontext.MapperModePointer;
+
+@Getter
+@AllArgsConstructor
+@FieldNameConstants(onlyExplicitlyIncluded = true)
+public enum TransitionTriggerMode implements MapperMode {
+    @FieldNameConstants.Include HIDE(0),
+    @FieldNameConstants.Include SHORT(1),
+    @FieldNameConstants.Include DETAILED(2);
+
+    final int priority;
+
+    @Getter
+    @AllArgsConstructor
+    @FieldNameConstants(onlyExplicitlyIncluded = true)
+    public enum qqq implements MapperModePointer<TransitionTriggerMode> {
+        @FieldNameConstants.Include HIDE(0),
+        @FieldNameConstants.Include SHORT(1),
+        @FieldNameConstants.Include DETAILED(2);
+
+        final int priority;
+
+        @Override
+        public TransitionTriggerMode point() {
+            return switch (this) {
+                case HIDE -> TransitionTriggerMode.HIDE;
+                case SHORT -> TransitionTriggerMode.SHORT;
+                case DETAILED -> TransitionTriggerMode.DETAILED;
+            };
+        }
+    }
+}
