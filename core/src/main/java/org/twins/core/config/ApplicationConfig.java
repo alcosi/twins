19bfcfa6ada90d24a38ceb985b3369f4c4beca48
BreninSul <42633769+BreninSul@users.noreply.github.com>
2025-05-15 15:34:05 +0400

    TWINS-314: UncaughtExceptionFilter added, new error code and Aspect refactoring

--- core/src/main/java/org/twins/core/config/ApplicationConfig.java
+++ core/src/main/java/org/twins/core/config/ApplicationConfig.java
@@ -8,7 +8,7 @@
 package org.twins.core.config;
 
 
-
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.benmanes.caffeine.cache.Caffeine;
 import io.micrometer.core.instrument.MeterRegistry;
 import lombok.extern.slf4j.Slf4j;
@@ -16,9 +16,7 @@ import org.cambium.service.EntitySmartService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-
 import org.springframework.cache.CacheManager;
-
 import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
@@ -35,6 +33,7 @@ import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.client.RestTemplate;
 import org.twins.core.config.filter.LoggingFilter;
+import org.twins.core.config.filter.UncaughtExceptionFilter;
 
 import javax.sql.DataSource;
 import java.util.concurrent.TimeUnit;
@@ -81,6 +80,11 @@ public class ApplicationConfig {
         return new LoggingFilter();
     }
 
+    @Order(2)
+    @Bean(name = "uncaughtExceptionFilter", value = "uncaughtExceptionFilter")
+    public UncaughtExceptionFilter uncaughtExceptionFilter(UncaughtExceptionFilter.LoggingController controller, ObjectMapper objectMapper) {
+        return new UncaughtExceptionFilter(controller, objectMapper);
+    }
     /**
      * Configures a MeterRegistry with common tags applied to all metrics.
      * This method customizes the MeterRegistry by adding a common tag
--- core/src/main/java/org/twins/core/config/ProtectedByAspect.java
+++ core/src/main/java/org/twins/core/config/ProtectedByAspect.java
@@ -7,6 +7,7 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.cambium.common.exception.ServiceException;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Component;
 import org.twins.core.controller.rest.annotation.ProtectedBy;
 import org.twins.core.exception.ErrorCodeTwins;
@@ -14,50 +15,58 @@ import org.twins.core.service.permission.PermissionService;
 import org.twins.core.service.permission.Permissions;
 
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Aspect
 @Component
 @Slf4j
 @RequiredArgsConstructor
 public class ProtectedByAspect {
-
     private final PermissionService permissionService;
 
     @Around("@annotation(org.twins.core.controller.rest.annotation.ProtectedBy) || @within(org.twins.core.controller.rest.annotation.ProtectedBy)")
     public Object checkPermission(ProceedingJoinPoint joinPoint) throws Throwable {
         MethodSignature signature = (MethodSignature) joinPoint.getSignature();
         Method method = signature.getMethod();
+        ProtectedBy annotation = resolveAnnotation(method);
+        if (annotation != null) {
+            List<Permissions> requiredButAbsentPermissions = getRequiredButAbsentPermissions(annotation.value());
+            if (!requiredButAbsentPermissions.isEmpty()) {
+                String noPermissionNames = requiredButAbsentPermissions.stream().map(Permissions::name).collect(Collectors.joining(","));
+                log.warn("Access denied: User does not have any of the required permissions {} for {}", noPermissionNames, method);
+                throw new ServiceException(ErrorCodeTwins.NO_REQUIRED_PERMISSION, "User does not have required permissions [" + noPermissionNames + "]");
+            }
+        }
+        return joinPoint.proceed();
+    }
 
+    @NotNull
+    private List<Permissions> getRequiredButAbsentPermissions(Permissions[] requiredPermissions) throws ServiceException {
+        if (requiredPermissions == null || requiredPermissions.length == 0) {
+            return new ArrayList<>();
+        }
+        List<Permissions> requiredButAbsentPermissions = new ArrayList<>();
+        try {
+            for (Permissions permission : requiredPermissions) {
+                if (!permissionService.currentUserHasPermission(permission)) {
+                    requiredButAbsentPermissions.add(permission);
+                }
+            }
+        } catch (Throwable e) {
+            log.error("Error checking permissions", e);
+            throw e;
+        }
+        return requiredButAbsentPermissions;
+    }
+
+    protected ProtectedBy resolveAnnotation(Method method) {
         // Check for method-level annotation
         ProtectedBy methodAnnotation = method.getAnnotation(ProtectedBy.class);
         // If not found, check for class-level annotation
         ProtectedBy classAnnotation = method.getDeclaringClass().getAnnotation(ProtectedBy.class);
-
         ProtectedBy annotation = methodAnnotation != null ? methodAnnotation : classAnnotation;
-
-        if (annotation != null) {
-            Permissions[] requiredPermissions = annotation.value();
-
-            // Check if the user has any of the required permissions
-            boolean hasPermission = false;
-            for (Permissions permission : requiredPermissions) {
-                try {
-                    if (permissionService.currentUserHasPermission(permission)) {
-                        hasPermission = true;
-                        break;
-                    }
-                } catch (ServiceException e) {
-                    log.error("Error checking permission: {}", permission, e);
-                    throw e;
-                }
-            }
-
-            if (!hasPermission) {
-                log.warn("Access denied: User does not have any of the required permissions for {}", method);
-                throw new ServiceException(ErrorCodeTwins.DOMAIN_PERMISSION_DENIED, "User does not have any of the required permissions");
-            }
-        }
-
-        return joinPoint.proceed();
+        return annotation;
     }
 }
