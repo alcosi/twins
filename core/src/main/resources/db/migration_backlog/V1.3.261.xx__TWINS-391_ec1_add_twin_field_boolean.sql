02d6e02d296bf26cbc0b64fe53eaa8a452ade74d
Egor Chernooky <e.chernooky@gmail.com>
2025-06-06 16:48:15 +0300

    TWINS-391: added index and migration for boolean data

--- core/src/main/resources/db/migration_backlog/V1.3.261.xx__TWINS-391_ec1_add_twin_field_boolean.sql
+++ core/src/main/resources/db/migration_backlog/V1.3.261.xx__TWINS-391_ec1_add_twin_field_boolean.sql
@@ -3,7 +3,8 @@ CREATE TABLE IF NOT EXISTS twin_field_boolean
     id                  UUID PRIMARY KEY,
     twin_id             UUID    NOT NULL REFERENCES twin (id) ON UPDATE CASCADE ON DELETE CASCADE,
     twin_class_field_id UUID    NOT NULL REFERENCES twin_class_field (id) ON UPDATE CASCADE ON DELETE CASCADE,
-    value               BOOLEAN NOT NULL
+    value               BOOLEAN
 );
 
 CREATE INDEX IF NOT EXISTS idx_twin_field_boolean_field_value ON twin_field_boolean (twin_class_field_id, value);
+CREATE INDEX IF NOT EXISTS idx_twin_field_boolean_field_value ON twin_field_boolean (twin_class_field_id, twin_id);
