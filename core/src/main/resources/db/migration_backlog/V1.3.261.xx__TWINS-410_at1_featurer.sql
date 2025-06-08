4eb3550eaa66e4cfa76041f6cb3ab466b694a868
Turkov.Aleksey <alextur94@gmail.com>
2025-06-09 01:15:52 +0300

    TWINS-410: drop extra file

--- core/src/main/resources/db/migration_backlog/V1.3.261.xx__TWINS-410_at1_featurer.sql
+++
@@ -1,2 +0,0 @@
--- new featurer FillerFieldUserFromApiUser
-INSERT INTO featurer (id, featurer_type_id, class, name, description, deprecated) VALUES (2338, 23, 'org.twins.core.featurer.factory.filler.FillerFieldUserFromApiUser', 'Basics assignee from api user', '', false) on conflict on constraint featurer_pk do nothing ;
