44ee7c19b87ab22110c2c5826f27d28000d28260
Turkov.Aleksey <alextur94@gmail.com>
2025-06-09 10:06:04 +0300

    TWINS-410: refactoring migration

--- core/src/main/resources/db/migration_backlog/V1.3.266.xx__TWINS-410_at1_twin_field_simple_and_user.sql
+++ core/src/main/resources/db/migration_backlog/V1.3.266.xx__TWINS-410_at1_twin_field_simple_and_user.sql
@@ -1,10 +1,10 @@
--- change name for index for table twin_field_simple
-alter index if exists twin_field_simple_twin_id_twin_class_field_id_uindex rename to twin_id_twin_class_field_id_uindex;
+-- change index name
+alter index if exists field_id_value_uindex rename to twin_field_simple_twin_class_field_id_value_uindex;
 
 -- change index for twin_field_user
 drop index if exists twin_field_user_twin_class_field_id_index;
 
-drop index if exists twin_class_field_id_user_id_index;
+drop index if exists twin_field_user_twin_class_field_id_user_id_index;
 
-create index twin_class_field_id_user_id_index
+create index twin_field_user_twin_class_field_id_user_id_index
     on twin_field_user (twin_class_field_id, user_id);
