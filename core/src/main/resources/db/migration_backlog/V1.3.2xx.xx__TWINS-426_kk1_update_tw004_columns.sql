aa7de4bd56f9766f04849c1c56a716ef8602b8e6
KevinORIS <c.s.2015@mail.ru>
2025-06-18 13:35:09 +0300

    TWINS-426: reverse branch

---
+++ core/src/main/resources/db/migration_backlog/V1.3.2xx.xx__TWINS-426_kk1_update_tw004_columns.sql
@@ -0,0 +1,20 @@
+DO $$
+BEGIN
+        IF EXISTS (
+            SELECT 1 FROM information_schema.columns
+            WHERE table_name = 'face_twidget_tw004'
+              AND column_name = 'field_filter_featurer_id'
+        ) THEN
+ALTER TABLE face_twidget_tw004
+    RENAME COLUMN field_filter_featurer_id TO editable_field_filter_featurer_id;
+END IF;
+
+        IF EXISTS (
+            SELECT 1 FROM information_schema.columns
+            WHERE table_name = 'face_twidget_tw004'
+              AND column_name = 'field_filter_params'
+        ) THEN
+ALTER TABLE face_twidget_tw004
+    RENAME COLUMN field_filter_params TO editable_field_filter_params;
+END IF;
+END $$;
