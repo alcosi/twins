49dd02d50c68e973bf74660ff5c3a3a19dacb4bb
Turkov.Aleksey <alextur94@gmail.com>
2025-06-26 10:46:16 +0300

    TWINS-372: rollback changes (deleted annotation)

--- core/src/main/java/org/twins/core/controller/rest/priv/usergroup/UserGroupMemberManageController.java
+++ core/src/main/java/org/twins/core/controller/rest/priv/usergroup/UserGroupMemberManageController.java
@@ -17,11 +17,13 @@ import org.twins.core.controller.rest.ApiController;
 import org.twins.core.controller.rest.ApiTag;
 import org.twins.core.controller.rest.annotation.MapperContextBinding;
 import org.twins.core.controller.rest.annotation.ParametersApiUserHeaders;
+import org.twins.core.controller.rest.annotation.ProtectedBy;
 import org.twins.core.dto.rest.DTOExamples;
 import org.twins.core.dto.rest.usergroup.UserGroupListRsDTOv1;
 import org.twins.core.dto.rest.usergroup.UserGroupMemberManageRqDTOv1;
 import org.twins.core.mappers.rest.mappercontext.MapperContext;
 import org.twins.core.mappers.rest.usergroup.UserGroupRestDTOMapper;
+import org.twins.core.service.permission.Permissions;
 import org.twins.core.service.user.UserGroupService;
 import org.twins.core.service.user.UserService;
 
@@ -31,6 +33,7 @@ import java.util.UUID;
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RequiredArgsConstructor
+@ProtectedBy({Permissions.USER_GROUP_MANAGE, Permissions.USER_GROUP_UPDATE})
 public class UserGroupMemberManageController extends ApiController {
     private final UserGroupRestDTOMapper userGroupDTOMapper;
     private final UserGroupService userGroupService;
