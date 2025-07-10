982b463df747ac39475085909f701098a8c3a1f9
Turkov.Aleksey <alextur94@gmail.com>
2025-07-10 11:23:47 +0300

    TWINS-367: refactoring code 2

--- core/src/main/java/org/twins/core/dao/attachment/AttachmentCUDValidateResult.java
+++ core/src/main/java/org/twins/core/dao/attachment/AttachmentCUDValidateResult.java
@@ -3,10 +3,14 @@ package org.twins.core.dao.attachment;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
 @EqualsAndHashCode(callSuper = true)
 public class AttachmentCUDValidateResult extends AttachmentCreateValidateResult {
     public AttachmentCUDProblems cudProblems = new AttachmentCUDProblems();
+    public List<TwinAttachmentEntity> attachmentsForUD = new ArrayList<>();
 
     public boolean hasProblems() {
         return getCudProblems().hasProblems();
--- core/src/main/java/org/twins/core/dao/attachment/AttachmentCreateValidateResult.java
+++ core/src/main/java/org/twins/core/dao/attachment/AttachmentCreateValidateResult.java
@@ -8,7 +8,6 @@ import java.util.List;
 @Data
 public class AttachmentCreateValidateResult {
     public AttachmentCreateProblems createProblems = new AttachmentCreateProblems();
-    public List<TwinAttachmentEntity> attachmentsForUD = new ArrayList<>();
 
     public boolean hasProblems() {
         return getCreateProblems().hasProblems();
