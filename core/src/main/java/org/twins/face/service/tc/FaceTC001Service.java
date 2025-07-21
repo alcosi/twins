2c50395dfe367db0b4ddeb948ca328508291e8fe
Kiryl Kurylenka <158213072+KevinORIS@users.noreply.github.com>
2025-07-21 10:16:32 +0300

    TWINS-426: refactor tc001, delete tc002 (#604)
    * TWINS-426: refactor tc001 and tc002
    
    * TWINS-426: refactor tc001
    
    * TWINS-426: mapper fix
    
    * TWINS-426: refactor after code review

--- core/src/main/java/org/twins/face/service/tc/FaceTC001Service.java
+++ core/src/main/java/org/twins/face/service/tc/FaceTC001Service.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.cambium.common.exception.ServiceException;
 import org.cambium.common.kit.Kit;
+import org.cambium.common.kit.KitGrouped;
+import org.cambium.common.util.CollectionUtils;
 import org.cambium.featurer.FeaturerService;
 import org.cambium.service.EntitySmartService;
 import org.springframework.context.annotation.Lazy;
@@ -17,6 +19,8 @@ import org.twins.core.service.face.FaceVariantsService;
 import org.twins.core.service.twin.TwinService;
 import org.twins.core.service.twinclass.TwinClassFieldSearchService;
 import org.twins.face.dao.tc.tc001.FaceTC001Entity;
+import org.twins.face.dao.tc.tc001.FaceTC001OptionEntity;
+import org.twins.face.dao.tc.tc001.FaceTC001OptionRepository;
 import org.twins.face.dao.tc.tc001.FaceTC001Repository;
 
 import java.util.Collection;
@@ -31,10 +35,8 @@ import java.util.function.Function;
 @RequiredArgsConstructor
 public class FaceTC001Service extends FaceVariantsService<FaceTC001Entity> {
     private final FaceTC001Repository faceTC001Repository;
-    private final FeaturerService featurerService;
-    private final TwinClassFieldSearchService twinClassFieldSearchService;
-    private final TwinService twinService;
     private final FaceService faceService;
+    private final FaceTC001OptionRepository faceTC001OptionRepository;
 
     @Override
     public CrudRepository<FaceTC001Entity, UUID> entityRepository() {
@@ -59,5 +61,33 @@ public class FaceTC001Service extends FaceVariantsService<FaceTC001Entity> {
     @Override
     public List<FaceTC001Entity> getVariants(UUID of) {
         return faceTC001Repository.findByFaceId(of);
+    }
+
+    public void loadOptions(FaceTC001Entity src) {
+        loadOptions(Collections.singletonList(src));
+    }
+
+    public void loadOptions(Collection<FaceTC001Entity> srcList) {
+        if (CollectionUtils.isEmpty(srcList)) {
+            return;
+        }
+
+        Kit<FaceTC001Entity, UUID> needLoad = new Kit<>(FaceTC001Entity::getId);
+
+        for (FaceTC001Entity entity : srcList ) {
+            if (entity.getOptions() == null){
+                entity.setOptions(new Kit<>(FaceTC001OptionEntity::getId));
+                needLoad.add(entity);
+            }
+        }
+        if (needLoad.isEmpty()) {
+            return;
+        }
+
+        KitGrouped<FaceTC001OptionEntity, UUID, UUID> loadedKit = new KitGrouped<>(
+                faceTC001OptionRepository.findByFaceTC001IdIn(needLoad.getIdSet()), FaceTC001OptionEntity::getId, FaceTC001OptionEntity::getFaceTC001Id);
+        for (var entry : loadedKit.getGroupedMap().entrySet()) {
+            needLoad.get(entry.getKey()).getOptions().addAll(entry.getValue());
+        }
     }
 }
---
+++ core/src/main/resources/db/migration_backlog/V1.3xx.xx__TWINS-426_kk1_refactor_tc001.sql
@@ -0,0 +1,69 @@
+DROP TABLE IF EXISTS face_tc001;
+
+DO $$
+BEGIN
+        IF EXISTS (
+            SELECT 1 FROM information_schema.tables
+            WHERE table_name = 'face_tc002'
+        ) THEN
+            EXECUTE 'ALTER TABLE face_tc002 RENAME TO face_tc001';
+
+            IF EXISTS (
+                SELECT 1 FROM pg_constraint
+                WHERE conrelid = 'face_tc001'::regclass
+                AND conname LIKE 'face_tc002_%_fkey'
+            ) THEN
+                EXECUTE 'ALTER TABLE face_tc001 RENAME CONSTRAINT face_tc002_face_id_fkey TO face_tc001_face_id_fkey';
+                EXECUTE 'ALTER TABLE face_tc001 RENAME CONSTRAINT face_tc002_header_i18n_id_fkey TO face_tc001_header_i18n_id_fkey';
+                EXECUTE 'ALTER TABLE face_tc001 RENAME CONSTRAINT face_tc002_header_icon_resource_id_fkey TO face_tc001_header_icon_resource_id_fkey';
+                EXECUTE 'ALTER TABLE face_tc001 RENAME CONSTRAINT face_tc002_save_button_label_i18n_id_fkey TO face_tc001_save_button_label_i18n_id_fkey';
+                EXECUTE 'ALTER TABLE face_tc001 RENAME CONSTRAINT face_tc002_twin_pointer_validator_rule_id_fkey TO face_tc001_twin_pointer_validator_rule_id_fkey';
+            END IF;
+        END IF;
+
+        IF EXISTS (
+            SELECT 1 FROM information_schema.tables
+            WHERE table_name = 'face_tc002_option'
+        ) THEN
+            EXECUTE 'ALTER TABLE face_tc002_option RENAME TO face_tc001_option';
+
+            IF EXISTS (
+                SELECT 1 FROM pg_constraint
+                WHERE conrelid = 'face_tc001_option'::regclass
+                AND conname LIKE 'face_tc002_%_fkey'
+            ) THEN
+                EXECUTE 'ALTER TABLE face_tc001_option RENAME CONSTRAINT face_tc002_option_face_tc002_id_fkey TO face_tc001_option_face_tc001_id_fkey';
+                EXECUTE 'ALTER TABLE face_tc001_option RENAME CONSTRAINT face_tc002_option_class_selector_label_i18n_id_fkey TO face_tc001_option_class_selector_label_i18n_id_fkey';
+                EXECUTE 'ALTER TABLE face_tc001_option RENAME CONSTRAINT face_tc002_option_head_twin_pointer_id_fkey TO face_tc001_option_head_twin_pointer_id_fkey';
+                EXECUTE 'ALTER TABLE face_tc001_option RENAME CONSTRAINT face_tc002_option_label_i18n_id_fkey TO face_tc001_option_label_i18n_id_fkey';
+                EXECUTE 'ALTER TABLE face_tc001_option RENAME CONSTRAINT face_tc002_option_twin_class_id_fkey TO face_tc001_option_twin_class_id_fkey';
+                EXECUTE 'ALTER TABLE face_tc001_option RENAME CONSTRAINT face_tc002_option_twin_pointer_validator_rule_id_fkey TO face_tc001_option_twin_pointer_validator_rule_id_fkey';
+            END IF;
+        END IF;
+    END;
+$$;
+
+DO $$
+BEGIN
+    IF EXISTS (
+        SELECT 1
+        FROM information_schema.columns
+        WHERE table_name = 'face_tc001_option'
+        AND column_name = 'face_tc002_id'
+    ) THEN
+        EXECUTE 'ALTER TABLE face_tc001_option RENAME COLUMN face_tc002_id TO face_tc001_id';
+    END IF;
+END $$;
+
+
+ALTER TABLE face_tc001_option
+    DROP COLUMN IF EXISTS twin_class_id,
+    DROP COLUMN IF EXISTS extends_depth;
+
+ALTER TABLE face_tc001
+    ADD COLUMN IF NOT EXISTS option_select_i18n_id UUID REFERENCES i18n(id) ON DELETE SET NULL ON UPDATE CASCADE,
+    ADD COLUMN IF NOT EXISTS sketch_mode BOOLEAN,
+    ADD COLUMN IF NOT EXISTS single_option_silent_mode BOOLEAN;
+
+ALTER TABLE face_tc001_option
+    ADD COLUMN IF NOT EXISTS twin_class_search_id UUID;
