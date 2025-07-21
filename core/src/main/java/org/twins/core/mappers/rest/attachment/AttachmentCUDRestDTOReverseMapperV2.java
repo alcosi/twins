ab8e8602b597c84fa257075ee36cdffb9255077d
BreninSul <42633769+BreninSul@users.noreply.github.com>
2025-07-21 14:23:36 +0200

    TWINS-424: Add multipart support to twin transition endpoints and preprocess attachments

--- core/src/main/java/org/twins/core/mappers/rest/attachment/AttachmentCUDRestDTOReverseMapperV2.java
+++ core/src/main/java/org/twins/core/mappers/rest/attachment/AttachmentCUDRestDTOReverseMapperV2.java
@@ -1,13 +1,21 @@
 package org.twins.core.mappers.rest.attachment;
 
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
 import org.twins.core.dao.attachment.TwinAttachmentEntity;
 import org.twins.core.domain.EntityCUD;
+import org.twins.core.dto.rest.attachment.AttachmentCreateDTOv1;
 import org.twins.core.dto.rest.attachment.AttachmentCudDTOv1;
+import org.twins.core.dto.rest.attachment.AttachmentUpdateDTOv1;
 import org.twins.core.mappers.rest.RestSimpleDTOMapper;
 import org.twins.core.mappers.rest.mappercontext.MapperContext;
 import org.twins.core.service.attachment.AttachmentService;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 
 @Component
@@ -23,5 +31,13 @@ public class AttachmentCUDRestDTOReverseMapperV2 extends RestSimpleDTOMapper<Att
                 .setUpdateList(attachmentUpdateRestDTOReverseMapper.convertCollection(src.getUpdate()))
                 .setCreateList(attachmentCreateRestDTOReverseMapper.convertCollection(src.getCreate()))
                 .setDeleteList(attachmentService.findEntitiesSafe(src.getDelete()).getList());
+    }
+
+    @SneakyThrows
+    public void preProcessAttachments(AttachmentCudDTOv1 attachments, Map<String, MultipartFile> files) {
+        List<AttachmentUpdateDTOv1> updateDTOv1List = attachments == null ? new ArrayList<>() : attachments.update;
+        List<AttachmentCreateDTOv1> createDTOv1List = attachments == null ? new ArrayList<>() : attachments.create;
+        attachmentUpdateRestDTOReverseMapper.preProcessAttachments(updateDTOv1List, files);
+        attachmentCreateRestDTOReverseMapper.preProcessAttachments(createDTOv1List, files);
     }
 }
