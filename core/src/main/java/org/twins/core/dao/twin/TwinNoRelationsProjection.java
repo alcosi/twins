bd9eeffb1a66396e30f064c586ec5a7ca33dc918
Turkov.Aleksey <alextur94@gmail.com>
2025-07-24 09:01:06 +0300

    TWINS-451: extended TwinNoRelationsProjection

--- core/src/main/java/org/twins/core/dao/twin/TwinNoRelationsProjection.java
+++ core/src/main/java/org/twins/core/dao/twin/TwinNoRelationsProjection.java
@@ -7,7 +7,22 @@ import java.util.UUID;
 public interface TwinNoRelationsProjection extends EasyLoggable {
 
     UUID getId();
+    UUID getTwinClassId();
     UUID getHeadTwinId();
+    String getHierarchyTree();
+    String getExternalId();
+    UUID getOwnerBusinessAccountId();
+    UUID getOwnerUserId();
+    UUID getViewPermissionId();
+    UUID getPermissionSchemaSpaceId();
+    UUID getTwinflowSchemaSpaceId();
+    UUID getTwinClassSchemaSpaceId();
+    UUID getAliasSpaceId();
+    UUID getTwinStatusId();
+    String getName();
+    String getDescription();
+    UUID getCreatedByUserId();
+    UUID getAssignerUserId();
 
     default String easyLog(Level level) {
         return "twin[" + getId() + "]";
