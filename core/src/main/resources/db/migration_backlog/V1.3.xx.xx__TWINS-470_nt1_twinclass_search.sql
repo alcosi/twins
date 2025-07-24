d1029c45d157cbbad824d5f3f7f5e76553a89154
natallia-tselikava <natashatsel@gmail.com>
2025-07-24 14:40:14 +0200

    TWINS-470: sql script changes for twinface usage

--- core/src/main/resources/db/migration_backlog/V1.3.xx.xx__TWINS-470_nt1_twinclass_search.sql
+++ core/src/main/resources/db/migration_backlog/V1.3.xx.xx__TWINS-470_nt1_twinclass_search.sql
@@ -19,6 +19,29 @@ create index if not exists twin_class_search_predicate_index1
 create index if not exists twin_class_search_predicate_index2
     on twin_class_search_predicate (twin_class_search_id);
 
+insert into twin_class_search (id, domain_id, name) values ('00000000-0000-0000-0015-000000000001', null, 'Unlimited classes search') on conflict do nothing ;
+
+DO $$
+BEGIN
+        IF NOT EXISTS (
+            SELECT 1
+            FROM information_schema.table_constraints
+            WHERE constraint_type = 'FOREIGN KEY'
+              AND constraint_name = 'face_tc001_option_twin_class_search_id_fk'
+              AND table_name = 'face_tc001_option'
+        ) THEN
+            insert into twin_class_search (id, domain_id, name) select twin_class_search_id, face.domain_id, ''
+                                                                      from face_tc001_option, face_tc001, face where face_tc001_option.face_tc001_id = face_tc001.id and face_tc001.face_id = face.id on conflict do nothing ;
+
+ALTER TABLE face_tc001_option
+    ADD CONSTRAINT face_tc001_option_twin_class_search_id_fk
+        FOREIGN KEY (twin_class_search_id)
+            REFERENCES twin_class_search
+            ON UPDATE CASCADE
+            ON DELETE RESTRICT;
+END IF;
+END $$;
+
 
 INSERT INTO public.featurer_type VALUES (39, 'ClassFinder', 'Finds classes according to given configuration') on conflict do nothing ;
 
