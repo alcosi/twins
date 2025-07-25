43b1bcd6b2afdd9aa836ff4b59fa4c4d96a6a270
nikita.mekh <n.mekh@hutkigrosh.by>
2025-07-25 10:59:58 +0300

    TWINS-470: small migration fixes

--- core/src/main/resources/db/migration_backlog/V1.3.xx.xx__TWINS-470_nt1_twinclass_search.sql
+++ core/src/main/resources/db/migration_backlog/V1.3.xx.xx__TWINS-470_nt1_twinclass_search.sql
@@ -1,15 +1,15 @@
-create table if not exists public.twin_class_search
+create table if not exists twin_class_search
 (
     id                  uuid      not null primary key,
-    domain_id           uuid references public.domain (id) on delete cascade on update cascade,
+    domain_id           uuid references domain (id) on delete cascade on update cascade,
     name         varchar   not null
     );
 
-create table if not exists public.twin_class_search_predicate
+create table if not exists twin_class_search_predicate
 (
     id                  uuid      not null primary key,
-    twin_class_search_id           uuid references public.twin_class_search (id) on delete cascade on update cascade,
-    class_finder_featurer_id int4    not null references public.featurer (id) on delete cascade on update cascade,
+    twin_class_search_id           uuid references twin_class_search (id) on delete cascade on update cascade,
+    class_finder_featurer_id int4    not null references featurer (id) on delete cascade on update cascade,
     class_finder_params      hstore    not null             default ''::hstore
     );
 
@@ -33,21 +33,17 @@ BEGIN
             insert into twin_class_search (id, domain_id, name) select twin_class_search_id, face.domain_id, ''
                                                                       from face_tc001_option, face_tc001, face where face_tc001_option.face_tc001_id = face_tc001.id and face_tc001.face_id = face.id on conflict do nothing ;
 
-ALTER TABLE face_tc001_option
-    ADD CONSTRAINT face_tc001_option_twin_class_search_id_fk
-        FOREIGN KEY (twin_class_search_id)
-            REFERENCES twin_class_search
-            ON UPDATE CASCADE
-            ON DELETE RESTRICT;
+            ALTER TABLE face_tc001_option
+                ADD CONSTRAINT face_tc001_option_twin_class_search_id_fk
+                    FOREIGN KEY (twin_class_search_id)
+                        REFERENCES twin_class_search
+                        ON UPDATE CASCADE
+                        ON DELETE RESTRICT;
 END IF;
 END $$;
 
 
-INSERT INTO public.featurer_type VALUES (39, 'ClassFinder', 'Finds classes according to given configuration') on conflict do nothing ;
+INSERT INTO featurer_type VALUES (39, 'ClassFinder', 'Finds classes according to given configuration') on conflict do nothing ;
 
-INSERT INTO public.featurer (id, featurer_type_id, class, name, description, deprecated) VALUES (3901, 39, 'org.twins.core.featurer.classfinder.ClassFinderExtendsHierarchyChildrenOf', 'Given twin class id and extends depth', '', false) on conflict (id) do nothing;
-INSERT INTO public.featurer (id, featurer_type_id, class, name, description, deprecated) VALUES (3102, 39, 'org.twins.core.featurer.classfinder.ClassFinderGivenSet', 'Given twin class id set', '', false) on conflict (id) do nothing;
-
-INSERT INTO public.featurer_param VALUES (3901, false, 1, 'twinClassId', 'twinClassId', '', 'UUID:TWINS:TWIN_CLASS_ID',false,'','');
-INSERT INTO public.featurer_param VALUES (3901, false, 2, 'extendsDepth', 'extendsDepth', '', 'INT',false,'','');
-INSERT INTO public.featurer_param VALUES (3902, false, 1, 'twinClassIdSet', 'twinClassIdSet', '', 'UUID_SET:TWINS:TWIN_CLASS_ID',false,'','');
+INSERT INTO featurer (id, featurer_type_id, class, name, description, deprecated) VALUES (3901, 39, 'org.twins.core.featurer.classfinder.ClassFinderExtendsHierarchyChildrenOf', 'Given twin class id and extends depth', '', false) on conflict (id) do nothing;
+INSERT INTO featurer (id, featurer_type_id, class, name, description, deprecated) VALUES (3102, 39, 'org.twins.core.featurer.classfinder.ClassFinderGivenSet', 'Given twin class id set', '', false) on conflict (id) do nothing;
