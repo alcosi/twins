e931ea1db030415261a4412fce4c98516e8f7c9e
Turkov.Aleksey <alextur94@gmail.com>
2025-07-28 16:04:02 +0300

    TWINS-476: check field

--- core/src/main/java/org/twins/core/featurer/factory/filler/FillerFieldFromParams.java
+++ core/src/main/java/org/twins/core/featurer/factory/filler/FillerFieldFromParams.java
@@ -11,11 +11,12 @@ import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 import org.twins.core.dao.twin.TwinEntity;
 import org.twins.core.domain.factory.FactoryItem;
+import org.twins.core.exception.ErrorCodeTwins;
 import org.twins.core.featurer.FeaturerTwins;
 import org.twins.core.featurer.fieldtyper.value.FieldValue;
 import org.twins.core.featurer.fieldtyper.value.FieldValueText;
 import org.twins.core.featurer.params.FeaturerParamUUIDTwinsTwinClassFieldId;
-import org.twins.core.service.twinclass.TwinClassFieldService;
+import org.twins.core.service.twin.TwinService;
 
 import java.util.Properties;
 import java.util.UUID;
@@ -34,13 +35,17 @@ public class FillerFieldFromParams extends Filler {
 
     @Lazy
     @Autowired
-    private TwinClassFieldService twinClassFieldService;
+    private TwinService twinService;
 
 
     @Override
     public void fill(Properties properties, FactoryItem factoryItem, TwinEntity templateTwin) throws ServiceException {
         UUID extractedTwinClassFieldId = twinClassFieldId.extract(properties);
-        FieldValue fieldValue = new FieldValueText(twinClassFieldService.findEntitySafe(extractedTwinClassFieldId)).setValue(value.extract(properties));
+        FieldValue fieldValue = twinService.getTwinFieldValue(twinService.wrapField(factoryItem.getOutput().getTwinEntity(), extractedTwinClassFieldId));
+        if (!(fieldValue instanceof FieldValueText)) {
+            throw new ServiceException(ErrorCodeTwins.FACTORY_PIPELINE_STEP_ERROR, "twinClassField[" + extractedTwinClassFieldId + "] is not instance of text field");
+        }
+        fieldValue = new FieldValueText(fieldValue.getTwinClassField()).setValue(value.extract(properties));
         factoryItem.getOutput().addField(fieldValue);
     }
 }
