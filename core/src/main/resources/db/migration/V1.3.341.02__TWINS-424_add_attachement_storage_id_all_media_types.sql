8e621bb5ebd3b7ec6f0ee957c314e3d7326ceb3f
BreninSul <42633769+BreninSul@users.noreply.github.com>
2025-07-28 10:22:34 +0200

    TWINS-424: Update SQL migration to use `public.storage` table and standardize supported MIME types

--- core/src/main/resources/db/migration/V1.3.341.02__TWINS-424_add_attachement_storage_id_all_media_types.sql
+++ core/src/main/resources/db/migration/V1.3.341.02__TWINS-424_add_attachement_storage_id_all_media_types.sql
@@ -1,3 +1,3 @@
-update twins_ons_db.public.storage
+update public.storage
 set storager_params= storager_params || hstore('supportedMimeTypes', '*/*')
 where id = '00000000-0000-0000-0013-000000000002';
