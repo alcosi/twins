99aceda1706e7fdcd97cc4c96ca8842d2dab2ca8
Turkov.Aleksey <alextur94@gmail.com>
2025-08-05 14:35:42 +0300

    TWINS-374: change migration file

--- core/src/main/resources/db/migration/V1.3.360.01__TWINS-374_at1_twinflow_transition_trigger.sql
+++ core/src/main/resources/db/migration/V1.3.360.01__TWINS-374_at1_twinflow_transition_trigger.sql
@@ -9,7 +9,17 @@ RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE TRIGGER before_insert_order
-    BEFORE INSERT ON twinflow_transition_trigger
-    FOR EACH ROW
-    EXECUTE FUNCTION transition_trigger_set_order();
+DO $$
+    BEGIN
+        IF NOT EXISTS (
+            SELECT 1 FROM pg_trigger
+            WHERE tgname = 'before_insert_order'
+              AND tgrelid = 'twinflow_transition_trigger'::regclass
+        ) THEN
+            CREATE TRIGGER before_insert_order
+                BEFORE INSERT ON twinflow_transition_trigger
+                FOR EACH ROW
+            EXECUTE FUNCTION transition_trigger_set_order();
+        END IF;
+    END
+$$;
