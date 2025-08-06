4ca48158be8e9600f237b21de0070ae0265f98ae
KevinORIS <c.s.2015@mail.ru>
2025-08-06 13:04:47 +0300

    TWINS-489: refactor after code review

--- core/src/main/java/org/twins/core/featurer/factory/filler/FillerFieldAsFoundTwinOfClass.java
+++ core/src/main/java/org/twins/core/featurer/factory/filler/FillerFieldAsFoundTwinOfClass.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Component;
 import org.twins.core.dao.twin.TwinEntity;
 import org.twins.core.domain.factory.FactoryItem;
 import org.twins.core.domain.search.BasicSearch;
+import org.twins.core.exception.ErrorCodeTwins;
 import org.twins.core.featurer.FeaturerTwins;
 import org.twins.core.featurer.fieldtyper.value.FieldValue;
 import org.twins.core.featurer.params.FeaturerParamUUIDTwinsTwinClassFieldId;
@@ -18,19 +19,21 @@ import org.twins.core.featurer.params.FeaturerParamUUIDTwinsTwinClassId;
 import org.twins.core.service.twin.TwinSearchService;
 import org.twins.core.service.twin.TwinService;
 
+import java.util.List;
 import java.util.Properties;
+import java.util.UUID;
 
 @Component
 @Featurer(id = FeaturerTwins.ID_2342,
-        name = "Field as context field head",
-        description = "Get head for twin from src field(link). Set this head to dst field(link)")
+        name = "Field as found twin of class",
+        description = "Fill link field by found twin of class")
 @Slf4j
 public class FillerFieldAsFoundTwinOfClass extends Filler {
 
-    @FeaturerParam(name = "Twin class field id", description = "", order = 1)
+    @FeaturerParam(name = "Twin class field id", description = "Link field witch need to fill", order = 1)
     public static final FeaturerParamUUID twinClassFieldLinkId = new FeaturerParamUUIDTwinsTwinClassFieldId("twinClassFieldLinkId");
 
-    @FeaturerParam(name = "Twin class id", description = "", order = 2)
+    @FeaturerParam(name = "Twin class id", description = "Class of twin that will fill the field", order = 2)
     public static final FeaturerParamUUID twinClassId = new FeaturerParamUUIDTwinsTwinClassId("twinClassId");
 
     @Lazy
@@ -44,9 +47,19 @@ public class FillerFieldAsFoundTwinOfClass extends Filler {
     @Override
     public void fill(Properties properties, FactoryItem factoryItem, TwinEntity templateTwin) throws ServiceException {
         BasicSearch search = new BasicSearch();
+        UUID extractedTwinClassId = twinClassId.extract(properties);
 
         search
-                .addTwinClassId(twinClassId.extract(properties), false);
+                .addTwinClassId(extractedTwinClassId, false);
+
+        List<TwinEntity> entityList = twinSearchService.findTwins(search);
+
+        if (entityList.isEmpty()) {
+            throw new ServiceException(ErrorCodeTwins.FACTORY_PIPELINE_STEP_ERROR, "there are no twins of class[" + extractedTwinClassId + "] found.");
+        }
+        if (entityList.size() > 1) {
+            throw new ServiceException(ErrorCodeTwins.FACTORY_PIPELINE_STEP_ERROR, "there are more than one twin of class[" + extractedTwinClassId + "] found.");
+        }
 
         TwinEntity twinEntity = twinSearchService.findTwins(search).getFirst();
 
