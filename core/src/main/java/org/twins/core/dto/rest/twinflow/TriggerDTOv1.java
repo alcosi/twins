67e979575cd1f6ff155c59e9c8b97398da3f95b0
Aleksey Turkov <74702834+alextur94@users.noreply.github.com>
2025-08-12 15:00:55 +0300

    TWINS-374: rollback DTO changes fow support in swagger (#646)

--- core/src/main/java/org/twins/core/dto/rest/twinflow/TriggerDTOv1.java
+++ core/src/main/java/org/twins/core/dto/rest/twinflow/TriggerDTOv1.java
@@ -2,6 +2,7 @@ package org.twins.core.dto.rest.twinflow;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
 import org.twins.core.dto.rest.DTOExamples;
 import org.twins.core.dto.rest.featurer.FeaturerDTOv1;
@@ -9,6 +10,7 @@ import org.twins.core.dto.rest.featurer.FeaturerDTOv1;
 import java.util.UUID;
 
 @Data
+@EqualsAndHashCode(callSuper = true)
 @Accessors(chain = true)
 @Schema(name = "TriggerV1")
 public class TriggerDTOv1 extends TriggerBaseDTOv1 {
