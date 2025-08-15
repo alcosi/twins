ebe78e5831522d5fa42e4fbc45fc75a02e2ba16e
Aleksey Turkov <74702834+alextur94@users.noreply.github.com>
2025-08-15 16:43:50 +0300

    TWINS-449: new ConditionerApiUserIsAssigneeForLinkedTwinOutputField
    * TWINS-449: new conditioner. Is current user assignee for linked twin from context field
    
    * TWINS-449: refactoring code
    
    * TWINS-449: new ConditionerApiUserIsAssigneeForLinkedTwinOutputField and refactoring code
    
    * TWINS-449: refactoring code

---
+++ core/src/main/java/org/twins/core/featurer/factory/conditioner/ConditionerApiUserIsAssigneeForLinkedTwinContextField.java
@@ -0,0 +1,24 @@
+package org.twins.core.featurer.factory.conditioner;
+
+import lombok.extern.slf4j.Slf4j;
+import org.cambium.common.exception.ServiceException;
+import org.cambium.featurer.annotations.Featurer;
+import org.springframework.stereotype.Component;
+import org.twins.core.domain.factory.FactoryItem;
+import org.twins.core.featurer.FeaturerTwins;
+import org.twins.core.featurer.fieldtyper.value.FieldValue;
+
+import java.util.Map;
+import java.util.UUID;
+
+@Component
+@Featurer(id = FeaturerTwins.ID_2435,
+        name = "Is current user assignee for linked twin from context field",
+        description = "Checks if current user is assignee for linked twin from context fields")
+@Slf4j
+public class ConditionerApiUserIsAssigneeForLinkedTwinContextField extends ConditionerApiUserIsAssigneeForLinkedTwinBase {
+    @Override
+    protected Map<UUID, FieldValue> getFields(FactoryItem factoryItem) throws ServiceException {
+        return factoryItem.getFactoryContext().getFields();
+    }
+}
---
+++ core/src/main/java/org/twins/core/featurer/factory/conditioner/ConditionerApiUserIsAssigneeForLinkedTwinOutputField.java
@@ -0,0 +1,24 @@
+package org.twins.core.featurer.factory.conditioner;
+
+import lombok.extern.slf4j.Slf4j;
+import org.cambium.common.exception.ServiceException;
+import org.cambium.featurer.annotations.Featurer;
+import org.springframework.stereotype.Component;
+import org.twins.core.domain.factory.FactoryItem;
+import org.twins.core.featurer.FeaturerTwins;
+import org.twins.core.featurer.fieldtyper.value.FieldValue;
+
+import java.util.Map;
+import java.util.UUID;
+
+@Component
+@Featurer(id = FeaturerTwins.ID_2436,
+        name = "Is current user assignee for linked twin from output field",
+        description = "Checks if current user is assignee for linked twin from output fields")
+@Slf4j
+public class ConditionerApiUserIsAssigneeForLinkedTwinOutputField extends ConditionerApiUserIsAssigneeForLinkedTwinBase {
+    @Override
+    protected Map<UUID, FieldValue> getFields(FactoryItem factoryItem) throws ServiceException {
+        return factoryItem.getOutput().getFields();
+    }
+}
