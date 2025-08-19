4278beeb0f1ead9a04e56bb117b904e1cdb2a8be
Egor Chernooky <e.chernooky@gmail.com>
2025-08-19 16:39:38 +0300

    TWINS-475: fix triggers

---
+++ core/src/main/resources/db/migration_backlog/V1.3.390.xx__TWINS-475_ec1_before_insert.sql
@@ -0,0 +1,28 @@
+create function twin_class_before_insert_wrapper() returns trigger
+    language plpgsql
+as
+$$
+begin
+    perform twin_class_set_inherited_face_on_insert(old, new);
+
+    return new;
+end;
+$$;
+
+create trigger twin_class_before_insert_wrapper_trigger
+    before insert
+    on twin_class
+    for each row
+execute procedure twin_class_before_insert_wrapper();
+
+create function twin_class_after_insert_wrapper() returns trigger
+    language plpgsql
+as
+$$
+begin
+    -- Call tree update on insert when extends_twin_class_id is set
+    perform hierarchy_twin_class_extends_process_tree_update(old, new, TG_OP);
+
+    return new;
+end;
+$$;
