4ec4b6cddd37acca8f78e088887bc01a6f3583c1
Aleksey Turkov <74702834+alextur94@users.noreply.github.com>
2025-09-09 12:26:33 +0300

    TWINS-504: configured user search (#658)
    * TWINS-504: configured user search
    
    * TWINS-504: configured user search and refactoring code
    
    * TWINS-504: refactoring code

--- core/src/main/java/org/twins/core/domain/search/UserSearch.java
+++ core/src/main/java/org/twins/core/domain/search/UserSearch.java
@@ -1,14 +1,19 @@
 package org.twins.core.domain.search;
 
-import io.swagger.v3.oas.annotations.media.Schema;
+import com.google.common.collect.ImmutableList;
 import lombok.Data;
 import lombok.experimental.Accessors;
 import lombok.experimental.FieldNameConstants;
+import org.apache.commons.lang3.tuple.Pair;
+import org.cambium.common.util.CollectionUtils;
+import org.twins.core.dao.user.UserSearchEntity;
 import org.twins.core.dao.user.UserStatus;
 
 import java.util.List;
 import java.util.Set;
 import java.util.UUID;
+import java.util.function.BiConsumer;
+import java.util.function.Function;
 
 @Data
 @Accessors(chain = true)
@@ -25,4 +30,24 @@ public class UserSearch {
     private List<SpaceSearch> spaceList;
     private List<SpaceSearch> spaceExcludeList;
     private BasicSearchList childTwinSearches;
+    private UserSearchEntity configuredSearch;
+
+    public UserSearch addSpace(final SpaceSearch spaceSearch, boolean exclude) {
+        if (exclude)
+            spaceExcludeList = CollectionUtils.safeAdd(spaceExcludeList, spaceSearch);
+        else
+            spaceList = CollectionUtils.safeAdd(spaceList, spaceSearch);
+        return this;
+    }
+
+    public static final ImmutableList<Pair<Function<UserSearch, Set>, BiConsumer<UserSearch, Set>>> FUNCTIONS = ImmutableList.of(
+            Pair.of(UserSearch::getUserIdList, UserSearch::setUserIdList),
+            Pair.of(UserSearch::getUserIdExcludeList, UserSearch::setUserIdExcludeList),
+            Pair.of(UserSearch::getUserNameLikeList, UserSearch::setUserNameLikeList),
+            Pair.of(UserSearch::getUserNameLikeExcludeList, UserSearch::setUserNameLikeExcludeList),
+            Pair.of(UserSearch::getUserEmailLikeList, UserSearch::setUserEmailLikeList),
+            Pair.of(UserSearch::getUserEmailLikeExcludeList, UserSearch::setUserEmailLikeExcludeList),
+            Pair.of(UserSearch::getStatusIdList, UserSearch::setStatusIdList),
+            Pair.of(UserSearch::getStatusIdExcludeList, UserSearch::setStatusIdExcludeList)
+    );
 }
