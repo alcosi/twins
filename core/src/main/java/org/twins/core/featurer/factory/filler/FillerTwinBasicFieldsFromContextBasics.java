f954eafcb00bb9987a836ce3018b14958cbdf805
Turkov.Aleksey <alextur94@gmail.com>
2025-09-18 09:39:09 +0300

    TWINS-137: refactoring code

--- core/src/main/java/org/twins/core/featurer/factory/filler/FillerTwinBasicFieldsFromContextBasics.java
+++ core/src/main/java/org/twins/core/featurer/factory/filler/FillerTwinBasicFieldsFromContextBasics.java
@@ -2,20 +2,22 @@ package org.twins.core.featurer.factory.filler;
 
 import lombok.extern.slf4j.Slf4j;
 import org.cambium.common.exception.ServiceException;
+import org.cambium.common.kit.Kit;
+import org.cambium.common.util.CollectionUtils;
 import org.cambium.featurer.annotations.Featurer;
 import org.cambium.featurer.annotations.FeaturerParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 import org.twins.core.dao.twin.TwinEntity;
+import org.twins.core.dao.user.UserEntity;
+import org.twins.core.dao.user.UserRepository;
 import org.twins.core.domain.TwinBasicFields;
 import org.twins.core.domain.factory.FactoryItem;
 import org.twins.core.featurer.FeaturerTwins;
 import org.twins.core.featurer.params.FeaturerParamBasicsSetTwinBasicField;
-import org.twins.core.service.user.UserService;
 
-import java.util.Properties;
-import java.util.Set;
+import java.util.*;
 
 @Component
 @Featurer(id = FeaturerTwins.ID_2327,
@@ -29,7 +31,7 @@ public class FillerTwinBasicFieldsFromContextBasics extends Filler {
 
     @Lazy
     @Autowired
-    private UserService userService;
+    private UserRepository userRepository;
 
     @Override
     public void fill(Properties properties, FactoryItem factoryItem, TwinEntity templateTwin) throws ServiceException {
@@ -37,14 +39,27 @@ public class FillerTwinBasicFieldsFromContextBasics extends Filler {
         TwinBasicFields basics = factoryItem.getFactoryContext().getBasics();
         Set<TwinBasicFields.Basics> fieldsString = fields.extract(properties);
         if (null != basics) {
-            if (fieldsString.contains(TwinBasicFields.Basics.createdByUserId))
+            Set<UUID> needLoad = new HashSet<>();
+            Kit<UserEntity, UUID> loadedUsersKit = new Kit<>(UserEntity::getId);
+            if (outputTwinEntity.getCreatedByUser() == null)
+                needLoad.add(outputTwinEntity.getCreatedByUserId());
+
+            if (outputTwinEntity.getAssignerUser() == null)
+                needLoad.add(outputTwinEntity.getAssignerUserId());
+
+            if (CollectionUtils.isNotEmpty(needLoad))
+                loadedUsersKit.addAll(userRepository.findByIdIn(needLoad));
+
+            if (fieldsString.contains(TwinBasicFields.Basics.createdByUserId)) {
                 outputTwinEntity
                         .setCreatedByUserId(basics.getCreatedByUserId())
-                        .setCreatedByUser(userService.findEntitySafe(basics.getCreatedByUserId()));
-            if (fieldsString.contains(TwinBasicFields.Basics.assigneeUserId))
+                        .setCreatedByUser(loadedUsersKit.get(outputTwinEntity.getCreatedByUserId()));
+            }
+            if (fieldsString.contains(TwinBasicFields.Basics.assigneeUserId)) {
                 outputTwinEntity
                         .setAssignerUserId(basics.getAssigneeUserId())
-                        .setAssignerUser(userService.findEntitySafe(basics.getAssigneeUserId()));;
+                        .setAssignerUser(loadedUsersKit.get(outputTwinEntity.getAssignerUserId()));
+            }
             if (fieldsString.contains(TwinBasicFields.Basics.name))
                 outputTwinEntity.setName(basics.getName());
             if (fieldsString.contains(TwinBasicFields.Basics.description))
