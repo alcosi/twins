ec11dce06c379049f4ec18bbbd2759429eb6448f
Turkov.Aleksey <alextur94@gmail.com>
2025-09-29 11:09:07 +0300

    TWINS-550: fix null pointer for user narrow search

--- core/src/main/java/org/twins/core/mappers/rest/user/UserSearchConfiguredDTOReverseMapper.java
+++ core/src/main/java/org/twins/core/mappers/rest/user/UserSearchConfiguredDTOReverseMapper.java
@@ -15,6 +15,8 @@ public class UserSearchConfiguredDTOReverseMapper extends RestSimpleDTOMapper<Us
 
     @Override
     public void map(UserSearchConfiguredDTOv1 src, UserSearch dst, MapperContext mapperContext) throws Exception {
-        userSearchDTOReverseMapper.map(src.getNarrow(), dst, mapperContext);
+        if (src.getNarrow() != null) {
+            userSearchDTOReverseMapper.map(src.getNarrow(), dst, mapperContext);
+        }
     }
 }
