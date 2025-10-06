afb7c57ce058f2b1943c7c3f776e09b1ac888bea
Alexander Kuzniatsou <a.kuzniatsou@alcosi.eu>
2025-10-06 11:17:30 +0300

    TWINS-555: add trigger

--- core/src/main/resources/db/migration_backlog/V1.3.4xx.01__TWINS-555_nm1_segments.sql
+++ core/src/main/resources/db/migration_backlog/V1.3.4xx.01__TWINS-555_nm1_segments.sql
@@ -4,8 +4,6 @@ alter table twin_class
 alter table twin_class
     add if not exists has_segments boolean default false not null;
 
--- TODO add trigger to set has_segments flag
-
 alter table twin_class_field
     add if not exists system boolean default false not null;
 
@@ -18,3 +16,100 @@ create index if not exists twin_class_has_segments_index
 
 create index if not exists twin_class_field_system_index
     on twin_class_field (system);
+
+CREATE OR REPLACE FUNCTION twin_class_has_segments_check(old twin_class) returns void
+    LANGUAGE plpgsql
+AS $$
+BEGIN
+    IF old.head_twin_class_id IS NOT NULL THEN
+        UPDATE twin_class
+        SET has_segments = (
+            SELECT EXISTS (
+                SELECT 1
+                FROM twin_class child
+                WHERE child.head_twin_class_id = old.head_twin_class_id
+                  AND child.segment = TRUE
+            )
+        )
+        WHERE id = old.head_twin_class_id;
+    END IF;
+END;
+$$;
+
+create or replace function public.twin_class_after_delete_wrapper() returns trigger
+    language plpgsql
+as
+$$
+begin
+    -- Remove i18n and translations for deleted twin_class
+    perform twin_class_on_delete_i18n_and_translations_delete(old);
+    perform twin_class_has_segments_check(old);
+    return old;
+end;
+$$;
+
+create or replace function public.twin_class_after_insert_wrapper() returns trigger
+    language plpgsql
+as
+$$
+begin
+    -- Call tree update on insert when extends_twin_class_id is set
+    perform hierarchy_twin_class_extends_process_tree_update(old, new, TG_OP);
+    perform twin_class_has_segments_check(new);
+    return new;
+end;
+$$;
+
+create or replace function public.twin_class_after_update_wrapper() returns trigger
+    language plpgsql
+as
+$$
+begin
+    -- Update tree if extends_twin_class_id changed
+    if new.extends_twin_class_id is distinct from old.extends_twin_class_id
+        -- we need to update tree only in case if extends_twin_class_id was updated and tree wasn't
+        and new.extends_hierarchy_tree is not distinct from old.extends_hierarchy_tree then
+        perform hierarchy_twin_class_extends_process_tree_update(old, new, TG_OP);
+    end if;
+
+    -- Update tree and has_segments if head_twin_class_id changed
+    if new.head_twin_class_id is distinct from old.head_twin_class_id then
+        perform hierarchy_twin_class_head_process_tree_update(old, new, TG_OP);
+        perform twin_class_has_segments_check(old);
+        perform twin_class_has_segments_check(new);
+    end if;
+
+    -- Update has_segments if segment changed
+    if new.segment is distinct from old.segment then
+        perform twin_class_has_segments_check(new);
+    end if;
+
+    -- Recalculate hierarchy if schema space fields changed
+    if (new.permission_schema_space is distinct from old.permission_schema_space)
+        or (new.twinflow_schema_space is distinct from old.twinflow_schema_space)
+        or (new.twin_class_schema_space is distinct from old.twin_class_schema_space)
+        or (new.alias_space is distinct from old.alias_space) then
+        perform twin_class_hierarchy_recalculate(old, new);
+    end if;
+
+    -- Auto update permissions if key changed
+    if new.key is distinct from old.key then
+        perform permissions_autoupdate_on_twin_class_update(old, new);
+    end if;
+
+    -- Update inherited breadcrumbs face id if changed
+    if new.bread_crumbs_face_id is distinct from old.bread_crumbs_face_id then
+        perform twin_class_update_inherited_bread_crumbs_face_id(old, new);
+    end if;
+
+    -- Update inherited page face id if changed
+    if new.page_face_id is distinct from old.page_face_id then
+        perform twin_class_update_inherited_page_face_id(old, new);
+    end if;
+
+    return new;
+end;
+$$;
+
+
+
