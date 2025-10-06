186d1f10faf56ffde9af3841e1d1cceb1475f8e6
Turkov.Aleksey <alextur94@gmail.com>
2025-10-06 11:46:32 +0300

    TWINS-504: fix NullPointerException in map

--- core/src/main/java/org/twins/core/exception/ErrorCodeTwins.java
+++ core/src/main/java/org/twins/core/exception/ErrorCodeTwins.java
@@ -171,7 +171,8 @@ public enum ErrorCodeTwins implements ErrorCode {
     NOTIFICATION_CONFIGURATION_ERROR(12901, "something wrong with notification configuration"),
     FACE_CONFIG_IS_NOT_UNIQ(13001, "face has few config variants"),
     FACE_NO_CONFIG_IS_SUITABLE(13002, "no suitable face config found"),
-    TWIN_VALIDATOR_INCORRECT(13101, "incorrect twin validator for this case");
+    TWIN_VALIDATOR_INCORRECT(13101, "incorrect twin validator for this case"),
+    USER_SEARCH_CONFIG_INCORRECT(14001, "user search config incorrect");
 
     private final int code;
     private final String message;
