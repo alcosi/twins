0a62d88a2fad99d847ad3c03cfa29bc8df0a2c3e
KevinORIS <c.s.2015@mail.ru>
2025-10-07 10:55:52 +0300

    TWINS-561: add featurer validation when create twinClassField

--- core/src/main/java/org/twins/core/service/twinclass/TwinClassFieldService.java
+++ core/src/main/java/org/twins/core/service/twinclass/TwinClassFieldService.java
@@ -342,6 +342,8 @@ public class TwinClassFieldService extends EntitySecureFindServiceImpl<TwinClass
             String fieldKey = KeyUtils.lowerCaseNullSafe(field.getKey(), ErrorCodeTwins.TWIN_CLASS_FIELD_KEY_INCORRECT);
             field.setKey(fieldKey);
 
+            featurerService.checkValid(field.getFieldTyperFeaturerId(), field.getFieldTyperParams(), FieldTyper.class);
+
             if (twinClassFieldRepository.existsByKeyAndTwinClassId(fieldKey, field.getTwinClassId())) {
                 throw new ServiceException(ErrorCodeTwins.TWIN_CLASS_FIELD_KEY_INCORRECT,
                         "Field key already exists: [" + fieldKey + "] for twin class [" + field.getTwinClassId() + "]");
