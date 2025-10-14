d96109b1d0e950e65adfa25764932f9f17296eb5
KevinORIS <c.s.2015@mail.ru>
2025-10-14 15:52:10 +0300

    TWINS-570: refactor FieldTyperNumeric

---
+++ core/src/main/java/org/cambium/featurer/params/FeaturerParamStringSet.java
@@ -0,0 +1,30 @@
+package org.cambium.featurer.params;
+
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.cambium.featurer.annotations.FeaturerParamType;
+
+import java.util.*;
+import java.util.regex.Pattern;
+
+@FeaturerParamType(
+        id = "STRING_SET",
+        description = "",
+        regexp = FeaturerParamStringSet.STRING_SET_REGEXP,
+        example = FeaturerParamStringSet.STRING_SET_EXAMPLE)
+public class FeaturerParamStringSet extends FeaturerParam<Set<String>> {
+    public static final String STRING_SET_REGEXP = ".*";
+    public static final String STRING_SET_EXAMPLE = "string|,|.| ";
+    private static final String SEPARATOR = "|";
+    public FeaturerParamStringSet(String key) {
+        super(key);
+    }
+
+    @Override
+    public Set<String> extract(Properties properties) {
+        Set<String> ret = new LinkedHashSet<>();
+        if (ObjectUtils.isNotEmpty(properties.get(key)) && StringUtils.isNotBlank(properties.get(key).toString()))
+            ret.addAll(Arrays.asList(properties.get(key).toString().split(Pattern.quote(SEPARATOR))));
+        return ret;
+    }
+}
--- core/src/main/java/org/twins/core/featurer/fieldtyper/descriptor/FieldDescriptorNumeric.java
+++ core/src/main/java/org/twins/core/featurer/fieldtyper/descriptor/FieldDescriptorNumeric.java
@@ -4,6 +4,8 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
 
+import java.util.Set;
+
 @Data
 @EqualsAndHashCode(callSuper = true)
 @Accessors(fluent = true)
@@ -11,7 +13,8 @@ public class FieldDescriptorNumeric extends FieldDescriptor {
     private Double min;
     private Double max;
     private Double step;
-    private String thousandSeparator;
-    private String decimalSeparator;
+    private Set<String> thousandSeparator;
+    private Set<String> decimalSeparator;
     private Integer decimalPlaces;
+    private Boolean round;
 }
