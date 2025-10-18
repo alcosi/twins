2d6498776b2a736182e7aad3ef10d8b4f87aa2b0
KevinORIS <c.s.2015@mail.ru>
2025-10-18 11:20:20 +0300

    TWINS-574: refactor after code review 2

--- core/src/main/java/org/twins/core/dao/twin/TwinFieldSimpleNonIndexedRepository.java
+++ core/src/main/java/org/twins/core/dao/twin/TwinFieldSimpleNonIndexedRepository.java
@@ -17,7 +17,6 @@ public interface TwinFieldSimpleNonIndexedRepository extends CrudRepository<Twin
     List<TwinFieldSimpleNonIndexedEntity> findByTwinId(UUID twinId);
     List<TwinFieldSimpleNonIndexedEntity> findByTwinIdIn(Collection<UUID> twinIdList);
     boolean existsByTwinClassFieldId(UUID twinClassFieldId);
-    boolean existsByTwinClassFieldIdAndValue(UUID twinClassFieldId, String value);
 
     @Query(value = """
             select distinct field.twinClassFieldId
@@ -33,18 +32,4 @@ public interface TwinFieldSimpleNonIndexedRepository extends CrudRepository<Twin
     void replaceTwinClassFieldForTwinsOfClass(@Param("twinClassId") UUID twinClassId, @Param("fromTwinClassFieldId") UUID fromTwinClassFieldId, @Param("toTwinClassFieldId") UUID toTwinClassFieldId);
 
     void deleteByTwinIdAndTwinClassFieldIdIn(UUID twinId, Set<UUID> twinClassFieldIds);
-
-    @Query(value = """
-        select COUNT(*) = 0 from TwinFieldSimpleNonIndexedEntity tfs
-        inner join TwinEntity t on tfs.twinId = t.id
-        where t.ownerUserId = :ownerUserId and tfs.value = :value and tfs.twinClassFieldId = :twinClassFieldId
-    """)
-    boolean existsByTwinClassFieldIdAndValueAndOwnerUserId(UUID twinClassFieldId, String value, UUID ownerUserId);
-
-    @Query(value = """
-        select COUNT(*) = 0 from TwinFieldSimpleNonIndexedEntity tfs
-        inner join TwinEntity t on tfs.twinId = t.id
-        where t.ownerBusinessAccountId = :ownerBusinessAccountId and tfs.value = :value and tfs.twinClassFieldId = :twinClassFieldId
-    """)
-    boolean existsByTwinClassFieldIdAndValueAndOwnerBusinessAccountId(UUID twinClassFieldId, String value, UUID ownerBusinessAccountId);
 }
--- core/src/main/java/org/twins/core/featurer/fieldtyper/FieldTyperTextNonIndexedField.java
+++ core/src/main/java/org/twins/core/featurer/fieldtyper/FieldTyperTextNonIndexedField.java
@@ -42,17 +42,12 @@ public class FieldTyperTextNonIndexedField extends FieldTyperSimpleNonIndexed<Fi
     public static final FeaturerParamString regexp = new FeaturerParamString("regexp");
     @FeaturerParam(name = "EditorType", description = "", order = 2, optional = true, defaultValue = "PLAIN")
     public static final FeaturerParamStringTwinsEditorType editorType = new FeaturerParamStringTwinsEditorType("editorType");
-    @FeaturerParam(name = "Unique", description = "", order = 3, optional = true, defaultValue = "false")
-    public static final FeaturerParamBoolean unique = new FeaturerParamBoolean("unique");
-
-    private final TwinFieldSimpleNonIndexedRepository twinFieldSimpleNonIndexedRepository;
 
     @Override
     public FieldDescriptorText getFieldDescriptor(TwinClassFieldEntity twinClassFieldEntity, Properties properties) {
         FieldDescriptorText descriptorText = new FieldDescriptorText()
                 .regExp(regexp.extract(properties))
                 .editorType(editorType.extract(properties));
-        descriptorText.backendValidated(unique.extract(properties));
         return descriptorText;
     }
 
@@ -71,44 +66,5 @@ public class FieldTyperTextNonIndexedField extends FieldTyperSimpleNonIndexed<Fi
     @Override
     public Specification<TwinEntity> searchBy(TwinFieldSearchText search) {
         return Specification.where(TwinSpecification.checkFieldText(search, TwinEntity.Fields.fieldsSimple, TwinFieldSimpleEntity.Fields.value));
-    }
-
-
-    private void checkForUniqueness(TwinEntity twin, FieldValueText value) throws ServiceException {
-        OwnerType ownerType = twin.getTwinClass().getOwnerType();
-
-        switch (ownerType) {
-            case USER, DOMAIN_USER -> {
-                if (!twinFieldSimpleNonIndexedRepository.existsByTwinClassFieldIdAndValueAndOwnerUserId(value.getTwinClassFieldId(), value.getValue(), twin.getOwnerUserId())) {
-                    throw new ServiceException(ErrorCodeTwins.TWIN_CLASS_FIELD_VALUE_IS_NOT_UNIQUE, value.getTwinClassField().logNormal() + " value[" + value.getValue() + "] is not unique");
-                }
-            }
-            case BUSINESS_ACCOUNT, DOMAIN_BUSINESS_ACCOUNT -> {
-                if (!twinFieldSimpleNonIndexedRepository.existsByTwinClassFieldIdAndValueAndOwnerBusinessAccountId(value.getTwinClassFieldId(), value.getValue(), twin.getOwnerBusinessAccountId())) {
-                    throw new ServiceException(ErrorCodeTwins.TWIN_CLASS_FIELD_VALUE_IS_NOT_UNIQUE, value.getTwinClassField().logNormal() + " value[" + value.getValue() + "] is not unique");
-                }
-            }
-            default -> {
-                if (!twinFieldSimpleNonIndexedRepository.existsByTwinClassFieldIdAndValue(value.getTwinClassFieldId(), value.getValue())) {
-                    throw new ServiceException(ErrorCodeTwins.TWIN_CLASS_FIELD_VALUE_IS_NOT_UNIQUE, value.getTwinClassField().logNormal() + " value[" + value.getValue() + "] is not unique");
-                }
-            }
-        }
-    }
-
-    @Override
-    public ValidationResult validate(Properties properties, TwinEntity twin, FieldValueText fieldValue) {
-        try {
-            String pattern = regexp.extract(properties);
-            if (!fieldValue.getValue().matches(pattern)) {
-                throw new ServiceException(ErrorCodeTwins.TWIN_CLASS_FIELD_VALUE_INCORRECT, fieldValue.getTwinClassField().easyLog(EasyLoggable.Level.NORMAL) + " value[" + fieldValue.getValue() + "] does not match pattern[" + pattern + "]");
-            }
-            if (unique.extract(properties).equals(true)) {
-                checkForUniqueness(twin, fieldValue);
-            }
-        } catch (ServiceException e) {
-            return new ValidationResult(false, i18nService.translateToLocale(fieldValue.getTwinClassField().getBeValidationErrorI18nId()));
-        }
-        return new ValidationResult(true);
     }
 }
