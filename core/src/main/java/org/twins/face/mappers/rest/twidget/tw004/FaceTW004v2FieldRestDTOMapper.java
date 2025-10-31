2cedde94ffbb8a199d3867bb176cc4a97c0f0157
nikita.mekh <n.mekh@hutkigrosh.by>
2025-10-31 12:41:04 +0300

    TWINS-589: replaced i18nService with I18nCacheHolder in Face DTO mappers

--- core/src/main/java/org/twins/face/mappers/rest/twidget/tw004/FaceTW004v2FieldRestDTOMapper.java
+++ core/src/main/java/org/twins/face/mappers/rest/twidget/tw004/FaceTW004v2FieldRestDTOMapper.java
@@ -3,17 +3,16 @@ package org.twins.face.mappers.rest.twidget.tw004;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.twins.core.controller.rest.annotation.MapperModePointerBinding;
-import org.twins.face.domain.twidget.tw004.FaceTW004TwinClassField;
+import org.twins.core.holder.I18nCacheHolder;
 import org.twins.core.mappers.rest.RestSimpleDTOMapper;
 import org.twins.core.mappers.rest.mappercontext.MapperContext;
 import org.twins.core.mappers.rest.twinclass.TwinClassFieldRestDTOMapper;
-import org.twins.core.service.i18n.I18nService;
+import org.twins.face.domain.twidget.tw004.FaceTW004TwinClassField;
 import org.twins.face.dto.rest.twidget.tw004.FaceTW004FieldDTOv1;
 
 @Component
 @RequiredArgsConstructor
 public class FaceTW004v2FieldRestDTOMapper extends RestSimpleDTOMapper<FaceTW004TwinClassField, FaceTW004FieldDTOv1> {
-    private final I18nService i18nService;
     @MapperModePointerBinding(modes = FaceTW004Modes.FaceTW0042TwinClassFieldMode.class)
     private final TwinClassFieldRestDTOMapper twinClassFieldRestDTOMapper;
 
@@ -21,7 +20,7 @@ public class FaceTW004v2FieldRestDTOMapper extends RestSimpleDTOMapper<FaceTW004
     public void map(FaceTW004TwinClassField src, FaceTW004FieldDTOv1 dst, MapperContext mapperContext) throws Exception {
         dst
                 .setKey(src.getField().getKey())
-                .setLabel(i18nService.translateToLocale(src.getField().getNameI18nId()))
+                .setLabel(I18nCacheHolder.addId(src.getField().getNameI18nId()))
                 .setTwinClassFieldId(src.getField().getId())
                 .setEditable(src.isEditable())
                 .setOrder(src.getOrder());
--- core/src/main/java/org/twins/face/mappers/rest/twidget/tw005/FaceTW005ButtonRestDTOMapper.java
+++ core/src/main/java/org/twins/face/mappers/rest/twidget/tw005/FaceTW005ButtonRestDTOMapper.java
@@ -4,10 +4,10 @@ import lombok.RequiredArgsConstructor;
 import org.cambium.common.util.StringUtils;
 import org.springframework.stereotype.Component;
 import org.twins.core.controller.rest.annotation.MapperModePointerBinding;
+import org.twins.core.holder.I18nCacheHolder;
 import org.twins.core.mappers.rest.RestSimpleDTOMapper;
 import org.twins.core.mappers.rest.mappercontext.MapperContext;
 import org.twins.core.mappers.rest.twinflow.TwinTransitionRestDTOMapper;
-import org.twins.core.service.i18n.I18nService;
 import org.twins.core.service.resource.ResourceService;
 import org.twins.face.dao.twidget.tw005.FaceTW005ButtonEntity;
 import org.twins.face.dto.rest.twidget.tw005.FaceTW005ButtonDTOv1;
@@ -16,7 +16,6 @@ import org.twins.face.dto.rest.twidget.tw005.FaceTW005ButtonDTOv1;
 @RequiredArgsConstructor
 public class FaceTW005ButtonRestDTOMapper extends RestSimpleDTOMapper<FaceTW005ButtonEntity, FaceTW005ButtonDTOv1> {
     private final ResourceService resourceService;
-    private final I18nService i18nService;
     @MapperModePointerBinding(modes = FaceTW005Modes.FaceTW005Button2TransitionMode.class)
     private final TwinTransitionRestDTOMapper twinTransitionRestDTOMapper;
 
@@ -24,7 +23,7 @@ public class FaceTW005ButtonRestDTOMapper extends RestSimpleDTOMapper<FaceTW005B
     public void map(FaceTW005ButtonEntity src, FaceTW005ButtonDTOv1 dst, MapperContext mapperContext) throws Exception {
         dst
                 .setId(src.getId())
-                .setLabel(i18nService.translateToLocale(src.getLabelI18nId() != null ? src.getLabelI18nId() : src.getTransition().getNameI18NId()))
+                .setLabel(I18nCacheHolder.addId(src.getLabelI18nId() != null ? src.getLabelI18nId() : src.getTransition().getNameI18NId()))
                 .setOrder(src.getOrder())
                 .setIcon(resourceService.getResourceUri(src.getIconResource()))
                 .setTransitionId(src.getTransitionId())
