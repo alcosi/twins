bc3a300665020326c0b256859a96a7f169bf223d
Egor Chernooky <e.chernooky@gmail.com>
2025-11-14 00:21:11 +0300

    TWINS-589: deleted useless comment

--- core/src/main/java/org/twins/core/featurer/storager/filehandler/StoragerFileHandlerController.java
+++ core/src/main/java/org/twins/core/featurer/storager/filehandler/StoragerFileHandlerController.java
@@ -114,7 +114,7 @@ public class StoragerFileHandlerController extends StoragerAbstractChecked {
         try {
             var properties = extractProperties(params, false);
             var url = fileHandlerUri.extract(properties) + "/api/delete/synced";
-            var dirs = extractDirsToDelete(fileKey); // extracting only relative path without file name
+            var dirs = extractDirsToDelete(fileKey);
             var request = new HttpEntity<>(new FileHandlerDeleteRqDTO(List.of(dirs), StorageType.S3), new HttpHeaders());
             var resp = restTemplate.exchange(url, HttpMethod.POST, request, Void.class);
 
