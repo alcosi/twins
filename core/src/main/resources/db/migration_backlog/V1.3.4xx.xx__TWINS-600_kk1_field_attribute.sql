2dad6b297931cdbe90f20f1ccd8f1de1ea2d16ef
KevinORIS <c.s.2015@mail.ru>
2025-11-18 14:40:43 +0300

    TWINS-600: migration fix

--- core/src/main/resources/db/migration_backlog/V1.3.4xx.xx__TWINS-600_kk1_field_attribute.sql
+++ core/src/main/resources/db/migration_backlog/V1.3.4xx.xx__TWINS-600_kk1_field_attribute.sql
@@ -1,4 +1,4 @@
-CREATE TABLE twin_class_field_attribute (
+CREATE TABLE IF NOT EXISTS twin_class_field_attribute (
     id UUID PRIMARY KEY,
     twin_class_id UUID REFERENCES twin_class(id),
     key VARCHAR NOT NULL,
@@ -6,24 +6,28 @@ CREATE TABLE twin_class_field_attribute (
     create_permission_id UUID REFERENCES permission(id),
     delete_permission_id UUID REFERENCES permission(id),
     uniq BOOLEAN DEFAULT FALSE,
-    ADD CONSTRAINT uc_twin_class_field_attribute_class_key UNIQUE (twin_class_id, key);
+    CONSTRAINT uc_twin_class_field_attribute_class_key UNIQUE (twin_class_id, key)
 );
 
-ALTER TABLE projection_exclusion RENAME TO twin_field_attribute;
+ALTER TABLE IF EXISTS projection_exclusion RENAME TO twin_field_attribute;
 
-ALTER TABLE twin_field_attribute
+ALTER TABLE IF EXISTS twin_field_attribute
     RENAME CONSTRAINT projection_exclusion_twin_class_field_id_fkey TO twin_field_attribute_twin_class_field_id_fkey;
 
-ALTER TABLE twin_field_attribute
+ALTER TABLE IF EXISTS twin_field_attribute
     RENAME CONSTRAINT projection_exclusion_twin_id_fkey TO twin_field_attribute_twin_id_fkey;
 
 ALTER TABLE twin_field_attribute
-    ADD COLUMN twin_class_field_attribute_id UUID REFERENCES twin_class_field_attribute (id),
-    ADD COLUMN note_msg TEXT,
-    ADD COLUMN note_msg_context HSTORE,
-    ADD COLUMN changed_at TIMESTAMP;
+    ADD COLUMN IF NOT EXISTS twin_class_field_attribute_id UUID,
+    ADD COLUMN IF NOT EXISTS note_msg TEXT,
+    ADD COLUMN IF NOT EXISTS note_msg_context HSTORE,
+    ADD COLUMN IF NOT EXISTS changed_at TIMESTAMP;
 
-CREATE INDEX twin_field_attribute_twin_class_field_idx ON twin_field_attribute (twin_id, twin_class_field_id);
-CREATE INDEX twin_field_attribute_attribute_id_idx ON twin_field_attribute (twin_class_field_attribute_id);
+ALTER TABLE twin_field_attribute
+    ADD CONSTRAINT fk_twin_field_attribute_class_attr
+        FOREIGN KEY (twin_class_field_attribute_id) REFERENCES twin_class_field_attribute(id);
+
+CREATE INDEX IF NOT EXISTS twin_field_attribute_twin_class_field_idx ON twin_field_attribute (twin_id, twin_class_field_id);
+CREATE INDEX IF NOT EXISTS twin_field_attribute_attribute_id_idx ON twin_field_attribute (twin_class_field_attribute_id);
 
 
