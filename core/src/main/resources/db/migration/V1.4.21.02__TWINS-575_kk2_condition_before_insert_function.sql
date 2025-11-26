a22b5b65b39f38f458c183d402aeecff2538f11b
KevinORIS <c.s.2015@mail.ru>
2025-11-26 12:38:43 +0300

    fix migration

--- core/src/main/resources/db/migration/V1.4.21.02__TWINS-575_kk2_condition_before_insert_function.sql
+++ core/src/main/resources/db/migration/V1.4.21.02__TWINS-575_kk2_condition_before_insert_function.sql
@@ -6,21 +6,21 @@ alter table public.twin_class_field_condition
 CREATE OR REPLACE FUNCTION validate_twin_class_field_condition()
 RETURNS TRIGGER AS $$
 BEGIN
-IF NEW.logic_operator = 'LEAF' THEN
+IF NEW.logic_operator_id = 'LEAF' THEN
     IF NEW.base_twin_class_field_id IS NULL THEN
-            RAISE EXCEPTION 'base_twin_class_field_id is required when logic_operator = LEAF';
+            RAISE EXCEPTION 'base_twin_class_field_id is required when logic_operator_id = LEAF';
     END IF;
 
     IF NEW.condition_evaluator_featurer_id IS NULL THEN
-            RAISE EXCEPTION 'condition_evaluator_featurer_id is required when logic_operator = LEAF';
+            RAISE EXCEPTION 'condition_evaluator_featurer_id is required when logic_operator_id = LEAF';
     END IF;
 ELSE
     IF NEW.base_twin_class_field_id IS NOT NULL THEN
-            RAISE EXCEPTION 'base_twin_class_field_id must be NULL when logic_operator != LEAF';
+            RAISE EXCEPTION 'base_twin_class_field_id must be NULL when logic_operator_id != LEAF';
     END IF;
 
     IF NEW.condition_evaluator_featurer_id IS NOT NULL THEN
-            RAISE EXCEPTION 'condition_evaluator_featurer_id must be NULL when logic_operator != LEAF';
+            RAISE EXCEPTION 'condition_evaluator_featurer_id must be NULL when logic_operator_id != LEAF';
     END IF;
 END IF;
 
