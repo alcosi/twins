4c021e67da092bfd14bdcd74a5e1981adc5f6f8f
Turkov.Aleksey <alextur94@gmail.com>
2025-11-27 22:47:19 +0300

    TWINS-612: fix featurer id

--- core/src/main/resources/db/migration_backlog/V1.4.26.xx__TWINS-612_at1_featurer.sql
+++ core/src/main/resources/db/migration_backlog/V1.4.26.xx__TWINS-612_at1_featurer.sql
@@ -1,1 +1,1 @@
-INSERT INTO featurer (id, featurer_type_id, class, name, description, deprecated) VALUES (2338, 23, '', '', '', false) on conflict on constraint featurer_pk do nothing ;
+INSERT INTO featurer (id, featurer_type_id, class, name, description, deprecated) VALUES (2343, 23, '', '', '', false) on conflict on constraint featurer_pk do nothing ;
