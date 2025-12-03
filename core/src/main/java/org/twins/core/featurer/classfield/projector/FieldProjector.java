605b31ebc585a4376463f1ebc81adc7ddbe13adb
KevinORIS <c.s.2015@mail.ru>
2025-12-03 16:19:15 +0300

    TWINS-599: add projector featurers

--- core/src/main/java/org/twins/core/featurer/classfield/projector/FieldProjector.java
+++ core/src/main/java/org/twins/core/featurer/classfield/projector/FieldProjector.java
@@ -3,7 +3,10 @@ package org.twins.core.featurer.classfield.projector;
 import lombok.extern.slf4j.Slf4j;
 import org.cambium.common.exception.ServiceException;
 import org.cambium.featurer.annotations.FeaturerType;
+import org.twins.core.dao.twin.TwinEntity;
+import org.twins.core.dao.twinclass.TwinClassFieldEntity;
 import org.twins.core.featurer.FeaturerTwins;
+import org.twins.core.featurer.fieldtyper.value.FieldValue;
 
 import java.util.HashMap;
 import java.util.Properties;
@@ -13,10 +16,10 @@ import java.util.Properties;
         description = "Field projections")
 @Slf4j
 public abstract class FieldProjector extends FeaturerTwins {
-    public void project(HashMap<String, String> fieldProjectionParams) throws ServiceException {
+    public FieldValue project(HashMap<String, String> fieldProjectionParams, TwinClassFieldEntity field, TwinEntity entity) throws ServiceException {
         Properties properties = featurerService.extractProperties(this, fieldProjectionParams, new HashMap<>());
-        project(properties);
+        return project(properties, field, entity);
     }
 
-    protected abstract void project(Properties properties) throws ServiceException;
+    protected abstract FieldValue project(Properties properties, TwinClassFieldEntity field, TwinEntity entity) throws ServiceException;
 }
