ad4a7dc7eea24647a1ff2339b62c1418cb2541a6
Kiryl Kurylenka <158213072+KevinORIS@users.noreply.github.com>
2025-12-04 14:39:36 +0300

    TWINS-599: add projector featurers (#762)

---
+++ core/src/main/java/org/twins/core/featurer/classfield/projector/FieldProjectorFetchDecimalToStringV1.java
@@ -0,0 +1,25 @@
+package org.twins.core.featurer.classfield.projector;
+
+import lombok.extern.slf4j.Slf4j;
+import org.cambium.common.exception.ServiceException;
+import org.cambium.featurer.annotations.Featurer;
+import org.springframework.stereotype.Component;
+import org.twins.core.dao.twin.TwinEntity;
+import org.twins.core.dao.twinclass.TwinClassFieldEntity;
+import org.twins.core.featurer.FeaturerTwins;
+import org.twins.core.featurer.fieldtyper.value.FieldValue;
+
+import java.util.Properties;
+
+@Slf4j
+@Component
+@Featurer(id = FeaturerTwins.ID_4414,
+        name = "Field projection fetch decimal to string v1",
+        description = "")
+public class FieldProjectorFetchDecimalToStringV1 extends FieldProjector {
+
+    @Override
+    protected FieldValue project(Properties properties, TwinClassFieldEntity field, TwinEntity entity) throws ServiceException {
+        return null;
+    }
+}
---
+++ core/src/main/java/org/twins/core/featurer/classfield/projector/FieldProjectorStringToDataListV3.java
@@ -0,0 +1,25 @@
+package org.twins.core.featurer.classfield.projector;
+
+import lombok.extern.slf4j.Slf4j;
+import org.cambium.common.exception.ServiceException;
+import org.cambium.featurer.annotations.Featurer;
+import org.springframework.stereotype.Component;
+import org.twins.core.dao.twin.TwinEntity;
+import org.twins.core.dao.twinclass.TwinClassFieldEntity;
+import org.twins.core.featurer.FeaturerTwins;
+import org.twins.core.featurer.fieldtyper.value.FieldValue;
+
+import java.util.Properties;
+
+@Slf4j
+@Component
+@Featurer(id = FeaturerTwins.ID_4403,
+        name = "Field projection string to data list v3",
+        description = "")
+public class FieldProjectorStringToDataListV3 extends FieldProjector {
+
+    @Override
+    protected FieldValue project(Properties properties, TwinClassFieldEntity field, TwinEntity entity) throws ServiceException {
+        return null;
+    }
+}
