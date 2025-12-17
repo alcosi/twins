a579f679dc17768ef3b0df34f122ad6b47e20f86
Turkov.Aleksey <alextur94@gmail.com>
2025-12-17 11:12:25 +0300

    TWINS-613: rename param for notifier 2

--- core/src/main/java/org/twins/core/featurer/notificator/notifier/Notifier.java
+++ core/src/main/java/org/twins/core/featurer/notificator/notifier/Notifier.java
@@ -17,20 +17,20 @@ import java.util.concurrent.ConcurrentHashMap;
 @Slf4j
 public abstract class Notifier extends FeaturerTwins {
     @FeaturerParam(name = "Throw exception on null values", description = "", order = 1, defaultValue = "true")
-    public static final FeaturerParamBoolean throwExceptionONNullValues = new FeaturerParamBoolean("throwExceptionONNullValues");
+    public static final FeaturerParamBoolean throwExceptionOnNullValues = new FeaturerParamBoolean("throwExceptionOnNullValues");
 
     protected final Map<String, Object> stubCache = new ConcurrentHashMap<>();
 
     public void notify(Set<UUID> recipientIds, Map<String, String> context, String eventCode, HashMap<String, String> notifierParams) throws ServiceException {
         Properties properties = featurerService.extractProperties(this, notifierParams, new HashMap<>());
-        validateContext(context, throwExceptionONNullValues.extract(properties));
+        validateContext(context, throwExceptionOnNullValues.extract(properties));
         notify(recipientIds, context, eventCode, properties);
     }
 
-    protected void validateContext(Map<String, String> context, boolean throwExceptionONNullValues) throws ServiceException {
+    protected void validateContext(Map<String, String> context, boolean throwExceptionOnNullValues) throws ServiceException {
         for (Map.Entry<String, String> entry : context.entrySet()) {
             if (entry.getValue() == null) {
-                if (throwExceptionONNullValues)
+                if (throwExceptionOnNullValues)
                     throw new ServiceException(ErrorCodeTwins.NOTIFICATION_CONTEXT_COLLECTOR_ERROR, "Entry in contect with key[" + entry.getKey() + "] has null value");
                 context.remove(entry.getKey());
             }
