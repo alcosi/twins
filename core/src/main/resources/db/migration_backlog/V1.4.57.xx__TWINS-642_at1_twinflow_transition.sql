c10d0535d2f633ede04ad8dd0a9b05149b273725
Turkov.Aleksey <alextur94@gmail.com>
2025-12-19 13:52:46 +0300

    TWINS-642: add new type for transitions

--- core/src/main/resources/db/migration_backlog/V1.4.57.xx__TWINS-642_at1_twinflow_transition.sql
+++ core/src/main/resources/db/migration_backlog/V1.4.57.xx__TWINS-642_at1_twinflow_transition.sql
@@ -6,4 +6,6 @@ alter table twinflow_transition
             on update cascade on delete cascade;
 
 
-INSERT INTO i18n_type (id, name) VALUES ('twinflowTransitionMessage', 'Twinflow transition message');
+INSERT INTO i18n_type (id, name) VALUES ('twinflowTransitionMessage', 'Twinflow transition message') on conflict on constraint i18n_type_pk do nothing ;
+
+INSERT INTO twinflow_transition_type (id, description) VALUES ('OPERATION_DISABLE', 'operation disable') on conflict on constraint twinflow_transition_type_pkey do nothing ;
