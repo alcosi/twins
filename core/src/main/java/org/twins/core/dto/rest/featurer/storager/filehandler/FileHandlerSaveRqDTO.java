8841042e01d2b0dc6824169d465259ff2dbe1102
Egor Chernooky <e.chernooky@gmail.com>
2025-12-20 12:30:11 +0300

    TWINS-589: added filename to FH rqs

--- core/src/main/java/org/twins/core/dto/rest/featurer/storager/filehandler/FileHandlerSaveRqDTO.java
+++ core/src/main/java/org/twins/core/dto/rest/featurer/storager/filehandler/FileHandlerSaveRqDTO.java
@@ -2,4 +2,4 @@ package org.twins.core.dto.rest.featurer.storager.filehandler;
 
 import org.twins.core.enums.featurer.storager.StorageType;
 
-public record FileHandlerSaveRqDTO(String id, String type, byte[] fileBytes, StorageType storageType, String storageDir) {}
+public record FileHandlerSaveRqDTO(String id, String fileName, String type, byte[] fileBytes, StorageType storageType, String storageDir) {}
