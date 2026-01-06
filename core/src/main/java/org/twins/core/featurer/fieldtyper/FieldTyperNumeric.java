e4c4ab8e73e2d83c56c33d711006f7ca3bdf547b
KevinORIS <c.s.2015@mail.ru>
2026-01-06 12:40:06 +0300

    TWINS-570: small fix 2

--- core/src/main/java/org/twins/core/featurer/fieldtyper/FieldTyperNumeric.java
+++ core/src/main/java/org/twins/core/featurer/fieldtyper/FieldTyperNumeric.java
@@ -49,9 +49,9 @@ public class FieldTyperNumeric extends FieldTyperSimple<FieldDescriptorNumeric, 
     public static final FeaturerParamString decimalSeparator = new FeaturerParamString("decimalSeparator");
     @FeaturerParam(name = "Decimal places", description = "Number of decimal places.", order = 6)
     public static final FeaturerParamInt decimalPlaces = new FeaturerParamInt("decimalPlaces");
-    @FeaturerParam(name = "Extra thousand separators", description = "Extra thousand separators. Must not be equal to decimal separator.", order = 4, optional = true)
+    @FeaturerParam(name = "Extra thousand separators", description = "Extra thousand separators. Must not be equal to decimal separator.", order = 7, optional = true)
     public static final FeaturerParamStringSet extraThousandSeparatorSet = new FeaturerParamStringSet("extraThousandSeparatorSet");
-    @FeaturerParam(name = "Extra decimal separators", description = "Extra decimal separators. Must not be equal to thousand separator.", order = 5, optional = true)
+    @FeaturerParam(name = "Extra decimal separators", description = "Extra decimal separators. Must not be equal to thousand separator.", order = 8, optional = true)
     public static final FeaturerParamStringSet extraDecimalSeparatorSet = new FeaturerParamStringSet("extraDecimalSeparatorSet");
     @FeaturerParam(name = "Round", description = "Round a number to the required number of decimal places", order = 9, optional = true, defaultValue = "true")
     public static final FeaturerParamBoolean round = new FeaturerParamBoolean("round");
