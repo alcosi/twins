f1124e15327f64543d2f51dbc2c274c002ccb143
Alexander Kuzniatsou <a.kuzniatsou@alcosi.eu>
2026-01-12 20:47:32 +0300

    TWINS-589-1: [core] remove deprecated StoragerAlcosiFileHandlerV3 and related migration; enhance fileHandlerUploadPath support in StoragerAlcosiFileHandlerV2

--- core/src/main/resources/db/migration_backlog/v1.4.94.xx__TWINS-589-1_ec1_featurer.sql
+++
@@ -1,8 +0,0 @@
--- for the first version of fh storager
-update featurer
-set deprecated = true
-where id=2906;
-
-insert into featurer (id, featurer_type_id, class, name, description, deprecated)
-values (2908, 29, 'org.twins.core.featurer.storager.filehandler.StoragerAlcosiFileHandlerV3', 'StoragerAlcosiFileHandlerV3', 'Service to save or delete files via file-handler service (async api with multipart)', false)
-on conflict do nothing;
