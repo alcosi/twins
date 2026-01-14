e5895f4157b3beb8906ca3f3b64d93aa8db1d555
Artem Bakun <a.bakun@alcosi.eu>
2026-01-14 21:38:41 +0300

    TWINS-650: refactoring code

--- core/src/main/java/org/twins/core/mappers/rest/notification/HistoryNotificationRecipientCollectorDTOMapperV1.java
+++ core/src/main/java/org/twins/core/mappers/rest/notification/HistoryNotificationRecipientCollectorDTOMapperV1.java
@@ -1,5 +1,6 @@
 package org.twins.core.mappers.rest.notification;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.twins.core.controller.rest.annotation.MapperModeBinding;
 import org.twins.core.dao.notification.HistoryNotificationRecipientCollectorEntity;
@@ -8,9 +9,12 @@ import org.twins.core.mappers.rest.RestSimpleDTOMapper;
 import org.twins.core.mappers.rest.mappercontext.MapperContext;
 import org.twins.core.mappers.rest.mappercontext.modes.HistoryNotificationRecipientCollectorMode;
 
+@RequiredArgsConstructor
 @MapperModeBinding(modes = HistoryNotificationRecipientCollectorMode.class)
 @Component
 public class HistoryNotificationRecipientCollectorDTOMapperV1 extends RestSimpleDTOMapper<HistoryNotificationRecipientCollectorEntity, HistoryNotificationRecipientCollectorDTOv1> {
+    private final HistoryNotificationRecipientDTOMapperV1 historyNotificationRecipientDTOMapperV1;
+
     @Override
     public void map(HistoryNotificationRecipientCollectorEntity src, HistoryNotificationRecipientCollectorDTOv1 dst, MapperContext mapperContext) throws Exception {
         switch (mapperContext.getModeOrUse(HistoryNotificationRecipientCollectorMode.DETAILED)) {
@@ -26,6 +30,20 @@ public class HistoryNotificationRecipientCollectorDTOMapperV1 extends RestSimple
                             .setId(src.getId())
                             .setRecipientId(src.getHistoryNotificationRecipientId())
                             .setExclude(src.getExclude());
+        }
+
+        if (mapperContext.hasModeButNot(HistoryNotificationRecipientCollectorMode.HistoryNotificationRecipient2HistoryNotificationRecipientCollectorMode.HIDE)) {
+            dst.setRecipientId(src.getHistoryNotificationRecipientId());
+
+            historyNotificationRecipientDTOMapperV1.postpone(src.getHistoryNotificationRecipientEntity(),
+                    mapperContext.forkOnPoint(mapperContext.getModeOrUse(HistoryNotificationRecipientCollectorMode.HistoryNotificationRecipient2HistoryNotificationRecipientCollectorMode.SHORT)));
+        }
+
+        if (mapperContext.hasModeButNot(HistoryNotificationRecipientCollectorMode.HistoryNotificationRecipientResolverFeaturer2HistoryNotificationRecipientCollectorMode.HIDE)) {
+            dst.setRecipientResolverFeaturerId(src.getRecipientResolverFeaturerId());
+
+            historyNotificationRecipientDTOMapperV1.postpone(src.getHistoryNotificationRecipientEntity(),
+                    mapperContext.forkOnPoint(mapperContext.getModeOrUse(HistoryNotificationRecipientCollectorMode.HistoryNotificationRecipientResolverFeaturer2HistoryNotificationRecipientCollectorMode.SHORT)));
         }
     }
 }
