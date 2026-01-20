83d60b19fb066635be05e222fafd7299a430735a
Egor Chernooky <72251104+blooond@users.noreply.github.com>
2026-01-21 00:46:51 +0300

    TWINS-611: SchedulerLogSearch & SchedulerSearch refactoring

--- core/src/main/java/org/twins/core/mappers/rest/scheduler/SchedulerLogSearchRestDTOReverseMapper.java
+++ core/src/main/java/org/twins/core/mappers/rest/scheduler/SchedulerLogSearchRestDTOReverseMapper.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component;
 import org.twins.core.domain.search.SchedulerLogSearch;
 import org.twins.core.dto.rest.scheduler.SchedulerLogSearchDTOv1;
 import org.twins.core.mappers.rest.DataTimeRangeDTOReverseMapper;
+import org.twins.core.mappers.rest.LongRangeDTOReverseMapper;
 import org.twins.core.mappers.rest.RestSimpleDTOMapper;
 import org.twins.core.mappers.rest.mappercontext.MapperContext;
 
@@ -13,6 +14,7 @@ import org.twins.core.mappers.rest.mappercontext.MapperContext;
 public class SchedulerLogSearchRestDTOReverseMapper extends RestSimpleDTOMapper<SchedulerLogSearchDTOv1, SchedulerLogSearch> {
 
     private final DataTimeRangeDTOReverseMapper dateMapper;
+    private final LongRangeDTOReverseMapper longRangeMapper;
 
     @Override
     public void map(SchedulerLogSearchDTOv1 src, SchedulerLogSearch dst, MapperContext mapperContext) throws Exception {
@@ -24,6 +26,6 @@ public class SchedulerLogSearchRestDTOReverseMapper extends RestSimpleDTOMapper<
                 .setResultLikeSet(src.getResultLikeSet())
                 .setResultNotLikeSet(src.getResultNotLikeSet())
                 .setCreatedAt(dateMapper.convert(src.getCreatedAt()))
-                .setExecutionTimeRange(src.getExecutionTimeRange());
+                .setExecutionTimeRange(longRangeMapper.convert(src.getExecutionTimeRange()));
     }
 }
