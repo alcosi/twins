b705817c303c100c76d740283f306e435b855c7d
Artem Bakun <a.bakun@alcosi.eu>
2026-02-06 19:24:41 +0300

    TWINS-661: fix DTO mistake

--- core/src/main/java/org/twins/core/dto/rest/factory/FactoryConditionSetSaveDTOv1.java
+++ core/src/main/java/org/twins/core/dto/rest/factory/FactoryConditionSetSaveDTOv1.java
@@ -20,7 +20,7 @@ public class FactoryConditionSetSaveDTOv1 {
     @Schema(description = "description", example = DTOExamples.DESCRIPTION)
     public String description;
 
-    @Schema(name = "twin factory id")
+    @Schema(description = "twin factory id")
     private UUID twinFactoryId;
 
 }
