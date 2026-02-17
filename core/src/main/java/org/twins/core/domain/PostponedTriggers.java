6143d2a75f49b0837d300d55ef2387f5afb2e98b
KevinORIS <c.s.2015@mail.ru>
2026-02-17 08:59:53 +0300

    TWINS-715: ArrayList to HashSet

--- core/src/main/java/org/twins/core/domain/PostponedTriggers.java
+++ core/src/main/java/org/twins/core/domain/PostponedTriggers.java
@@ -2,11 +2,11 @@ package org.twins.core.domain;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.UUID;
 
 @Slf4j
-public class PostponedTriggers extends ArrayList<PostponedTriggers.PostponedTrigger> {
+public class PostponedTriggers extends HashSet<PostponedTriggers.PostponedTrigger> {
     public void add(UUID twinId, UUID statusId, UUID triggerId) {
         log.info("Adding postponed trigger[{}] for twin[{}]", triggerId, twinId);
         add(new PostponedTrigger(twinId, statusId, triggerId));
