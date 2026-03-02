786392c1e93f9da6058869268b44a839f9e669c5
Artem Bakun <a.bakun@alcosi.eu>
2026-03-02 13:39:11 +0300

    TWIN-716: add related object for twin validator set

--- core/src/main/java/org/twins/core/mappers/rest/twinclass/TwinClassDynamicMarkerDTOMapper.java
+++ core/src/main/java/org/twins/core/mappers/rest/twinclass/TwinClassDynamicMarkerDTOMapper.java
@@ -13,6 +13,11 @@ import org.twins.core.mappers.rest.mappercontext.MapperContext;
 import org.twins.core.mappers.rest.mappercontext.modes.DataListOptionMode;
 import org.twins.core.mappers.rest.mappercontext.modes.TwinClassDynamicMarkerMode;
 import org.twins.core.mappers.rest.mappercontext.modes.TwinClassMode;
+import org.twins.core.mappers.rest.mappercontext.modes.TwinValidatorSetMode;
+import org.twins.core.mappers.rest.validator.TwinValidatorSetRestDTOMapper;
+import org.twins.core.service.twinclass.TwinClassDynamicMarkerService;
+
+import java.util.Collection;
 
 @Slf4j
 @Component
@@ -24,6 +29,10 @@ public class TwinClassDynamicMarkerDTOMapper extends RestSimpleDTOMapper<TwinCla
     private final TwinClassRestDTOMapper twinClassRestDTOMapper;
     @MapperModePointerBinding(modes = {DataListOptionMode.TwinClassDynamicMarker2DataListOptionMode.class})
     private final DataListOptionRestDTOMapper dataListOptionRestDTOMapper;
+    @MapperModePointerBinding(modes = {TwinValidatorSetMode.TwinClassDynamicMarker2TwinValidatorSetMode.class})
+    private final TwinValidatorSetRestDTOMapper twinValidatorSetRestDTOMapper;
+
+    private final TwinClassDynamicMarkerService twinClassDynamicMarkerService;
 
     @Override
     public void map(TwinClassDynamicMarkerEntity src, TwinClassDynamicMarkerDTOv1 dst, MapperContext mapperContext) throws Exception {
@@ -51,6 +60,21 @@ public class TwinClassDynamicMarkerDTOMapper extends RestSimpleDTOMapper<TwinCla
         if (mapperContext.hasModeButNot(DataListOptionMode.TwinClassDynamicMarker2DataListOptionMode.HIDE)) {
             dst.setMarkerDataListOptionId(src.getMarkerDataListOptionId());
             dataListOptionRestDTOMapper.postpone(src.getMarkerDataListOption(), mapperContext.forkOnPoint(DataListOptionMode.TwinClassDynamicMarker2DataListOptionMode.SHORT));
+        }
+        if (mapperContext.hasModeButNot(TwinValidatorSetMode.TwinClassDynamicMarker2TwinValidatorSetMode.HIDE)) {
+            dst.setTwinValidatorSetId(src.getTwinValidatorSetId());
+
+            twinClassDynamicMarkerService.loadTwinValidatorSet(src);
+            twinValidatorSetRestDTOMapper.postpone(src.getTwinValidatorSet(),
+                    mapperContext.forkOnPoint(mapperContext.getModeOrUse(TwinValidatorSetMode.TwinClassDynamicMarker2TwinValidatorSetMode.SHORT)));
+        }
+    }
+
+    @Override
+    public void beforeCollectionConversion(Collection<TwinClassDynamicMarkerEntity> srcCollection, MapperContext mapperContext) throws Exception {
+        super.beforeCollectionConversion(srcCollection, mapperContext);
+        if (mapperContext.hasModeButNot(TwinValidatorSetMode.TwinClassDynamicMarker2TwinValidatorSetMode.HIDE)) {
+            twinClassDynamicMarkerService.loadTwinValidatorSet(srcCollection);
         }
     }
 }
