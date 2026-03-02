d595955916821318f36cb8c42cf84e3bb110f9f0
Artsiom Bakun <159791597+artbakun@users.noreply.github.com>
2026-03-02 14:56:03 +0300

    TWIN-716: add related object for twin validator set (#896)

--- core/src/main/java/org/twins/core/dto/rest/twinclass/TwinClassDynamicMarkerDTOv1.java
+++ core/src/main/java/org/twins/core/dto/rest/twinclass/TwinClassDynamicMarkerDTOv1.java
@@ -6,6 +6,7 @@ import lombok.experimental.Accessors;
 import org.twins.core.dto.rest.DTOExamples;
 import org.twins.core.dto.rest.datalist.DataListOptionDTOv1;
 import org.twins.core.dto.rest.related.RelatedObject;
+import org.twins.core.dto.rest.validator.TwinValidatorSetDTOv1;
 
 import java.util.UUID;
 
@@ -22,6 +23,7 @@ public class TwinClassDynamicMarkerDTOv1 {
     public UUID twinClassId;
 
     @Schema(description = "twin validator set id")
+    @RelatedObject(type = TwinValidatorSetDTOv1.class, name = "twinValidatorSet")
     public UUID twinValidatorSetId;
 
     @Schema(description = "marker data list option id")
