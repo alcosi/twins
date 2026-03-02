1b07460b3c84bdc3a6ecdc856ce577951d619aed
Nikita <33869035+nikitamekh@users.noreply.github.com>
2026-03-02 17:46:30 +0300

    TWINS-741: refactor and consolidate UserGroupMap

--- core/src/main/java/org/twins/core/featurer/usergroup/slugger/SluggerDomainAndBusinessAccountScopeBusinessAccountManage.java
+++ core/src/main/java/org/twins/core/featurer/usergroup/slugger/SluggerDomainAndBusinessAccountScopeBusinessAccountManage.java
@@ -6,13 +6,19 @@ import org.cambium.common.exception.ServiceException;
 import org.cambium.featurer.annotations.Featurer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.twins.core.dao.user.*;
-import org.twins.core.domain.ApiUser;
+import org.twins.core.dao.user.UserEntity;
+import org.twins.core.dao.user.UserGroupEntity;
+import org.twins.core.dao.user.UserGroupRepository;
+import org.twins.core.dao.user.UserGroupTypeEntity;
+import org.twins.core.dao.usergroup.UserGroupMapEntity;
+import org.twins.core.dao.usergroup.UserGroupMapRepository;
 import org.twins.core.featurer.FeaturerTwins;
 
 import java.sql.Timestamp;
 import java.time.Instant;
-import java.util.*;
+import java.util.List;
+import java.util.Properties;
+import java.util.UUID;
 
 @Slf4j
 @Component
@@ -20,29 +26,15 @@ import java.util.*;
         name = "Domain and business account scope / business account manage",
         description = "")
 @RequiredArgsConstructor
-public class SluggerDomainAndBusinessAccountScopeBusinessAccountManage extends Slugger<UserGroupMapType1Entity> {
+public class SluggerDomainAndBusinessAccountScopeBusinessAccountManage extends Slugger {
     @Autowired
     UserGroupRepository userGroupRepository;
 
     @Autowired
-    UserGroupMapType1Repository userGroupMapType1Repository;
+    UserGroupMapRepository userGroupMapRepository;
 
     @Override
-    protected boolean checkConfig(Properties properties, UserGroupMapType1Entity userGroupMapEntity) throws ServiceException {
-        return check(userGroupMapEntity.getUserGroup(), true, true);
-    }
-
-    @Override
-    protected List<? extends UserGroupMap> getGroups(Properties properties, Set<UUID> userIds) throws ServiceException {
-        ApiUser apiUser = authService.getApiUser();
-        return userGroupMapType1Repository.findByUserIdInAndUserGroup_BusinessAccountIdAndUserGroup_DomainId(
-                userIds,
-                apiUser.getBusinessAccountId(),
-                apiUser.getDomainId());
-    }
-
-    @Override
-    protected UserGroupMap enterGroup(Properties properties, UserEntity user, UserGroupEntity userGroup) throws ServiceException {
+    protected UserGroupMapEntity enterGroup(Properties properties, UserEntity user, UserGroupEntity userGroup) throws ServiceException {
         if (!checkBusinessAccountCompatability(userGroup.getBusinessAccountId())) {
             log.warn("{} can not be entered by {}", userGroup.logNormal(), user);
             return null;
@@ -52,7 +44,7 @@ public class SluggerDomainAndBusinessAccountScopeBusinessAccountManage extends S
             return null;
         }
         var apiUser = authService.getApiUser();
-        return new UserGroupMapType1Entity()
+        return new UserGroupMapEntity()
                 .setUserGroupId(userGroup.getId())
                 .setUserGroup(userGroup)
                 .setUserId(user.getId())
@@ -66,11 +58,11 @@ public class SluggerDomainAndBusinessAccountScopeBusinessAccountManage extends S
     protected boolean exitGroup(Properties properties, UserEntity user, UserGroupEntity userGroup) throws ServiceException {
         //todo no need for select, cause data is already loaded in user
         var apiUser = authService.getApiUser();
-        UserGroupMapType1Entity entityToDelete = userGroupMapType1Repository
+        UserGroupMapEntity entityToDelete = userGroupMapRepository
                 .findByUserIdAndUserGroupIdAndUserGroup_BusinessAccountIdAndUserGroup_DomainId(user.getId(), userGroup.getId(), apiUser.getBusinessAccountId(), apiUser.getDomainId());
         if (entityToDelete == null)
             return false;
-        entitySmartService.deleteAndLog(entityToDelete.getId(), userGroupMapType1Repository);
+        entitySmartService.deleteAndLog(entityToDelete.getId(), userGroupMapRepository);
         return true;
     }
 
@@ -91,11 +83,4 @@ public class SluggerDomainAndBusinessAccountScopeBusinessAccountManage extends S
     protected void processBusinessAccountDeletion(Properties properties) throws ServiceException {
 
     }
-
-    @Override
-    protected Set<UUID> getUsers(Properties properties, UUID domainId, UUID businessAccountId, Collection<UUID> userGroupIds) throws ServiceException {
-        return userGroupMapType1Repository.findUserIdsByUserGroupIdsAndDomainIdAndBusinessAccountId(domainId, businessAccountId, userGroupIds);
-    }
-
-
 }
