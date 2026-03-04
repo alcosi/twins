8d3b3575b703cb9ce172ac2a940f99121d4d7457
nikita.mekh <n.mekh@hutkigrosh.by>
2026-03-04 16:09:36 +0300

    TWINS-734: set default value for `viewPermissionCustom` in TwinEntity

--- core/src/main/resources/db/migration_backlog/V1.4.178.03__TWINS-735_ak1_fixes.sql
+++ core/src/main/resources/db/migration_backlog/V1.4.178.03__TWINS-735_ak1_fixes.sql
@@ -1,9 +1,10 @@
+UPDATE twin SET view_permission_custom = false WHERE view_permission_id is null;
+
 UPDATE twin t
 SET view_permission_id = tc.view_permission_id
 FROM twin_class tc
 WHERE not t.view_permission_custom and t.twin_class_id = tc.id and t.view_permission_id is null;
 
-UPDATE twin SET view_permission_custom = false WHERE view_permission_id is null;
 ALTER TABLE twin ALTER COLUMN view_permission_custom SET NOT NULL;
 
 create or replace function twin_before_insert_wrapper() returns trigger
