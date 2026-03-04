935b54704d7a4e2261b5d3ec168d3683ea7fd550
nikita.mekh <n.mekh@hutkigrosh.by>
2026-03-04 17:20:18 +0300

    TWINS-735: fix scheduler id

--- core/src/main/resources/db/migration_backlog/V1.4.178.02__TWINS-735_ak1_scheduler.sql
+++ core/src/main/resources/db/migration_backlog/V1.4.178.02__TWINS-735_ak1_scheduler.sql
@@ -2,4 +2,4 @@ INSERT INTO featurer (id, featurer_type_id, class, name, description, deprecated
 
 insert into scheduler(id, domain_id, scheduler_featurer_id, scheduler_params, active, log_enabled, cron, fixed_rate, description, created_at, updated_at)
 values
-    ('00000000-0000-0000-0015-000000000015', null, 5015, null, true, true, '10 0 0 * * *', null, 'Scheduler to check user group map involves count is not less then 0', now(), now()) on conflict do nothing;
+    ('00000000-0000-0000-0015-000000000016', null, 5015, null, true, true, '10 0 0 * * *', null, 'Scheduler to check user group map involves count is not less then 0', now(), now()) on conflict do nothing;
