a9e865ebc2664faf81942af5f031474d2cd7c110
nikita.mekh <n.mekh@hutkigrosh.by>
2026-03-05 15:46:05 +0300

    TWINS-735: schedulers descriptions fix

--- core/src/main/resources/db/migration_backlog/V1.4.178.03__TWINS-734_nm1_twin_counter_fix.sql
+++ core/src/main/resources/db/migration_backlog/V1.4.178.03__TWINS-734_nm1_twin_counter_fix.sql
@@ -12,3 +12,9 @@ BEGIN
 END;
 $$;
 
+-- fixing incorrect descriptions
+UPDATE scheduler SET cron = '6 0 * * * *', description = 'Scheduler to check permission materialization user groups grants count is not less then 0' WHERE id = '00000000-0000-0000-0015-000000000012';
+UPDATE scheduler SET cron = '5 0 * * * *', description = 'Scheduler to check permission materialization global grants count is not less then 0' WHERE id = '00000000-0000-0000-0015-000000000011';
+UPDATE scheduler SET cron = '9 0 * * * *', description = 'Scheduler to check permission materialization space user grants count is not less then 0' WHERE id = '00000000-0000-0000-0015-000000000015';
+UPDATE scheduler SET cron = '7 0 * * * *', description = 'Scheduler to check permission schema detect mismatches in twin' WHERE id = '00000000-0000-0000-0015-000000000013';
+UPDATE scheduler SET cron = '8 0 * * * *', description = 'Scheduler to check permission materialization space user groups grants count is not less then 0' WHERE id = '00000000-0000-0000-0015-000000000014';
