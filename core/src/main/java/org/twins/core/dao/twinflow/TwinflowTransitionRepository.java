16b1431cd78ab95153caeb5c446e6710e0e415fa
Artsiom Bakun <159791597+artbakun@users.noreply.github.com>
2026-03-18 14:20:57 +0300

    TWINS-745: fix find transitions

--- core/src/main/java/org/twins/core/dao/twinflow/TwinflowTransitionRepository.java
+++ core/src/main/java/org/twins/core/dao/twinflow/TwinflowTransitionRepository.java
@@ -21,7 +21,7 @@ public interface TwinflowTransitionRepository extends CrudRepository<TwinflowTra
         This logic can be done with postgres sql "distinct on" operator, but it's not supported in hibernate
         */
     @Query(value = "select tt from TwinflowTransitionEntity tt where tt.twinflowId = :twinflowId and (tt.srcTwinStatusId = :srcTwinStatusId or (tt.srcTwinStatusId is null and tt.dstTwinStatusId != :srcTwinStatusId)) " +
-            " and true = function('permission_check_mater', :permissionSchemaId, tt.permissionId, :permissionSpaceId, :userId, :userGroupFootprintId, :twinClassId, :isAssignee, :isCreator)")
+            " and true = function('permission_check_mater', :permissionSchemaId, tt.permissionId, :permissionSpaceId, :userId, :userGroupFootprintId, :twinClassId, :isCreator, :isAssignee)")
     List<TwinflowTransitionEntity> findValidTransitions(
             @Param("twinflowId") UUID twinflowId,
             @Param("srcTwinStatusId") UUID srcTwinStatusId,
@@ -34,7 +34,7 @@ public interface TwinflowTransitionRepository extends CrudRepository<TwinflowTra
             @Param("isCreator") boolean isCreator);
 
     @Query(value = "select tt from TwinflowTransitionEntity tt where tt.id = :transitionId " +
-            " and true = function('permission_check_mater', :permissionSchemaId, tt.permissionId, :permissionSpaceId, :userId, :userGroupFootprintId, :twinClassId, :isAssignee, :isCreator)")
+            " and true = function('permission_check_mater', :permissionSchemaId, tt.permissionId, :permissionSpaceId, :userId, :userGroupFootprintId, :twinClassId, :isCreator, :isAssignee)")
     TwinflowTransitionEntity findTransition(
             @Param("transitionId") UUID transitionId,
             @Param("permissionSchemaId") UUID permissionSchemaId,
@@ -49,7 +49,7 @@ public interface TwinflowTransitionRepository extends CrudRepository<TwinflowTra
             "where tt.twinflowId = :twinflowId and (tt.srcTwinStatusId = :srcTwinStatusId or (tt.srcTwinStatusId is null and tt.dstTwinStatusId != :srcTwinStatusId)) " +
             "and tt.twinflowTransitionAlias.alias = :alias " +
             "and tt.twinflowTransitionTypeId != :twinflowTransitionType " +
-            "and true = function('permission_check_mater', :permissionSchemaId, tt.permissionId, :permissionSpaceId, :userId, :userGroupFootprintId, :twinClassId, :isAssignee, :isCreator)")
+            "and true = function('permission_check_mater', :permissionSchemaId, tt.permissionId, :permissionSpaceId, :userId, :userGroupFootprintId, :twinClassId, :isCreator, :isAssignee)")
     TwinflowTransitionEntity findTransitionByAlias(
             @Param("twinflowId") UUID twinflowId,
             @Param("srcTwinStatusId") UUID srcTwinStatusId,
