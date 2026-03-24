f8029ca7496a524b5f9d2ec7db76afa05d2d382c
nikita.mekh <n.mekh@hutkigrosh.by>
2026-03-24 17:03:26 +0300

    TWINS-772: add support for DataListOptionExternalId condition descriptor

--- core/src/main/java/org/twins/core/featurer/FeaturerTwins.java
+++ core/src/main/java/org/twins/core/featurer/FeaturerTwins.java
@@ -343,6 +343,7 @@ public abstract class FeaturerTwins extends Featurer {
     public static final int ID_4414 = 4414;
     public static final int ID_4501 = 4501;
     public static final int ID_4502 = 4502;
+    public static final int ID_4503 = 4503;
     public static final int ID_4601 = 4601;
     public static final int ID_4602 = 4602;
     public static final int ID_4603 = 4603;
---
+++ core/src/main/resources/db/migration_backlog/V1.4.xx.01__TWINS-772_nm1_condition_evaluator_option_ext_id.sql
@@ -0,0 +1,2 @@
+INSERT INTO featurer (id, featurer_type_id, class, name, description, deprecated)
+ VALUES (4503, 45, '', '', '', false) on conflict do nothing;
