7e71b0345f5ed32fffa74eb8d033105849b73685
KevinORIS <c.s.2015@mail.ru>
2026-03-26 16:07:47 +0300

    TWINS-738: minor fix

--- core/src/main/resources/db/migration/V1.4.203.01__TWINS-738_kk1_fix_scheduler_featurer_ids.sql
+++ core/src/main/resources/db/migration/V1.4.203.01__TWINS-738_kk1_fix_scheduler_featurer_ids.sql
@@ -11,7 +11,7 @@ WHERE scheduler_featurer_id = 5010;
 
 -- Create new scheduler 5016 for SchedulerConsistencyCheckPermissionMaterGlobal
 INSERT INTO scheduler (id, domain_id, scheduler_featurer_id, cron, active, log_enabled, scheduler_params, fixed_rate, description, created_at, updated_at)
-VALUES ('00000000-0000-0000-0015-000000000011'::uuid, NULL, 5016, '5 0 * * * *', true, true, NULL, NULL, 'Scheduler to check permission materialization global grants count is not less then 0', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)
+VALUES ('00000000-0000-0000-0015-000000000017'::uuid, NULL, 5016, '5 0 * * * *', true, true, NULL, NULL, 'Scheduler to check permission materialization global grants count is not less then 0', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)
 ON CONFLICT (id) DO UPDATE SET
     scheduler_featurer_id = 5016,
     cron = EXCLUDED.cron,
