7851d825b87b3a0c29ebbc4523326fc32bbb37dd
Egor Chernooky <e.chernooky@gmail.com>
2026-05-05 20:17:43 +0300

    TWINS-814: tests for templator + businessaccount + domain and moving to unit folder

---
+++ core/src/test/java/org/twins/core/unit/featurer/fieldrule/fieldoverwriter/FieldParamOverwriterStubTest.java
@@ -0,0 +1,23 @@
+package org.twins.core.featurer.fieldrule.fieldoverwriter;
+
+import org.cambium.common.exception.ServiceException;
+import org.junit.jupiter.api.Test;
+import org.twins.core.base.BaseUnitTest;
+import org.twins.core.dao.twinclass.TwinClassFieldRuleEntity;
+import org.twins.core.featurer.fieldrule.fieldoverwriter.FieldParamOverwriterStub;
+
+import java.util.Properties;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class FieldParamOverwriterStubTest extends BaseUnitTest {
+
+    private final FieldParamOverwriterStub overwriter = new FieldParamOverwriterStub();
+
+    @Test
+    void getFieldOverwriterDescriptor_anyInput_returnsNull() throws ServiceException {
+        var descriptor = overwriter.getFieldOverwriterDescriptor(new TwinClassFieldRuleEntity(), new Properties());
+
+        assertNull(descriptor);
+    }
+}
