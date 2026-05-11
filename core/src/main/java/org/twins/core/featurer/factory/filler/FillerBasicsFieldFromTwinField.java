3da093b8abfe685ccc0f08ab8c19b03744a065f0
Turkov.Aleksey <alextur94@gmail.com>
2026-05-12 01:05:41 +0300

    TWINS-820: Support saving custom target user field

--- core/src/main/java/org/twins/core/featurer/factory/filler/FillerBasicsFieldFromTwinField.java
+++ core/src/main/java/org/twins/core/featurer/factory/filler/FillerBasicsFieldFromTwinField.java
@@ -8,6 +8,7 @@ import org.cambium.featurer.params.FeaturerParamUUID;
 import org.springframework.stereotype.Component;
 import org.twins.core.dao.twin.TwinEntity;
 import org.twins.core.dao.user.UserEntity;
+import org.twins.core.domain.TwinBasicFields;
 import org.twins.core.domain.factory.FactoryItem;
 import org.twins.core.exception.ErrorCodeTwins;
 import org.twins.core.featurer.FeaturerTwins;
@@ -15,6 +16,8 @@ import org.twins.core.featurer.factory.lookuper.FieldLookuperNearest;
 import org.twins.core.featurer.fieldtyper.value.FieldValue;
 import org.twins.core.featurer.fieldtyper.value.FieldValueText;
 import org.twins.core.featurer.fieldtyper.value.FieldValueUser;
+import org.twins.core.featurer.fieldtyper.value.FieldValueUserSingle;
+import org.twins.core.featurer.params.FeaturerParamBasicsTwinBasicField;
 import org.twins.core.featurer.params.FeaturerParamUUIDTwinsTwinClassFieldId;
 import org.twins.core.service.SystemEntityService;
 
@@ -30,6 +33,9 @@ public class FillerBasicsFieldFromTwinField extends Filler {
     @FeaturerParam(name = "Field id", description = "", order = 1)
     public static final FeaturerParamUUID fieldId = new FeaturerParamUUIDTwinsTwinClassFieldId("fieldId");
 
+    @FeaturerParam(name = "Destination basics user field", description = "", order = 2, optional = true)
+    public static final FeaturerParamBasicsTwinBasicField dstBasicsUser = new FeaturerParamBasicsTwinBasicField("dstBasicsUser");
+
     @Override
     public void fill(Properties properties, FactoryItem factoryItem, TwinEntity templateTwin) throws ServiceException {
         fill(properties, factoryItem, templateTwin, fieldLookupers.getFromContextTwinDbFields());
@@ -37,12 +43,14 @@ public class FillerBasicsFieldFromTwinField extends Filler {
 
     public void fill(Properties properties, FactoryItem factoryItem, TwinEntity templateTwin, FieldLookuperNearest fieldLookuperNearest) throws ServiceException {
         TwinEntity outputTwinEntity = factoryItem.getOutput().getTwinEntity();
-        UUID fieldId = FillerBasicsFieldFromTwinField.fieldId.extract(properties);
-        FieldValue fieldValue = fieldLookuperNearest.lookupFieldValue(factoryItem, fieldId);
+        UUID sourceFieldId = fieldId.extract(properties);
+        TwinBasicFields.Basics dstUserBasic = extractDstBasicsUserOptional(properties);
+        FieldValue fieldValue = fieldLookuperNearest.lookupFieldValue(factoryItem, sourceFieldId);
         String fieldName;
         switch (fieldValue) {
-            case FieldValueText fieldValueText -> fieldName = handleTextField(fieldId, fieldValueText, outputTwinEntity);
-            case FieldValueUser fieldValueUser -> fieldName = handleUserField(fieldValueUser, outputTwinEntity, fieldValue);
+            case FieldValueText fieldValueText -> fieldName = handleTextField(sourceFieldId, fieldValueText, outputTwinEntity);
+            case FieldValueUser fieldValueUser -> fieldName = handleUserField(fieldValueUser, outputTwinEntity, fieldValue, sourceFieldId, dstUserBasic);
+            case FieldValueUserSingle fieldValueUserSingle -> fieldName = handleUserFieldSingle(fieldValueUserSingle, outputTwinEntity, fieldValue, sourceFieldId, dstUserBasic);
             default -> throw new ServiceException(
                     ErrorCodeTwins.TWIN_CLASS_FIELD_INCORRECT_TYPE,
                     fieldValue.getTwinClassField().logShort() + " is incorrect field type"
@@ -50,6 +58,17 @@ public class FillerBasicsFieldFromTwinField extends Filler {
         }
         log.info("{} with field[{}] will be filled from context {}", outputTwinEntity.logShort(), fieldName, fieldValue.getTwinClassField().logShort()
         );
+    }
+
+    private static TwinBasicFields.Basics extractDstBasicsUserOptional(Properties properties) {
+        if (!properties.containsKey(dstBasicsUser.getKey())) {
+            return null;
+        }
+        Object raw = properties.get(dstBasicsUser.getKey());
+        if (raw == null || raw.toString().isBlank()) {
+            return null;
+        }
+        return dstBasicsUser.extract(properties);
     }
 
     private String handleTextField(UUID fieldId, FieldValueText fieldValueText, TwinEntity outputTwinEntity) {
@@ -63,25 +82,67 @@ public class FillerBasicsFieldFromTwinField extends Filler {
         return null;
     }
 
-    private String handleUserField(FieldValueUser fieldValueUser, TwinEntity outputTwinEntity, FieldValue fieldValue) throws ServiceException {
+    private String handleUserField(FieldValueUser fieldValueUser, TwinEntity outputTwinEntity, FieldValue fieldValue, UUID sourceFieldId, TwinBasicFields.Basics dstUserBasic) throws ServiceException {
         if (fieldValueUser.isEmpty()) {
             throw new ServiceException(ErrorCodeTwins.TWIN_CLASS_FIELD_VALUE_REQUIRED, fieldValue.getTwinClassField().logShort() + " is not filled");
         } else if (fieldValueUser.size() > 1) {
             throw new ServiceException(ErrorCodeTwins.TWIN_CLASS_FIELD_VALUE_MULTIPLY_OPTIONS_ARE_NOT_ALLOWED, fieldValue.getTwinClassField().logShort() + " is filled by multiple users");
         } else {
             UserEntity user = fieldValueUser.getItems().getFirst();
-            if (fieldId.equals(SystemEntityService.TWIN_CLASS_FIELD_TWIN_ASSIGNEE_USER)) {
+            return mapUserToOutputTwinBasics(user, outputTwinEntity, fieldValue, sourceFieldId, dstUserBasic);
+        }
+    }
+
+    /**
+     * Base assignee / creator / owner fields use {@link org.twins.core.featurer.fieldtyper.FieldTyperBaseUserField} and deserialize as {@link FieldValueUserSingle}.
+     */
+    private String handleUserFieldSingle(FieldValueUserSingle fieldValueUserSingle, TwinEntity outputTwinEntity, FieldValue fieldValue, UUID sourceFieldId, TwinBasicFields.Basics dstUserBasic) throws ServiceException {
+        if (fieldValueUserSingle.isEmpty() || fieldValueUserSingle.getValue() == null) {
+            throw new ServiceException(ErrorCodeTwins.TWIN_CLASS_FIELD_VALUE_REQUIRED, fieldValue.getTwinClassField().logShort() + " is not filled");
+        }
+        return mapUserToOutputTwinBasics(fieldValueUserSingle.getValue(), outputTwinEntity, fieldValue, sourceFieldId, dstUserBasic);
+    }
+
+    private String mapUserToOutputTwinBasics(UserEntity user, TwinEntity outputTwinEntity, FieldValue fieldValue, UUID sourceFieldId, TwinBasicFields.Basics dstUserBasic) throws ServiceException {
+        if (dstUserBasic != null) {
+            return applyUserToOutputBasics(outputTwinEntity, user, dstUserBasic);
+        }
+        if (sourceFieldId.equals(SystemEntityService.TWIN_CLASS_FIELD_TWIN_ASSIGNEE_USER)) {
+            outputTwinEntity
+                    .setAssignerUser(user)
+                    .setAssignerUserId(user.getId());
+            return TwinEntity.Fields.assignerUserId;
+        } else if (sourceFieldId.equals(SystemEntityService.TWIN_CLASS_FIELD_TWIN_CREATOR_USER)) {
+            outputTwinEntity
+                    .setCreatedByUser(user)
+                    .setCreatedByUserId(user.getId());
+            return TwinEntity.Fields.createdByUserId;
+        }
+        throw new ServiceException(
+                ErrorCodeTwins.FACTORY_PIPELINE_STEP_ERROR,
+                "User field " + fieldValue.getTwinClassField().logShort()
+                        + ": set dstBasicsUser to assigneeUserId or createdByUserId, or use system assignee/creator field id as fieldId"
+        );
+    }
+
+    private static String applyUserToOutputBasics(TwinEntity outputTwinEntity, UserEntity user, TwinBasicFields.Basics dstUserBasic) throws ServiceException {
+        return switch (dstUserBasic) {
+            case assigneeUserId -> {
                 outputTwinEntity
                         .setAssignerUser(user)
                         .setAssignerUserId(user.getId());
-                return TwinEntity.Fields.assignerUserId;
-            } else if (fieldId.equals(SystemEntityService.TWIN_CLASS_FIELD_TWIN_CREATOR_USER)) {
+                yield TwinEntity.Fields.assignerUserId;
+            }
+            case createdByUserId -> {
                 outputTwinEntity
                         .setCreatedByUser(user)
                         .setCreatedByUserId(user.getId());
-                return TwinEntity.Fields.createdByUserId;
+                yield TwinEntity.Fields.createdByUserId;
             }
-        }
-        return null;
+            case name, description -> throw new ServiceException(
+                    ErrorCodeTwins.FACTORY_PIPELINE_STEP_ERROR,
+                    "dstBasicsUser for user fields must be assigneeUserId or createdByUserId, got: " + dstUserBasic
+            );
+        };
     }
 }
