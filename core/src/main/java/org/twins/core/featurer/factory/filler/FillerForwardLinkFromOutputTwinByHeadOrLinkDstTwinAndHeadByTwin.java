8e222049b92db1d26fa1842683973d3a1a426972
Turkov.Aleksey <alextur94@gmail.com>
2026-05-20 10:57:27 +0300

    TWINS-818: refactoring for second featurer

--- core/src/main/java/org/twins/core/featurer/factory/filler/FillerForwardLinkFromOutputTwinByHeadOrLinkDstTwinAndHeadByTwin.java
+++ core/src/main/java/org/twins/core/featurer/factory/filler/FillerForwardLinkFromOutputTwinByHeadOrLinkDstTwinAndHeadByTwin.java
@@ -4,6 +4,7 @@ import org.apache.commons.collections4.CollectionUtils;
 import org.cambium.common.exception.ServiceException;
 import org.cambium.featurer.annotations.Featurer;
 import org.cambium.featurer.annotations.FeaturerParam;
+import org.cambium.featurer.params.FeaturerParamBoolean;
 import org.cambium.featurer.params.FeaturerParamUUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
@@ -25,13 +26,12 @@ import java.util.UUID;
 @Component
 @Featurer(
         id = FeaturerTwins.ID_2351,
-        name = "Forward link from output TwinCreate link dst twin head",
-        description = "Finds link in output TwinCreate links. " +
-                "Get dst twin for this link. " +
-                "Get head of this dst twin. " +
+        name = "Forward link from output twin by (head/link) twin and head twin",
+        description = "Resolve intermediate twin via link or head. " +
+                "Get head of intermediate twin. " +
                 "Create new link of given type from current twin pointing to this head"
 )
-public class FillerForwardLinkFromOutputTwinLinkDstTwinHeadByTwin extends FillerLinks {
+public class FillerForwardLinkFromOutputTwinByHeadOrLinkDstTwinAndHeadByTwin extends FillerLinks {
 
     @Lazy
     @Autowired
@@ -43,16 +43,43 @@ public class FillerForwardLinkFromOutputTwinLinkDstTwinHeadByTwin extends Filler
     @FeaturerParam(name = "New link id", description = "", order = 1)
     public static final FeaturerParamUUID newLinksId = new FeaturerParamUUIDTwinsLinkId("newLinksId");
 
+    @FeaturerParam(name = "Head else link", description = "", order = 3, optional = true, defaultValue = "true")
+    public static final FeaturerParamBoolean headElseLink = new FeaturerParamBoolean("headElseLink");
+
     @Override
     public void fill(Properties properties, FactoryItem factoryItem, TwinEntity templateTwin) throws ServiceException {
-        UUID headHunterLinkId = headHunterLink.extract(properties);
         TwinEntity outputTwin = factoryItem.getTwin();
         if (outputTwin == null) {
             throw new ServiceException(ErrorCodeTwins.FACTORY_PIPELINE_STEP_ERROR, "Factory output twin is empty");
         }
-        if (!(factoryItem.getOutput() instanceof TwinCreate twinCreate)) {
+        if (!(factoryItem.getOutput() instanceof TwinCreate)) {
             throw new ServiceException(ErrorCodeTwins.FACTORY_PIPELINE_STEP_ERROR, "Factory output is not TwinCreate");
         }
+
+        TwinEntity intermediateTwin = resolveIntermediateTwin(properties, outputTwin, (TwinCreate) factoryItem.getOutput());
+        TwinEntity detectedHead = twinService.loadHeadForTwin(intermediateTwin);
+        if (detectedHead == null) {
+            throw new ServiceException(ErrorCodeTwins.FACTORY_PIPELINE_STEP_ERROR, "No head twin configured for " + intermediateTwin.logShort());
+        }
+        LinkEntity link = linkService.findEntitySafe(newLinksId.extract(properties));
+        TwinLinkEntity newLink = new TwinLinkEntity()
+                .setLink(link)
+                .setLinkId(link.getId())
+                .setDstTwin(detectedHead)
+                .setDstTwinId(detectedHead.getId());
+        addLink(factoryItem.getOutput(), newLink);
+    }
+
+    private TwinEntity resolveIntermediateTwin(Properties properties, TwinEntity outputTwin, TwinCreate twinCreate) throws ServiceException {
+        if (!headElseLink.extract(properties)) {
+            TwinEntity headTwin = twinService.loadHeadForTwin(outputTwin);
+            if (headTwin == null) {
+                throw new ServiceException(ErrorCodeTwins.FACTORY_PIPELINE_STEP_ERROR, "No head twin configured for " + outputTwin.logShort());
+            }
+            return headTwin;
+        }
+
+        UUID headHunterLinkId = headHunterLink.extract(properties);
         List<TwinLinkEntity> outputTwinLinks = twinCreate.getLinksEntityList();
         if (CollectionUtils.isEmpty(outputTwinLinks)) {
             throw new ServiceException(ErrorCodeTwins.FACTORY_PIPELINE_STEP_ERROR, "No links[" + headHunterLinkId + "] configured from " + outputTwin.logShort());
@@ -74,14 +101,6 @@ public class FillerForwardLinkFromOutputTwinLinkDstTwinHeadByTwin extends Filler
             }
             dstTwin = twinService.findEntitySafe(matchedLink.getDstTwinId());
         }
-
-        TwinEntity detectedHead = twinService.loadHeadForTwin(dstTwin);
-        LinkEntity link = linkService.findEntitySafe(newLinksId.extract(properties));
-        TwinLinkEntity newLink = new TwinLinkEntity()
-                .setLink(link)
-                .setLinkId(link.getId())
-                .setDstTwin(detectedHead)
-                .setDstTwinId(detectedHead.getId());
-        addLink(factoryItem.getOutput(), newLink);
+        return dstTwin;
     }
 }
