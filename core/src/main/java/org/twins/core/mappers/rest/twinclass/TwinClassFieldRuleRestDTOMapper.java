1c321f839aae25e11ce9cbd8f6149b64f8868e20
nikita.mekh <n.mekh@hutkigrosh.by>
2026-06-01 16:02:22 +0300

    TWINS-509: add MANAGED mode for TwinClassFieldRule; extend DTO with overwriter fields and parameters; implement permission validation for MANAGED mode

--- core/src/main/java/org/twins/core/mappers/rest/twinclass/TwinClassFieldRuleRestDTOMapper.java
+++ core/src/main/java/org/twins/core/mappers/rest/twinclass/TwinClassFieldRuleRestDTOMapper.java
@@ -1,21 +1,26 @@
 package org.twins.core.mappers.rest.twinclass;
 
 import lombok.RequiredArgsConstructor;
+import org.cambium.common.exception.ServiceException;
 import org.cambium.featurer.FeaturerService;
 import org.springframework.stereotype.Component;
 import org.twins.core.controller.rest.annotation.MapperModeBinding;
 import org.twins.core.controller.rest.annotation.MapperModePointerBinding;
-import org.twins.core.dao.twinclass.TwinClassFieldEntity;
 import org.twins.core.dao.twinclass.TwinClassFieldRuleEntity;
 import org.twins.core.dto.rest.twinclass.TwinClassFieldDescriptorDTO;
 import org.twins.core.dto.rest.twinclass.TwinClassFieldRuleDTOv1;
+import org.twins.core.exception.ErrorCodeTwins;
 import org.twins.core.featurer.fieldrule.fieldoverwriter.FieldParamOverwriter;
 import org.twins.core.featurer.fieldtyper.descriptor.FieldDescriptor;
 import org.twins.core.mappers.rest.RestSimpleDTOMapper;
+import org.twins.core.mappers.rest.featurer.FeaturerRestDTOMapper;
 import org.twins.core.mappers.rest.mappercontext.MapperContext;
+import org.twins.core.mappers.rest.mappercontext.modes.FeaturerMode;
 import org.twins.core.mappers.rest.mappercontext.modes.TwinClassFieldConditionMode;
 import org.twins.core.mappers.rest.mappercontext.modes.TwinClassFieldMode;
 import org.twins.core.mappers.rest.mappercontext.modes.TwinClassFieldRuleMode;
+import org.twins.core.service.permission.PermissionService;
+import org.twins.core.service.permission.Permissions;
 import org.twins.core.service.twinclass.TwinClassFieldConditionService;
 import org.twins.core.service.twinclass.TwinClassFieldService;
 
@@ -32,6 +37,9 @@ public class TwinClassFieldRuleRestDTOMapper extends RestSimpleDTOMapper<TwinCla
     @MapperModePointerBinding(modes = TwinClassFieldMode.TwinRule2TwinClassFieldMode.class)
     private final TwinClassFieldRestDTOMapper twinClassFieldRestDTOMapper;
 
+    @MapperModePointerBinding(modes = FeaturerMode.TwinClassFieldRule2FeaturerMode.class)
+    private final FeaturerRestDTOMapper featurerRestDTOMapper;
+
     private final TwinClassFieldDescriptorRestDTOMapper twinClassFieldDescriptorRestDTOMapper;
 
     private final TwinClassFieldConditionService twinClassFieldConditionService;
@@ -40,16 +48,31 @@ public class TwinClassFieldRuleRestDTOMapper extends RestSimpleDTOMapper<TwinCla
 
     private final FeaturerService featurerService;
 
+    private final PermissionService permissionService;
+
     @Override
     public void map(TwinClassFieldRuleEntity src, TwinClassFieldRuleDTOv1 dst, MapperContext mapperContext) throws Exception {
         if (src == null || dst == null)
             return;
-        // map scalar fields
-        dst
-                .setId(src.getId())
-                .setOverwrittenValue(src.getOverwrittenValue())
-                .setOverwrittenRequired(src.getOverwrittenRequired())
-                .setRulePriority(src.getRulePriority());
+        switch (mapperContext.getModeOrUse(TwinClassFieldRuleMode.DETAILED)) {
+            case MANAGED:
+                if (!permissionService.currentUserHasPermission(Permissions.TWIN_CLASS_FIELD_MANAGE))
+                    throw new ServiceException(ErrorCodeTwins.SHOW_MODE_ACCESS_DENIED, "Show Mode[" + TwinClassFieldMode.MANAGED + "] is not allowed for current user");
+                dst
+                        .setFieldOverwriterFeaturerId(src.getFieldOverwriterFeaturerId())
+                        .setFieldOverwriterParams(src.getFieldOverwriterParams());
+                if (mapperContext.hasModeButNot(FeaturerMode.TwinClassFieldRule2FeaturerMode.HIDE)) {
+                    dst.setFieldOverwriterFeaturerId(src.getFieldOverwriterFeaturerId());
+                    featurerRestDTOMapper.postpone(src.getFieldOverwriterFeaturerId(), mapperContext.forkOnPoint(FeaturerMode.TwinClassFieldRule2FeaturerMode.SHORT));
+                }
+            default:
+                dst
+                        .setId(src.getId())
+                        .setOverwrittenValue(src.getOverwrittenValue())
+                        .setOverwrittenRequired(src.getOverwrittenRequired())
+                        .setRulePriority(src.getRulePriority());
+        }
+
         if (src.getFieldOverwriterFeaturerId() != null) {
             FieldParamOverwriter overwriter = featurerService.getFeaturer(src.getFieldOverwriterFeaturerId(), FieldParamOverwriter.class);
             FieldDescriptor descriptor = overwriter.getFieldOverwriterDescriptor(src);
