5f7d0b24cdbef86624be4450c40e18af7075062a
KevinORIS <c.s.2015@mail.ru>
2026-06-04 14:13:34 +0300

    TWINS-846: small refactor 2

--- core/src/main/java/org/twins/core/featurer/fieldtyper/FieldTyperCalcOnFly.java
+++ core/src/main/java/org/twins/core/featurer/fieldtyper/FieldTyperCalcOnFly.java
@@ -2,6 +2,7 @@ package org.twins.core.featurer.fieldtyper;
 
 import org.cambium.common.exception.ServiceException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.twins.core.domain.ApiUser;
 import org.twins.core.featurer.fieldtyper.descriptor.FieldDescriptor;
 import org.twins.core.featurer.fieldtyper.storage.TwinFieldStorage;
 import org.twins.core.featurer.fieldtyper.value.FieldValue;
@@ -16,9 +17,10 @@ public abstract class FieldTyperCalcOnFly<D extends FieldDescriptor, T extends F
     protected AuthService authService;
 
     protected PermissionContext calcPermissionContext() throws ServiceException {
+        ApiUser apiUser = authService.getApiUser();
         return new PermissionContext(
-                authService.getApiUser().getUserId(),
-                authService.getApiUser().getUser().getUserGroupsFootprint()
+                apiUser.getUserId(),
+                apiUser.getUser().getUserGroupsFootprint()
         );
     }
 }
