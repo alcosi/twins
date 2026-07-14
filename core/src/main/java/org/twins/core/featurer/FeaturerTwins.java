e1562f6709cafe4393941cadc25d116fba668338
nikita.mekh <n.mekh@hutkigrosh.by>
2026-07-14 18:14:12 +0300

    TWINS-871: new FillerFieldFromItemField

--- core/src/main/java/org/twins/core/featurer/FeaturerTwins.java
+++ core/src/main/java/org/twins/core/featurer/FeaturerTwins.java
@@ -237,6 +237,7 @@ public abstract class FeaturerTwins extends Featurer {
     public static final int ID_2360 = 2360;
     public static final int ID_2361 = 2361;
     public static final int ID_2362 = 2362;
+    public static final int ID_2363 = 2363;
     public static final int ID_2401 = 2401;
     public static final int ID_2402 = 2402;
     public static final int ID_2403 = 2403;
