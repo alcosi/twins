87a04ec84bcc4c6868460ecf44c1dac282bf3731
Turkov.Aleksey <alextur94@gmail.com>
2026-07-16 11:23:38 +0300

    TWINS-877: add missing featurer

--- core/src/main/resources/db/migration/V1.4.319.01__TWINS-877_featurer.sql
+++ core/src/main/resources/db/migration/V1.4.319.01__TWINS-877_featurer.sql
@@ -1,6 +1,7 @@
 alter table public.featurer_param_type
     alter column regexp type varchar(1024) using regexp;
 
+INSERT INTO featurer (id, featurer_type_id, class, name, description, deprecated) VALUES (2452::integer, 24::integer, '', '', '', DEFAULT) ON CONFLICT DO NOTHING;
 INSERT INTO featurer (id, featurer_type_id, class, name, description, deprecated) VALUES (2363::integer, 23::integer, '', '', '', DEFAULT) ON CONFLICT DO NOTHING;
 INSERT INTO featurer (id, featurer_type_id, class, name, description, deprecated) VALUES (2364::integer, 23::integer, '', '', '', DEFAULT) ON CONFLICT DO NOTHING;
 INSERT INTO featurer (id, featurer_type_id, class, name, description, deprecated) VALUES (2365::integer, 23::integer, '', '', '', DEFAULT) ON CONFLICT DO NOTHING;
