4e5ee0b2d69eefc0e0c151584ad6eed77a68e152
Turkov.Aleksey <alextur94@gmail.com>
2026-07-30 16:29:37 +0300

    TWINS-877: add lookuper param for conditioner

--- core/src/main/java/org/twins/core/featurer/factory/conditioner/ConditionerFactoryItemValueExists.java
+++ core/src/main/java/org/twins/core/featurer/factory/conditioner/ConditionerFactoryItemValueExists.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Component;
 import org.twins.core.domain.factory.FactoryItem;
 import org.twins.core.featurer.FeaturerTwins;
 import org.twins.core.featurer.factory.lookuper.FieldLookuperNearest;
+import org.twins.core.featurer.params.FeaturerParamStringTwinsFactoryFieldLookuper;
 import org.twins.core.featurer.params.FeaturerParamUUIDTwinsTwinClassFieldId;
 
 import java.util.Properties;
@@ -22,9 +23,12 @@ public class ConditionerFactoryItemValueExists extends Conditioner {
     @FeaturerParam(name = "Twin class field id", description = "", order = 1)
     public static final FeaturerParamUUID twinClassFieldId = new FeaturerParamUUIDTwinsTwinClassFieldId("twinClassFieldId");
 
+    @FeaturerParam(name = "Field lookupper", description = "Field lookupper", order = 2, defaultValue = "fromItemOutputFields", optional = true)
+    public static final FeaturerParamStringTwinsFactoryFieldLookuper fieldLookupper = new FeaturerParamStringTwinsFactoryFieldLookuper("fieldLookupper");
+
     @Override
     public boolean check(Properties properties, FactoryItem factoryItem) throws ServiceException {
-        return check(properties, factoryItem, fieldLookupers.getFromItemOutputFields());
+        return check(properties, factoryItem, ((FieldLookuperNearest) fieldLookupers.getByType(fieldLookupper.extract(properties))));
     }
 
     public boolean check(Properties properties, FactoryItem factoryItem, FieldLookuperNearest fieldLookuper) throws ServiceException {
